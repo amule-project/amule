75a1389deb4fd76c6edb632e1b2d0fdd9379ac41
hetfield <hetfield@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-04-01 17:51:33 +0000

    fixed wrong and duplicated inclusion. please commit alla changes to changelog too (files related..) to keep tracks of changes and fix bugs

--- trunk/src/ClientCredits.cpp
+++ trunk/src/ClientCredits.cpp
@@ -19,25 +19,31 @@
 
 
 #ifdef __CRYPTO_DEBIAN_GENTOO__
-	#include <crypto++/config.h>
+//	#include <crypto++/config.h>
 	#include <crypto++/base64.h>
 	#include <crypto++/osrng.h>
 	#include <crypto++/files.h>
 	#include <crypto++/sha.h>
 #else
 	#ifdef __CRYPTO_MDK_SUSE_FC__
-		#include <cryptopp/config.h>
+//		#include <cryptopp/config.h>
 		#include <cryptopp/base64.h>
 		#include <cryptopp/osrng.h>
 		#include <cryptopp/files.h>
 		#include <cryptopp/sha.h>
 	#else
 		#ifdef __CRYPTO_SOURCE__
-			#include <crypto-5.1/config.h>
+//			#include <crypto-5.1/config.h>
 			#include <crypto-5.1/base64.h>
 			#include <crypto-5.1/osrng.h>
 			#include <crypto-5.1/files.h>
 			#include <crypto-5.1/sha.h>
+		#else 
+//		#include <cryptopp/config.h>
+		#include <cryptopp/base64.h>
+		#include <cryptopp/osrng.h>
+		#include <cryptopp/files.h>
+		#include <cryptopp/sha.h>
 		#endif
 	#endif
 #endif
