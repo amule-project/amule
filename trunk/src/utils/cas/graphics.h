9644816be9fc05f8a61fb70179c88c422a8cfd7f
citroklar <citroklar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-04-17 10:34:52 +0000

    belongs to the other commit

---
+++ trunk/src/utils/cas/graphics.h
@@ -0,0 +1,8 @@
+#ifndef CAS_GRAPHICS_H
+#define CAS_GRAPHICS_H
+
+#include "configfile.h"
+
+int createimage(CONF *config, char lines[6][80]);
+
+#endif
---
+++ trunk/src/utils/cas/version.h
@@ -0,0 +1,3 @@
+#ifndef CAS_VERSION
+#define CAS_VERSION	"0.3"
+#endif
