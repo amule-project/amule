297e2195c7187852f2b02d0704c3d58d66cb58bc
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-04-26 16:47:56 +0000

    Cleaning

--- trunk/src/SharedFileList.h
+++ trunk/src/SharedFileList.h
@@ -28,13 +28,6 @@
 #include "types.h"		// Needed for uint16 and uint64
 #include "CTypedPtrList.h"	// Needed for CTypedPtrList
 
-/*
-struct UnknownFile_Struct{
-	char* name;
-	char* directory;
-	CPartFile* partfile_Owner;
-};
-*/
 struct UnknownFile_Struct;
 
 class CKnownFileList;
@@ -44,45 +37,6 @@ class CKnownFile;
 class CServerConnect;
 class CPreferences;
 class CMemFile;
-
-#if 0
-WX_DECLARE_LIST(UnknownFileStruct,UnknownList);
-
-class CFilePtrList : public UnknownList
-{
-public:
-  CFilePtrList() : UnknownList()
-    { };
-  virtual ~CFilePtrList() 
-    { };
-  
-  int GetCount()	{ int nResult;
-  wxMutexLocker locker(m_lockFilePtrList);
-  nResult = UnknownList::GetCount();
-  return nResult; };
-  
-  bool IsEmpty()		{ bool bResult;
-  wxMutexLocker locker(m_lockFilePtrList);
-  bResult = UnknownList::IsEmpty();
-  return bResult; };
-  
-  UnknownList::Node* AddTail(UnknownFileStruct* pNewElement) 
-    { UnknownList::Node* pos;
-    wxMutexLocker locker(m_lockFilePtrList);
-    pos = UnknownList::Append(pNewElement);
-    return pos; };
-  
-  UnknownFileStruct* RemoveHead() 
-    { UnknownFileStruct* pFile;
-    wxMutexLocker locker(m_lockFilePtrList);
-    pFile = UnknownList::GetFirst()->GetData();
-    UnknownList::DeleteNode(UnknownList::GetFirst());
-    return pFile; };
-  
-  //CRITICAL_SECTION 	m_lockFilePtrList;
-  wxMutex m_lockFilePtrList;
-};
-#endif
 
 typedef std::map<CCKey,CKnownFile*> CKnownFileMap;
 
@@ -119,57 +73,5 @@ private:
 	CSharedFilesCtrl*	output;
 	CAddFileThread*		m_Thread;
 };
-
-#if 0
-WX_DECLARE_HASH_MAP(wxString,CKnownFile*,wxStringHash,wxStringEqual,FilesMap);
-WX_DECLARE_LIST(UnknownFileStruct,WaitingForList);
-
-class CSharedFileList{
-	friend class CSharedFilesCtrl;
-	friend class CClientReqSocket;
-public:
-	CSharedFileList(CPreferences* in_prefs,CServerConnect* in_server, CKnownFileList* in_filelist);
-	~CSharedFileList();
-	void	SendListToServer();
-	void	Reload(bool sendtoserver = true);
-	void	SafeAddKFile(CKnownFile* toadd, bool bOnlyAdd = false);
-	void	SetOutputCtrl(CSharedFilesCtrl* in_ctrl);
-	void	RemoveFile(CKnownFile* toremove);
-	wxMutex	list_mut;
-	CKnownFile*	GetFileByID(unsigned char* filehash);
-	short	GetFilePriorityByID(unsigned char* filehash);
-	CKnownFileList*		filelist;
-	void	CreateOfferedFilePacket(CKnownFile* cur_file,CMemFile* files);
-	uint64	GetDatasize();
-	uint16	GetCount()	{return m_Files_map.size(); }
-	void	UpdateItem(CKnownFile* toupdate);
-private:
-	void	FindSharedFiles();
-	void	AddFilesFromDirectory(char* directory);
-	void	HashNextFile();
-
-	//CMap<CCKey,CCKey&,CKnownFile*,CKnownFile*> m_Files_map;
-	FilesMap m_Files_map;
-	//CTypedPtrList<CPtrList, UnknownFileStruct*> waitingforhash_list;
-	WaitingForList waitingforhash_list;
-	CPreferences*		app_prefs;
-	CServerConnect*		server;
-	CSharedFilesCtrl*	output;
-};
-#endif
-
-/*
-class CAddFileThread : public wxThread
-{
-public:
-	CAddFileThread();
-public:
-	virtual	bool	InitInstance() {return true;}
-	virtual ExitCode Entry();
-	virtual void OnExit() {}
-
-private:
-};
-*/
 
 #endif // SHAREDFILELIST_H
