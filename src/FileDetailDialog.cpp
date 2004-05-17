1b28ea413be7fb65d0bb8b8e649a592398819c00
kry <kry@amule.org>
2004-05-17 23:46:16 +0000

    wx cvs fixings, 1st part

--- src/FileDetailDialog.cpp
+++ src/FileDetailDialog.cpp
@@ -22,6 +22,8 @@
 
 #include <wx/settings.h>	// Needed for wxSYS_COLOUR_WINDOW
 #include <wx/stattext.h>	// Needed for wxStaticText
+#include <wx/sizer.h>
+#include <wx/intl.h>
 
 #include "muuli_wdr.h"		// Needed for ID_CLOSEWNDFD,...,IDC_RENAME
 #include "FileDetailDialog.h"	// Interface declarations
