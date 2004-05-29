51cb6a8878610fd2dce0194f6838ac3fafbf8e12
citroklar <citroklar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-05-29 18:21:07 +0000

    even more stupid...maybe I should give write access to cvs back

--- trunk/src/ServerList.cpp
+++ trunk/src/ServerList.cpp
@@ -539,7 +539,7 @@ void CServerList::AddServersFromTextFile(CString strFilename,bool isstaticserver
 
 		// create server object and add it to the list
 		CServer* nsrv = new CServer(atoi(unicode2char(strPort)), unicode2char(strHost));
-		nsrv->SetListName(unicode2char(strName));
+		nsrv->SetListName((char*) unicode2char(strName));
 
 		// emanuelw(20030924) fix: isstaticserver now is used! before it was always true
 		nsrv->SetIsStaticMember(isstaticserver);
