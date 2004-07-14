e96b2c677696a55c07c0f4b3186e0195c4fa195e
lfroen <lfroen@amule.org>
2004-07-14 18:19:46 +0000

    guievents implementation

--- src/GuiEvents.h
+++ src/GuiEvents.h
@@ -21,6 +21,14 @@
 #ifndef GUIEVENTS_H
 #define GUIEVENTS_H
 
+#include "types.h"
+#include  <map>
+#include  <list>
+
+class wxSocketBase;
+class wxSocketServer;
+class wxSocketClient;
+
 enum GUI_Event_ID {
 	INVALID_EVENT = 0,
 	// queue list
@@ -136,6 +144,17 @@ class GUIEvent {
                 ptr_aux_value   = NULL;
         }
 
+        GUIEvent(GUI_Event_ID new_id, void *new_ptr, void *new_aux_ptr,
+		 byte value8, uint32 value32, uint64 value64, wxChar *str) {
+                ID              = new_id;       
+                byte_value      = value8;
+                long_value      = value32;
+                longlong_value  = value64;
+                string_value    = str;
+                ptr_value       = new_ptr;
+                ptr_aux_value   = new_aux_ptr;
+        }
+
 	GUI_Event_ID ID;
 	byte			byte_value;
 	uint16			short_value;
@@ -195,7 +214,6 @@ class GUIEvent {
 
 // chat
 #define Notify_ChatRefreshFriend(ptr)               Notify_1_ValEvent(CHAT_REFRESH_FRIEND, ptr)
-#define Notify_ChatFindFriend(ptr, val0, val1)      Notify_3_ValEvent(CHAT_FIND_FRIEND, ptr, (uint32)val0, (uint16)val1)
 #define Notify_ChatConnResult(ptr, val)             Notify_2_ValEvent(CHAT_CONN_RESULT, (void *)ptr, (byte)val)
 
 // misc
@@ -207,5 +225,284 @@ class GUIEvent {
 
 #define AddLogLineM(x,y); 			theApp.NotifyEvent(GUIEvent(ADDLOGLINE,x,y));
 #define AddDebugLogLineM(x,y); 	theApp.NotifyEvent(GUIEvent(ADDDEBUGLOGLINE,x,y));
+//
+// "Late bound pointers" lib
+//
+// Known issues: fix endiness, wxString xfer, compilation options
+//
+
+
+enum PtrXferCmd {
+	PTR_XFER_AUTH = 0, // auth gui against core
+	// ptr data xfer
+	PTR_XFER_PTR_REQ,  // request ptr data
+	PTR_XFER_STR_REQ,  // request string data
+	PTR_XFER_PTR_ACK,  // ptr data
+	PTR_XFER_STR_ACK,  // string data
+	// notification
+	PTR_XFER_NOTIFY,    // Core2Gui_Event_Msg
+	// initial exchage
+	PTR_XFER_INIT,
+};
+
+
+// this is packet from Notify_* macros as it travels thru ipc (network)
+// assuming sizeof(void*) is equal on both client and server
+class Notify_Event_Msg {
+ public:
+	byte cmd;  // PTR_XFER_*
+	uint32 event_id;
+	uint32 byte_value;
+	uint32 long_value;
+	uint64 longlong_value;
+	void*  ptr_value; 
+	void*  ptr_aux_value; 
+	uint32 data_len;
+	// string data in size string_len coming here in packet
+
+	bool PtrLooksGood()
+	{
+		return ((data_len < 1024) && ( ((uint32)ptr_value > 0x04000000) ) );
+	}
+	Notify_Event_Msg(GUIEvent &event);
+	Notify_Event_Msg() {}
+	void print() { printf("Notify_Event_Msg: %p %d bytes type %d\n", ptr_value, data_len, long_value); }
+};
+
+DECLARE_EVENT_TYPE(wxEVT_CORE_NOTIFY, wxEVT_USER_FIRST+1)
+
+class wxCoreNotifyEvent : public wxEvent {
+ public:
+	wxCoreNotifyEvent(Notify_Event_Msg &msg, wxChar *str);
+	wxEvent *Clone(void) const;
+
+	GUIEvent gui_evt;
+};
+
+//
+// "login" gui to core
+//
+class Notify_Gui_Login {
+ public:
+	byte cmd;       // PTR_XFER_AUTH
+	uint16 port;    // on this port client listening
+	char user[256]; // fixed size, no unicode
+	Notify_Gui_Login() { cmd = PTR_XFER_AUTH; }
+};
+
+
+//
+// Connection to already running remote side.
+class PtrsXferClient : public wxThread {
+	// one for data, one for core notification
+	wxSocketBase *sock, *notif_sock;
+
+	uint32 data_buff_size;
+	void *data_buff;
+
+	typedef unsigned int ptr_type;
+	// assuming that sizeof(ptr) == sizeof(int)
+	std::map<ptr_type, ptr_type> ptr_hash;
+
+	enum {
+		PTR_LOCAL  = 1,
+		PTR_STRING = 2,
+	};
+	std::map<ptr_type, int> ptr_hash_flags;
+
+	wxEvtHandler *handler;
+
+	virtual void *Entry();
+
+ public:
+	PtrsXferClient(wxEvtHandler *handler);
+	~PtrsXferClient();
+
+	void *LU(const void *ptr)
+		{
+			return (void *)ptr_hash[(ptr_type)ptr];
+		}
+
+	void NotifyHandler(Notify_Event_Msg &msg, wxChar *str);
+
+	void *RemotePtrInsert(void *rem_ptr, int size);
+	bool RemotePtrRemove(void *rem_ptr);
+
+	wxString *StrPtrInsert(wxString *rem_ptr);
+	void LocalPtrInsert(void *rem_ptr, void *loc_ptr);
+
+	bool XferReady();
+
+	// update local data associated with this ptr
+	int UpdateReq(void *rem_ptr, void *loc_ptr, int size);
+	int UpdateStrReq(wxString *rem_str, wxString *loc_str);
+
+	int ReadSrvAck();
+};
+
+class PtrsXferServerCliThread;
+
+class PtrsXferServer : public wxThread {
+	wxSocketBase *sock;
+	wxEvtHandler *handler;
+
+	virtual void *Entry();
+
+	// 1 core, many clients
+	std::list<PtrsXferServerCliThread *> clients;
+ public:
+	PtrsXferServer(wxEvtHandler *handler, int port);
+	~PtrsXferServer();
+
+	bool InitOk() { return sock != 0; }
+
+	void SendNotify(GUIEvent &evt);
+	int HaveClients() { return clients.size(); }
+
+	void OnClientGone(PtrsXferServerCliThread *client);
+};
+
+//
+// no events without gui - thread per client is a must
+//
+class PtrsXferServerCliThread : public wxThread {
+	wxSocketBase *sock;
+	wxSocketClient *notif_sock;
+	PtrsXferServer *server;
+
+	virtual void *Entry();
+
+	// dispatch functions
+	void Cmd_PtrInitXchange(Notify_Event_Msg &);
+
+	void Cmd_PtrReq(Notify_Event_Msg &);
+	void Cmd_StrReq(Notify_Event_Msg &);
+ public:
+	PtrsXferServerCliThread(wxSocketBase *sock, PtrsXferServer *server);
+	~PtrsXferServerCliThread();
+
+	int SendNotify(GUIEvent &evt);
+};
+
+DECLARE_EVENT_TYPE(wxEVT_CORE_NEW_CLIENT, wxEVT_USER_FIRST+2)
+
+class wxCoreNewClent : public wxEvent {
+ public:
+	wxCoreNewClent(PtrsXferServerCliThread *client);
+	wxEvent *Clone(void) const;
+
+	PtrsXferServerCliThread *client;
+};
+
+
+//
+// Representation of pointer on remote side. Object values on local side
+// updated in ctor. T is type of pointer, U type of class that perform
+// actual update.
+//
+// need template for proper casting
+//
+template <class T, class U>
+class RPtr : public U {
+	public:
+	RPtr(T* rem_ptr) : U(rem_ptr, sizeof(T))
+		{
+			RunUpdate();
+		}
+	~RPtr()
+		{
+		}
+	T *operator->()
+		{
+			return (T *)loc_ptr;
+		}
+	T *PtrCast(void *ptr)
+		{
+			return (T *)ptr;
+		}
+};
+
+// Default "class U" for RPtr: simple class - all data is scalar values
+// update ptr just by copying remote data here
+class RPtrDefaultBase {
+	protected:
+	void *rem_ptr, *loc_ptr;
+	int size;
+
+	public:
+        RPtrDefaultBase(void *rem_ptr, int size);
+
+	// transfer data from server and update client side ptrs
+	bool RunUpdate();
+
+	static void *RemotePtrInsert(void *ptr, int size);
+	static wxString *StrPtrInsert(wxString *str);
+	static void RemotePtrRemove(void *ptr);
+
+	// it's quite unreasonable to have more than one
+	static PtrsXferClient *xfer;
+};
+
+
+#if !defined(offset_of)
+#define offset_of(type,member) ((int)(&((type *)0)->member))
+#endif
+
+
+// "remote object" is pointer to object type C have field of type (T *) which have
+//  corresponding entry in hash table. Object pointed by hash is updated
+// it's updated same way as regular pointer
+
+// update object fields that valid only on local side
+inline void my_memcpy(void *dst, const void *src, int count)
+{
+	printf("xfercopy: %p -> %p %d bytes\n", src, dst, count);
+	memcpy(dst, src, count);
+}
+
+//
+// "local object" is something created locally: object type C have field of type T,
+//  this object is not updated, and hash table holding pointer to valid copy of T
+//  which is memcpy'ed after all data of C has been memcpy'ed
+#define XFER_LOCAL_OBJ_UPDATE(loc_ptr, rem_ptr, field) my_memcpy(&loc_ptr->field, \
+							      xfer->LU(&rem_ptr->field), \
+							      sizeof((rem_ptr)->field) )
+
+// wxString requires special attention (as any other lib class). Good thing is that
+// it need to be update only once, when object pointer is first inserted
+#define XFER_MSG_STR_UPDATE(field_ptr) xfer->UpdateStrReq(field_ptr, xfer->StrPtrInsert(field_ptr))
+
+// avoid calling xfer->RemotePtr* directly: need to do it thru correct type
+#define XFER_MSG_PTR_INSERT(ptr, type) type##RPtr::RemotePtrInsert((type *)ptr)
+#define XFER_MSG_PTR_REMOVE(ptr, type) type##RPtr::RemotePtrRemove(ptr)
+
+// FIXME:
+// "smart" pointer behavior must be different depending on target:
+//  1. On statically build client (core & gui together) it works like ordinary ptr
+//  2. On remote gui client it must call to late binding
+// Choosing behavior thru "define" requires to compile all sources twice: once per target
+// Instead, behavior can be choosen by linking with different file.
+//
+// define it or not depending on target type
+//#define REMOTE_GUI_BUILD
+
+#ifdef REMOTE_GUI_BUILD
+#define DEFINE_XFER_PTR(type, var) type##RPtr var
+#else
+#define DEFINE_XFER_PTR(type, var) type *var
+#endif
+
+// "RPtr" for real objects
+class CPartFile;
+
+class RPtrCPartFileBase : public RPtrDefaultBase {
+	public:
+        RPtrCPartFileBase(CPartFile *rem_ptr, int);
+	bool RunUpdate();
+	static CPartFile *RemotePtrInsert(CPartFile *ptr, int unused = 0);
+	static void RemotePtrRemove(CPartFile *ptr);
+
+};
+typedef RPtr<CPartFile, RPtrCPartFileBase> CPartFileRPtr;
 
 #endif // __GUIEVENTS_H__
