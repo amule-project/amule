bdd32f26ed9a7da82c8f0830d9d01b04d9730e88
kry <kry@amule.org>
2004-07-18 21:50:44 +0000

    Opps, I did it again

--- src/amule.cpp
+++ src/amule.cpp
@@ -1326,14 +1326,6 @@ void CamuleApp::Localize_mule()
 
 	}
 
-	if (language != wxLANGUAGE_CUSTOM) {
-		m_locale.AddCatalogLookupPathPrefix(wxT(LOCALEDIR));
-		m_locale.AddCatalog(wxT(PACKAGE));
-	} else {
-		m_locale.AddCatalogLookupPathPrefix(wxString::Format(wxT("%s/.aMule"), getenv("HOME")));
-		m_locale.AddCatalog(wxT("custom"));
-	}
-
 	int language_flags = 0;
 	
 	if ((language != wxLANGUAGE_CUSTOM) && (language != wxLANGUAGE_ITALIAN_NAPOLITAN)) {
@@ -1343,6 +1335,15 @@ void CamuleApp::Localize_mule()
 	if ((!m_locale.Init(language,language_flags)) && (language != wxLANGUAGE_DEFAULT) && (language != wxLANGUAGE_CUSTOM)) {
 		AddLogLineM(false,wxT("The selected locale seems not to be installed on your box. (Note: I'll try to set it anyway)"));
 	}
+
+        if (language != wxLANGUAGE_CUSTOM) {
+                m_locale.AddCatalogLookupPathPrefix(wxT(LOCALEDIR));
+                m_locale.AddCatalog(wxT(PACKAGE));
+        } else {
+                m_locale.AddCatalogLookupPathPrefix(wxString::Format(wxT("%s/.aMule"), getenv("HOME")));
+                m_locale.AddCatalog(wxT("custom"));
+        }
+
 }
 
 
