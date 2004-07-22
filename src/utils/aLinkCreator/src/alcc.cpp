f5db2191919c7dac615f3f3db45fb1ee49770102
thepolish <thepolish@amule.org>
2004-07-22 15:01:23 +0000

    Fix icons copyright

--- src/utils/aLinkCreator/src/alcc.cpp
+++ src/utils/aLinkCreator/src/alcc.cpp
@@ -7,8 +7,6 @@
 ///
 /// Copyright (C) 2004 by ThePolish
 ///
-/// Pixmaps from http://www.everaldo.com and http://www.amule.org
-///
 /// This program is free software; you can redistribute it and/or modify
 /// it under the terms of the GNU General Public License as published by
 /// the Free Software Foundation; either version 2 of the License, or
--- src/utils/aLinkCreator/src/alcpix.cpp
+++ src/utils/aLinkCreator/src/alcpix.cpp
@@ -7,7 +7,7 @@
 ///
 /// Copyright (C) 2004 by ThePolish
 ///
-/// Pixmaps from http://jimmac.musichall.cz/ikony.php3 | http://www.everaldo.com | http://www.amule.org
+/// Pixmaps from http://jimmac.musichall.cz/ikony.php3 | http://www.everaldo.com | http://www.icomania.com
 ///
 /// This program is free software; you can redistribute it and/or modify
 /// it under the terms of the GNU General Public License as published by
