9afa7fc31dfbc6bd1b582fdea08868beff3e9b70
thepolish <thepolish@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-07-22 15:01:23 +0000

    Fix icons copyright

--- trunk/src/utils/aLinkCreator/src/alcframe.cpp
+++ trunk/src/utils/aLinkCreator/src/alcframe.cpp
@@ -9,7 +9,7 @@
 ///
 /// Copyright (C) 2004 by Phoenix
 ///
-/// Pixmaps from http://jimmac.musichall.cz/ikony.php3 | http://www.everaldo.com | http://www.amule.org
+/// Pixmaps from http://jimmac.musichall.cz/ikony.php3 | http://www.everaldo.com | http://www.icomania.com
 ///
 /// This program is free software; you can redistribute it and/or modify
 /// it under the terms of the GNU General Public License as published by
@@ -424,7 +424,8 @@ AlcFrame::OnBarAbout (wxCommandEvent & event)
   wxMessageBox (_
                 ("aLinkCreator, the aMule ed2k link creator\n\n"
                  "(c) 2004 ThePolish <thepolish@vipmail.ru>\n\n"
-                 "Pixmaps from http://www.everaldo.com and http://www.amule.org\n\n"
+                 "Pixmaps from http://www.everaldo.com and http://www.icomania.com\n"
+		 "and http://jimmac.musichall.cz/ikony.php3\n\n"
                  "Distributed under GPL"),
                 _("About aLinkCreator"), wxOK | wxCENTRE | wxICON_INFORMATION);
 }
