e7df5285f950431113082287e4742d524139c7ed
xaignar <xaignar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-08-09 19:41:47 +0000

    Removing old hashing function.

--- trunk/src/KnownFile.cpp
+++ trunk/src/KnownFile.cpp
@@ -52,8 +52,6 @@
 WX_DEFINE_OBJARRAY(ArrayOfCMD4Hash);
 WX_DEFINE_OBJARRAY(ArrayOfCTag);
 
-static void MD4Transform(uint32 Hash[4], uint32 x[16]);
-
 CFileStatistic::CFileStatistic() : 
 	requested(0), 
 	transfered(0),
@@ -685,165 +683,29 @@ bool CKnownFile::WriteToFile(CFile* file){
 	return true;
 }
 
-#define USE_CRYPTO_HASH
-using namespace CryptoPP;
 
-void CKnownFile::CreateHashFromInput(CFile* file, int Length, uchar* Output, uchar* in_string) {
+void CKnownFile::CreateHashFromInput(CFile* file, int Length, uchar* Output, uchar* in_string)
+{
+	CryptoPP::MD4 a;
 
-#ifdef USE_CRYPTO_HASH
-	
-		MD4 a;
-	
-		if (in_string) {
-			//MD4(in_string,Length,Output);
-			a.CalculateDigest(Output,in_string,Length);
-		} else { 
-			unsigned char* input = new unsigned char[Length];
-			file->Read(input,Length);
-			a.CalculateDigest(Output,input,Length);
-			//MD4(input,Length,Output);
-			delete[] input;
-		}
-
-#else
-	// time critial
-
-	CFile* data = NULL;
-	if (in_string)
-		data = new CSafeMemFile(in_string,Length);
-		
-	bool PaddingStarted = false;
-	uint32 Hash[4];
-	Hash[0] = 0x67452301;
-	Hash[1] = 0xEFCDAB89;
-	Hash[2] = 0x98BADCFE;
-	Hash[3] = 0x10325476;
-	uint32 Required = Length;
-	uchar   X[64*128];  
-	while (Required >= 64){
-        uint32 len = Required / 64; 
-        if (len > sizeof(X)/(64 * sizeof(X[0]))) 
-             len = sizeof(X)/(64 * sizeof(X[0])); 
-		if (in_string)
-			data->Read(&X,len*64);
-		else
-			file->Read(&X,len*64);
-		for (uint32 i = 0; i < len; i++) 
-        { 
-           MD4Transform(Hash, (uint32*)(X + i*64)); 
-        }
-		Required -= len*64;
+	if (in_string) {
+		//MD4(in_string,Length,Output);
+		a.CalculateDigest(Output,in_string,Length);
+	} else { 
+		unsigned char* input = new unsigned char[Length];
+		file->Read(input,Length);
+		a.CalculateDigest(Output,input,Length);
+		//MD4(input,Length,Output);
+		delete[] input;
 	}
-	// bytes to read
-	Required = Length % 64;
-	if (Required != 0){
-		if (in_string)
-			data->Read(&X,Required);
-		else 
-			file->Read(&X,Required);
-	}
-	// in byte scale 512 = 64, 448 = 56
-	if (Required >= 56){
-		X[Required] = 0x80;
-		PaddingStarted = TRUE;
-		memset(&X[Required + 1], 0, 63 - Required);
-		MD4Transform(Hash, (uint32*)X);
-		Required = 0;
-	}
-	if (!PaddingStarted)
-		X[Required++] = 0x80;
-	memset(&X[Required], 0, 64 - Required);
-	// add size (convert to bits)
-	uint32 Length2 = Length >> 29; // Length / 2^29 (536870912)
-	Length <<= 3; // Length * 8
-	memcpy(&X[56], &Length, 4);
-	memcpy(&X[60], &Length2, 4);
-	MD4Transform(Hash, (uint32*)X);
-	md4cpy(Output, Hash);
-
-	if (data) {
-		delete data;
-		data = 0;
-	}
-
-#endif
-
-	
 }
+
 
 const CMD4Hash& CKnownFile::GetPartHash(uint16 part) const {
 	wxASSERT( part < hashlist.GetCount() );
 		
 	return hashlist[part];
 }
-
-static void MD4Transform(uint32 Hash[4], uint32 x[16])
-{
-  uint32 a = Hash[0];
-  uint32 b = Hash[1];
-  uint32 c = Hash[2];
-  uint32 d = Hash[3];
-
-  /* Round 1 */
-  MD4_FF(a, b, c, d, x[ 0], S11); // 01
-  MD4_FF(d, a, b, c, x[ 1], S12); // 02
-  MD4_FF(c, d, a, b, x[ 2], S13); // 03
-  MD4_FF(b, c, d, a, x[ 3], S14); // 04
-  MD4_FF(a, b, c, d, x[ 4], S11); // 05
-  MD4_FF(d, a, b, c, x[ 5], S12); // 06
-  MD4_FF(c, d, a, b, x[ 6], S13); // 07
-  MD4_FF(b, c, d, a, x[ 7], S14); // 08
-  MD4_FF(a, b, c, d, x[ 8], S11); // 09
-  MD4_FF(d, a, b, c, x[ 9], S12); // 10
-  MD4_FF(c, d, a, b, x[10], S13); // 11
-  MD4_FF(b, c, d, a, x[11], S14); // 12
-  MD4_FF(a, b, c, d, x[12], S11); // 13
-  MD4_FF(d, a, b, c, x[13], S12); // 14
-  MD4_FF(c, d, a, b, x[14], S13); // 15
-  MD4_FF(b, c, d, a, x[15], S14); // 16
-
-  /* Round 2 */
-  MD4_GG(a, b, c, d, x[ 0], S21); // 17
-  MD4_GG(d, a, b, c, x[ 4], S22); // 18
-  MD4_GG(c, d, a, b, x[ 8], S23); // 19
-  MD4_GG(b, c, d, a, x[12], S24); // 20
-  MD4_GG(a, b, c, d, x[ 1], S21); // 21
-  MD4_GG(d, a, b, c, x[ 5], S22); // 22
-  MD4_GG(c, d, a, b, x[ 9], S23); // 23
-  MD4_GG(b, c, d, a, x[13], S24); // 24
-  MD4_GG(a, b, c, d, x[ 2], S21); // 25
-  MD4_GG(d, a, b, c, x[ 6], S22); // 26
-  MD4_GG(c, d, a, b, x[10], S23); // 27
-  MD4_GG(b, c, d, a, x[14], S24); // 28
-  MD4_GG(a, b, c, d, x[ 3], S21); // 29
-  MD4_GG(d, a, b, c, x[ 7], S22); // 30
-  MD4_GG(c, d, a, b, x[11], S23); // 31
-  MD4_GG(b, c, d, a, x[15], S24); // 32
-
-  /* Round 3 */
-  MD4_HH(a, b, c, d, x[ 0], S31); // 33
-  MD4_HH(d, a, b, c, x[ 8], S32); // 34
-  MD4_HH(c, d, a, b, x[ 4], S33); // 35
-  MD4_HH(b, c, d, a, x[12], S34); // 36
-  MD4_HH(a, b, c, d, x[ 2], S31); // 37
-  MD4_HH(d, a, b, c, x[10], S32); // 38
-  MD4_HH(c, d, a, b, x[ 6], S33); // 39
-  MD4_HH(b, c, d, a, x[14], S34); // 40
-  MD4_HH(a, b, c, d, x[ 1], S31); // 41
-  MD4_HH(d, a, b, c, x[ 9], S32); // 42
-  MD4_HH(c, d, a, b, x[ 5], S33); // 43
-  MD4_HH(b, c, d, a, x[13], S34); // 44
-  MD4_HH(a, b, c, d, x[ 3], S31); // 45
-  MD4_HH(d, a, b, c, x[11], S32); // 46
-  MD4_HH(c, d, a, b, x[ 7], S33); // 47
-  MD4_HH(b, c, d, a, x[15], S34); // 48
-
-  Hash[0] += a;
-  Hash[1] += b;
-  Hash[2] += c;
-  Hash[3] += d;
-}
-
 
 Packet*	CKnownFile::CreateSrcInfoPacket(const CUpDownClient* forClient){
 	CTypedPtrList<CPtrList, CUpDownClient*> srclist;
