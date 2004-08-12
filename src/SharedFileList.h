43a86d72522a1b5aa87c2c673e6f98caed9228f1
phoenix <phoenix@amule.org>
2004-08-12 13:38:07 +0000

    lfroen, please review this. It undoes part of your last patch. CVS compilation was broken.

--- src/SharedFileList.h
+++ src/SharedFileList.h
@@ -1,22 +1,23 @@
+//
 // This file is part of the aMule Project
 //
 // Copyright (c) 2003-2004 aMule Project ( http://www.amule-project.net )
 // Copyright (C) 2002 Merkur ( merkur-@users.sourceforge.net / http://www.emule-project.net )
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
 
 #ifndef SHAREDFILELIST_H
 #define SHAREDFILELIST_H
@@ -48,6 +49,7 @@ public:
 	void	SendListToServer();
 	void 	Reload(bool sendtoserver = true, bool firstload = false);
 	void	SafeAddKFile(CKnownFile* toadd, bool bOnlyAdd = false);
+	void    SetOutputCtrl(CSharedFilesCtrl* in_ctrl);
 	void	RemoveFile(CKnownFile* toremove);
 	wxMutex	list_mut;
 	CKnownFile*	GetFileByID(const CMD4Hash& filehash);
@@ -69,6 +71,8 @@ private:
 	CKnownFileMap		m_Files_map;
 	CPreferences*		app_prefs;
 	CServerConnect*		server;
+	CSharedFilesCtrl*       output;
 };
 
 #endif // SHAREDFILELIST_H
+
