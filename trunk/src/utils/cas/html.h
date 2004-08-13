251af07ea5c3002969f2600154747924a82f4ad9
falso <falso@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-08-13 20:15:54 +0000

    and also forgot to add these two new files, DAMN ME!!

---
+++ trunk/src/utils/cas/html.h
@@ -0,0 +1,34 @@
+/*
+ *  Name:         HTML creation functions 
+ *
+ *  Purpose:      Create a nice HTML page with all the statistics
+ *
+ *  Author:       Pedro de Oliveira <falso@rdk.homeip.net>
+ *
+ *  Copyright (C) 2004 by Pedro de Oliveira
+ * 
+ *  This file is part of aMule.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the
+ *  Free Software Foundation, Inc.,
+ *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef CAS_HTML_H
+#define CAS_HTML_H
+
+int create_html(char stats[20][80], char lines[6][80], char template[120]);
+
+#endif
+
