8487b3b96b0413e85c36a502ee4568b8482979c7
phoenix <phoenix@amule.org>
2004-09-09 12:54:13 +0000

    Initial import of new MD4 hash code from emule 0.44a into KnownFile.{cpp,h}.

--- src/KnownFile.cpp
+++ src/KnownFile.cpp
@@ -1,3 +1,4 @@
+//
 // This file is part of the aMule Project
 //
 // parts of this file are based on work from pan One (http://home-3.tiscali.nl/~meost/pms/)
@@ -5,19 +6,20 @@
 // Copyright (c) 2003-2004 aMule Project ( http://www.amule-project.net )
 // Copyright (C) 2002 Merkur ( merkur-@users.sourceforge.net / http://www.emule-project.net )
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
 
 
 #include <wx/filefn.h>
@@ -749,11 +751,206 @@ void CKnownFile::CreateHashFromInput(CFile* file, int Length, uchar* Output, uch
 	}
 }
 
+#ifndef _AFX
+#ifndef VERIFY
+#define VERIFY(x) (void(x))
+#endif //VERIFY
+#ifndef ASSERT
+#define ASSERT(x)
+#endif //ASSERT
+#endif //_AFX
+
+void CKnownFile::CreateHashFromInput(FILE* file,CFile* file2, int Length, uchar* Output, uchar* in_string, CAICHHashTree* pShaHashOut) const
+{
+	ASSERT( Output != NULL || pShaHashOut != NULL);
+	// time critial
+	bool PaddingStarted = false;
+	uint32 Hash[4];
+	Hash[0] = 0x67452301;
+	Hash[1] = 0xEFCDAB89;
+	Hash[2] = 0x98BADCFE;
+	Hash[3] = 0x10325476;
+	CFile* data = NULL;
+	if (in_string)
+		data = new CMemFile(in_string,Length);
+	uint32 Required = Length;
+	uchar   X[64*128];
+
+	uint32	posCurrentEMBlock = 0;
+	uint32	nIACHPos = 0;
+	CAICHHashAlgo* pHashAlg = m_pAICHHashSet->GetNewHashAlgo();
+
+	while (Required >= 64){
+        uint32 len = Required / 64; 
+        if (len > sizeof(X)/(64 * sizeof(X[0]))) 
+             len = sizeof(X)/(64 * sizeof(X[0])); 
+		if (in_string)
+			data->Read(&X,len*64);
+		else if (file)
+            fread(&X,len*64,1,file); 
+		else if (file2)
+			file2->Read(&X,len*64);
+
+		// SHA hash needs 180KB blocks
+		if (pShaHashOut != NULL){
+			if (nIACHPos + len*64 >= EMBLOCKSIZE){
+				uint32 nToComplete = EMBLOCKSIZE - nIACHPos;
+				pHashAlg->Add(X, nToComplete);
+				ASSERT( nIACHPos + nToComplete == EMBLOCKSIZE );
+				pShaHashOut->SetBlockHash(EMBLOCKSIZE, posCurrentEMBlock, pHashAlg);
+				posCurrentEMBlock += EMBLOCKSIZE;
+				pHashAlg->Reset();
+				pHashAlg->Add(X+nToComplete,(len*64) - nToComplete);
+				nIACHPos = (len*64) - nToComplete;
+			}
+			else{
+				pHashAlg->Add(X, len*64);
+				nIACHPos += len*64;
+			}
+		}
+
+		if (Output != NULL){
+			for (uint32 i = 0; i < len; i++) 
+			{ 
+				MD4Transform(Hash, (uint32*)(X + i*64)); 
+			}
+		}
+		Required -= len*64;
+	}
+	// bytes to read
+	Required = Length % 64;
+	if (Required != 0){
+		if (in_string)
+			data->Read(&X,Required);
+		else if (file)
+			fread(&X,Required,1,file);
+		else if (file2)
+			file2->Read(&X,Required);
+
+		if (pShaHashOut != NULL){
+			if (nIACHPos + Required >= EMBLOCKSIZE){
+				uint32 nToComplete = EMBLOCKSIZE - nIACHPos;
+				pHashAlg->Add(X, nToComplete);
+				ASSERT( nIACHPos + nToComplete == EMBLOCKSIZE );
+				pShaHashOut->SetBlockHash(EMBLOCKSIZE, posCurrentEMBlock, pHashAlg);
+				posCurrentEMBlock += EMBLOCKSIZE;
+				pHashAlg->Reset();
+				pHashAlg->Add(X+nToComplete, Required - nToComplete);
+				nIACHPos = Required - nToComplete;
+			}
+			else{
+				pHashAlg->Add(X, Required);
+				nIACHPos += Required;
+			}
+
+		}
+	}
+	if (pShaHashOut != NULL){
+		if(nIACHPos > 0){
+			pShaHashOut->SetBlockHash(nIACHPos, posCurrentEMBlock, pHashAlg);
+			posCurrentEMBlock += nIACHPos;
+		}
+		ASSERT( posCurrentEMBlock == Length );
+		VERIFY( pShaHashOut->ReCalculateHash(pHashAlg, false) );
+	}
+
+	if (Output != NULL){
+		// in byte scale 512 = 64, 448 = 56
+		if (Required >= 56){
+			X[Required] = 0x80;
+			PaddingStarted = TRUE;
+			memset(&X[Required + 1], 0, 63 - Required);
+			MD4Transform(Hash, (uint32*)X);
+			Required = 0;
+		}
+		if (!PaddingStarted)
+			X[Required++] = 0x80;
+		memset(&X[Required], 0, 64 - Required);
+		// add size (convert to bits)
+		uint32 Length2 = Length >> 29;
+		Length <<= 3;
+		memcpy(&X[56], &Length, 4);
+		memcpy(&X[60], &Length2, 4);
+		MD4Transform(Hash, (uint32*)X);
+		md4cpy(Output, Hash);
+	}
+
+	delete pHashAlg;
+	if (data)
+		delete data;
+}
 
 const CMD4Hash& CKnownFile::GetPartHash(uint16 part) const {
 	wxASSERT( part < hashlist.GetCount() );
 		
 	return hashlist[part];
+}
+
+void CKnownFile::MD4Transform(uint32 Hash[4], uint32 x[16]) const
+{
+  uint32 a = Hash[0];
+  uint32 b = Hash[1];
+  uint32 c = Hash[2];
+  uint32 d = Hash[3];
+
+  /* Round 1 */
+  MD4_FF(a, b, c, d, x[ 0], S11); // 01
+  MD4_FF(d, a, b, c, x[ 1], S12); // 02
+  MD4_FF(c, d, a, b, x[ 2], S13); // 03
+  MD4_FF(b, c, d, a, x[ 3], S14); // 04
+  MD4_FF(a, b, c, d, x[ 4], S11); // 05
+  MD4_FF(d, a, b, c, x[ 5], S12); // 06
+  MD4_FF(c, d, a, b, x[ 6], S13); // 07
+  MD4_FF(b, c, d, a, x[ 7], S14); // 08
+  MD4_FF(a, b, c, d, x[ 8], S11); // 09
+  MD4_FF(d, a, b, c, x[ 9], S12); // 10
+  MD4_FF(c, d, a, b, x[10], S13); // 11
+  MD4_FF(b, c, d, a, x[11], S14); // 12
+  MD4_FF(a, b, c, d, x[12], S11); // 13
+  MD4_FF(d, a, b, c, x[13], S12); // 14
+  MD4_FF(c, d, a, b, x[14], S13); // 15
+  MD4_FF(b, c, d, a, x[15], S14); // 16
+
+  /* Round 2 */
+  MD4_GG(a, b, c, d, x[ 0], S21); // 17
+  MD4_GG(d, a, b, c, x[ 4], S22); // 18
+  MD4_GG(c, d, a, b, x[ 8], S23); // 19
+  MD4_GG(b, c, d, a, x[12], S24); // 20
+  MD4_GG(a, b, c, d, x[ 1], S21); // 21
+  MD4_GG(d, a, b, c, x[ 5], S22); // 22
+  MD4_GG(c, d, a, b, x[ 9], S23); // 23
+  MD4_GG(b, c, d, a, x[13], S24); // 24
+  MD4_GG(a, b, c, d, x[ 2], S21); // 25
+  MD4_GG(d, a, b, c, x[ 6], S22); // 26
+  MD4_GG(c, d, a, b, x[10], S23); // 27
+  MD4_GG(b, c, d, a, x[14], S24); // 28
+  MD4_GG(a, b, c, d, x[ 3], S21); // 29
+  MD4_GG(d, a, b, c, x[ 7], S22); // 30
+  MD4_GG(c, d, a, b, x[11], S23); // 31
+  MD4_GG(b, c, d, a, x[15], S24); // 32
+
+  /* Round 3 */
+  MD4_HH(a, b, c, d, x[ 0], S31); // 33
+  MD4_HH(d, a, b, c, x[ 8], S32); // 34
+  MD4_HH(c, d, a, b, x[ 4], S33); // 35
+  MD4_HH(b, c, d, a, x[12], S34); // 36
+  MD4_HH(a, b, c, d, x[ 2], S31); // 37
+  MD4_HH(d, a, b, c, x[10], S32); // 38
+  MD4_HH(c, d, a, b, x[ 6], S33); // 39
+  MD4_HH(b, c, d, a, x[14], S34); // 40
+  MD4_HH(a, b, c, d, x[ 1], S31); // 41
+  MD4_HH(d, a, b, c, x[ 9], S32); // 42
+  MD4_HH(c, d, a, b, x[ 5], S33); // 43
+  MD4_HH(b, c, d, a, x[13], S34); // 44
+  MD4_HH(a, b, c, d, x[ 3], S31); // 45
+  MD4_HH(d, a, b, c, x[11], S32); // 46
+  MD4_HH(c, d, a, b, x[ 7], S33); // 47
+  MD4_HH(b, c, d, a, x[15], S34); // 48
+
+  Hash[0] += a;
+  Hash[1] += b;
+  Hash[2] += c;
+  Hash[3] += d;
 }
 
 Packet*	CKnownFile::CreateSrcInfoPacket(const CUpDownClient* forClient){
