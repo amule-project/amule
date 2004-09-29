af14da1fa08b8d5d2b27569f4cd147aabc3c02d1
xaignar <xaignar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-09-29 19:29:24 +0000

    Small fix, thanks efe.

--- trunk/src/PrefsUnifiedDlg.cpp
+++ trunk/src/PrefsUnifiedDlg.cpp
@@ -297,7 +297,7 @@ bool PrefsUnifiedDlg::TransferToWindow()
 	FindWindow( IDC_OSDIR )->Enable( CPreferences::IsOnlineSignatureEnabled() );
 #warning UDPDisable isnt implemented!
 	FindWindow( IDC_UDPPORT )->Enable( !CPreferences::s_UDPDisable );
-	FindWindow( IDC_SERVERRETRIES )->Enable( CPreferences::DeadServer );
+	FindWindow( IDC_SERVERRETRIES )->Enable( CPreferences::DeadServer() );
 	FindWindow( IDC_HQR_VALUE )->Enable( CPreferences::DropHighQueueRankingSources() );
 
 
