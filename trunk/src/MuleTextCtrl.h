244329e5bfb2a1d265472b65ecd59b71480bd9e7
xaignar <xaignar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-09-30 16:00:58 +0000

    Adding GPL blorb and #ifndef to MuleTextCtrl*.

--- trunk/src/MuleTextCtrl.h
+++ trunk/src/MuleTextCtrl.h
@@ -1,3 +1,26 @@
+//
+// This file is part of the aMule Project
+//
+// Copyright (c) 2003-2004 aMule Project ( http://www.amule-project.net )
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#ifndef MULETEXTCTRL_H
+#define MULETEXTCTRL_H
+
 #include <wx/textctrl.h>
 
 
@@ -65,4 +88,5 @@ protected:
 	DECLARE_EVENT_TABLE()
 };
 
+#endif
 
