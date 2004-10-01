c51629ecff3ff011bb2ea212750ca43b6d550172
xaignar <xaignar@amule.org>
2004-10-01 14:21:30 +0000

    Fix cat-colors on downloadlistctrl.

--- src/DownloadListCtrl.cpp
+++ src/DownloadListCtrl.cpp
@@ -427,7 +427,7 @@ void CDownloadListCtrl::OnDrawItem(int item, wxDC* dc, const wxRect& rect, const
 	
 	dc->SetPen(*wxTRANSPARENT_PEN);
 
-	if ( content->type == FILE_TYPE && !GetFocus() ) {
+	if ( content->type == FILE_TYPE && ( !highlight || !GetFocus() ) ) {
 		// If we have category, override textforeground with what category tells us.
 		CPartFile *file = (CPartFile *) content->value;
 		if ( file->GetCategory() ) {
