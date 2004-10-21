e3305d4c6c421644fc99e981acc33434d6ac7171
xaignar <xaignar@amule.org>
2004-10-21 11:15:05 +0000

    Reverting lfroens change to ClientList.cpp and fixing the actual bug.

--- src/ClientList.cpp
+++ src/ClientList.cpp
@@ -141,7 +141,7 @@ void CClientList::UpdateClientID( CUpDownClient* client, uint32 newID )
 void CClientList::UpdateClientIP( CUpDownClient* client, uint32 newIP )
 {
 	// Sainity check
-	if ( !client || ( client->GetClientState() != CS_LISTED ) || ( client->GetIP() == newIP ) )
+	if ( ( client->GetClientState() != CS_LISTED ) || ( client->GetIP() == newIP ) )
 		return;
 
 	// Remove the old IP entry
@@ -156,7 +156,7 @@ void CClientList::UpdateClientIP( CUpDownClient* client, uint32 newIP )
 void CClientList::UpdateClientHash( CUpDownClient* client, const CMD4Hash& newHash )
 {
 	// Sainity check
-	if ( !client || ( client->GetClientState() != CS_LISTED ) || ( client->GetUserHash() == newHash ) )
+	if ( ( client->GetClientState() != CS_LISTED ) || ( client->GetUserHash() == newHash ) )
 		return;
 
 
@@ -638,8 +638,8 @@ void CClientList::FilterQueues()
 	for ( IDMap::iterator it = m_ipList.begin(); it != m_ipList.end(); ) {
 		IDMap::iterator tmp = it++;
 		
-		if ( it->second && theApp.ipfilter->IsFiltered( it->second->GetIP() ) ) {
-			it->second->Safe_Delete();
+		if ( theApp.ipfilter->IsFiltered( tmp->second->GetIP() ) ) {
+			tmp->second->Safe_Delete();
 		}
 	}
 }
