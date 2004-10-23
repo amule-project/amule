d28bfdc7032589e9af8527bcbdf55ae25bc360f2
gonosztopi <gonosztopi@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-10-23 14:03:43 +0000

    Documentation update.

--- trunk/src/ECcodes.h
+++ trunk/src/ECcodes.h
@@ -16,6 +16,11 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
+/*!
+ * \file ECcodes.h
+ *
+ * \brief EC types, OPCODEs, TAGNAMEs.
+ */
 
 #ifndef	ECCODES_H
 #define	ECCODES_H
@@ -37,58 +42,205 @@ typedef struct {
 } EC_Version_t;
 
 
-/*
- * EC opcodes
+/**
+ * <b>EC opcodes</b>
  *
- * If you plan to use opcodes greater than 0x00ff, please
- * change the size of ec_opcode_t to uint16.
+ * OpCodes tell the receiver what to do (with the provided data).
  */
 
+// If you plan to use opcodes greater than 0x00ff, please
+// change the size of ec_opcode_t to uint16.
+
 enum {
-	// authentication
-	EC_OP_AUTH_REQ	= 0x0001,
+
+		/*!
+		 * \brief Perform no operation.
+		 *
+		 * \par Tags:
+		 *	(none)
+		 */
+	EC_OP_NOOP = 0x0001,
+
+	//
+	// Authentication
+	//
+
+		/*!
+		 * \brief Authentication request.
+		 *
+		 * \par Tags:
+		 *	\b ::EC_TAG_PASSWD_HASH (required)\n
+		 *	::EC_TAG_CLIENT_NAME\n
+		 *	::EC_TAG_CLIENT_VERSION\n
+		 *	::EC_TAG_CLIENT_MOD\n
+		 *	\b ::EC_TAG_PROTOCOL_VERSION (required)
+		 */
+	EC_OP_AUTH_REQ,
+
+		/*!
+		 * \brief Authentication failed.
+		 *
+		 * \par Tags:
+		 *	::EC_TAG_STRING (optional) describing the reason why authentication was rejected.
+		 */
 	EC_OP_AUTH_FAIL,
+
+		/*!
+		 * \brief Authentication succeeded.
+		 *
+		 * \par Tags:
+		 *	(none)
+		 */
 	EC_OP_AUTH_OK,
-	
-	// generic commands - both way
-	EC_OP_STRINGS,
+
+	//
+	// Bidirectional commands
+	//
+
+	EC_OP_STRINGS,		// I don't know if will use this command ...
+
+		/*!
+		 * \brief Miscanelous data.
+		 *
+		 * \par Tags:
+		 *	(any tag)
+		 */
 	EC_OP_MISC_DATA,
 
+	//
 	// client -> server commands
-	
-	EC_OP_SHUTDOWN, // stop server
-	EC_OP_STAT_REQ, // request statistics
-	EC_OP_ED2K_LINK, // handle ed2k link
-	
-	EC_OP_GET_DLOAD_QUEUE, // request download queue contents
-	
-	EC_OP_Q_FILE_CMD, // perform action on file in queue
+	//
 
-	// server -> client response
-	EC_OP_DLOAD_QUEUE, // download queue contents
-	
+		/*!
+		 * \brief Request to shut down aMule.
+		 *
+		 * \par Tags:
+		 *	(none)
+		 */
+	EC_OP_SHUTDOWN,
+
+		/*!
+		 * \brief Request statistics.
+		 *
+		 * Request from client, server must reply with statistics string.
+		 *
+		 * \par Tags:
+		 *	(none)
+		 */
+	EC_OP_STAT_REQ,
+
+		/*!
+		 * \brief Handle ED2k link.
+		 *
+		 * \par Tags:
+		 *	\b ::EC_TAG_STRING (1+) holding ED2k link.
+		 */
+	EC_OP_ED2K_LINK,
+
+		/*!
+		 * \brief Request for download queue.
+		 *
+		 * \par Tags:
+		 *	(none)
+		 */
+	EC_OP_GET_DLOAD_QUEUE,
+
+		/*!
+		 * \brief Perform action on file in queue.
+		 *
+		 * \par Tags:
+		 *	(yet undefined) - lfroen, please review the idea!
+		 */
+	EC_OP_Q_FILE_CMD,
+
+
+
+
 	EC_OP_COMPAT	= 0x00ff	// compatibility opcode, for testing purposes only
 					// tags: EC_TAG_STRING: v1.0 message
 };
 
 
-/*
- * EC TAG names
+/******************************************************************************/
+
+/**
+ * <b>EC TAG names</b>
  *
- * Please do not use tagnames greater than 0x7fff, beacuse that
- * would mess up the protocol.
+ * TAGs hold the data, on/with the requested operation is to be performed.
  */
 
+// Please do not use tagnames greater than 0x7fff, beacuse that
+// would mess up the protocol.
+
 enum {
+		/*!
+		 * \brief General TAG with string type content.
+		 *
+		 * An EC_TAG_STRING holds a UTF-8 encoded string, with terminating zero byte.
+		 *
+		 * \par Child TAGs:
+		 *	(none)
+		 *
+		 * \note <tt>TAGLEN := strlen(</tt>string<tt>) + 1;</tt>
+		 */
 	EC_TAG_STRING		= 0x0001,
-	EC_TAG_PASSWD_HASH	= 0x0002,
-	EC_TAG_CLIENT_NAME	= 0x0003,
-	EC_TAG_CLIENT_VERSION	= 0x0004,
-	EC_TAG_CLIENT_MOD	= 0x0005,
-	EC_TAG_PROTOCOL_VERSION	= 0x0006,
-	
+
+		/*!
+		 * \brief MD4 password hash.
+		 *
+		 * An ::EC_TAG_STRING, dedicated to hold the password hash.
+		 *
+		 * \par Child TAGs:
+		 *	(none)
+		 */
+	EC_TAG_PASSWD_HASH,
+
+		/*!
+		 * \brief Client name.
+		 *
+		 * An ::EC_TAG_STRING, dedicated to hold the client name.
+		 *
+		 * \par Child TAGs:
+		 *	(none)
+		 */
+	EC_TAG_CLIENT_NAME,
+
+		/*!
+		 * \brief Client version string.
+		 *
+		 * An ::EC_TAG_STRING, dedicated to hold the client version.
+		 *
+		 * \par Child TAGs:
+		 *	(none)
+		 */
+	EC_TAG_CLIENT_VERSION,
+
+		/*!
+		 * \brief Client mod-name.
+		 *
+		 * An ::EC_TAG_STRING, dedicated to hold the client mod-name.
+		 *
+		 * \par Child TAGs:
+		 *	(none)
+		 */
+	EC_TAG_CLIENT_MOD,
+
+		/*!
+		 * \brief EC protocol version.
+		 *
+		 * Type of this tag is EC_Version_t, which defaults to
+		 * <tt>{ 2, 0 }</tt> for the 2.0 version.
+		 *
+		 * \par Child TAGs:
+		 *	(none)
+		 */
+	EC_TAG_PROTOCOL_VERSION,
+
+	//
 	// tags for partfile info
-	EC_TAG_PARTFILE = 0x0007,
+	//
+
+	EC_TAG_PARTFILE,
 	EC_TAG_PARTFILE_SIZE_FULL,
 	EC_TAG_PARTFILE_SIZE_XFER,
 	EC_TAG_PARTFILE_SIZE_DONE,
@@ -99,6 +251,7 @@ enum {
 	EC_TAG_PARTFILE_SOURCE_COUNT_NOT_CURRENT,
 	EC_TAG_PARTFILE_SOURCE_COUNT_XFER,
 	EC_TAG_PARTFILE_ED2K_LINK,
+
 };
 
 #endif	/* ECCODES_H */
