3ff235dffe888d1b525502775a4da380711a04d6
xaignar <xaignar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-10-25 09:22:23 +0000

    This changes the primary hashing thread to do both MD4 and AICH hashing for non-part files (normal shares) instead of having them first read and hashed for the MD4 hashset in the AddFileThread and then read and hashed for the AICH hashset in the AICH sync thread. This cut down hashing of ~2.8gb of data from ~12:40 minutes to ~7 minutes on my system in a simple testrun.

--- trunk/src/AddFileThread.cpp
+++ trunk/src/AddFileThread.cpp
@@ -37,13 +37,11 @@
 #include "CFile.h"			// Needed for CFile
 #include "AICHSyncThread.h"	// Needed for CAICHSyncThread
 
-#include "CryptoPP_Inc.h"	// Needed for MD4
-
-//! Size to hash in one go: PARTSIZE/95, which is 100kb.
-const unsigned int CRUMBSIZE = PARTSIZE/95;
+#include <algorithm>
 
 //! Max number of threads. Change if you have > 1 CPUs ;)
 const unsigned int MAXTHREADCOUNT = 1;
+
 
 /**
  * Container for queued files.
@@ -77,6 +75,12 @@ CAddFileThread::CAddFileThread()
 	// Ensure that the AICH thread isn't running
 	if ( CAICHSyncThread::IsRunning() )
 		CAICHSyncThread::Stop();
+}
+
+
+CAddFileThread::~CAddFileThread()
+{
+	ThreadCountDec();
 }
 
 
@@ -296,161 +300,131 @@ void CAddFileThread::AddFile(const wxString& path, const wxString& name, const C
 
 wxThread::ExitCode CAddFileThread::Entry()
 {
-	// Temporary file to contain the result
-	CKnownFile* knownfile 		= NULL;
-	// Current queued file
-	QueuedFile*	current 		= NULL;
-
-	// Cached value of filesize, to avoid constantly calling GetFileSize()
-	uint32		filesize 		= 0;
-
-	// The position in the current part.
-	uint32		current_pos 	= 0;
-
-	// The file currently getting hashed
-	CFile		file;
-	
-	// MD4 hashing class.
-	CryptoPP::MD4	hasher;
-	
+	// Pointer to the queued file currently being hashed
+	QueuedFile* current = NULL;
+	// Pointer to the known-file used to store the results
+	CKnownFile* knownfile = NULL;
 
 	// Continue to loop until there's nothing to do, or someone kills the threads
 	while ( IsRunning() ) {
-		// Haven't we started on hashing the file?
-		if ( !file.IsOpened() ) {
-
-			// Try to get the next file on queue
-			current = GetNextFile();
+		// Try to get the next file on queue
+		current = GetNextFile();
 			
-			if ( !current ) {
-				// Nothing to do, break
-				printf("Hasher: No non-busy files on queue, stopping thread.\n");
-				break;
-			}
-			
-			wxString filename = current->m_path + wxFileName::GetPathSeparator() + current->m_name;
+		if ( !current ) {
+			// Nothing to do, break
+			printf("Hasher: No files on queue, stopping thread.\n");
+			break;
+		}
 		
-			// Attempt to open the file
-			if ( !file.Open( filename, CFile::read ) ) {
-				printf("Hasher: Warning, failed to open file, skipping: %s\n", unicode2char(filename));
-				
-				// Whoops, something's wrong. Delete the item and continue
-				RemoveFromQueue( current );
-				current = NULL;
-				continue;
-			}
 
-
-			// We only support files <= 4gigs
-			if ( (uint64)file.GetLength() >= (uint64)(4294967295U) ) {
-				printf("Hasher: Warning, file is bigger than 4GB, skipping: %s\n", unicode2char(filename));
-				
-				// Close the current file so we can continue with the next
-				file.Close();
-
-				// Delete and continue 
-				RemoveFromQueue( current );
-				current = NULL;
-				continue;
-			}
-
-
-			// Create a CKnownFile to contain the result
-			knownfile = new CKnownFile();
-			
-			// Set initial values
-			knownfile->m_strFilePath = current->m_path;
-			knownfile->m_strFileName = current->m_name;
+		wxString filename = current->m_path + wxFileName::GetPathSeparator() + current->m_name;
 		
-			filesize = file.GetLength();
-			knownfile->SetFileSize( filesize );
-			knownfile->date = wxFileModificationTime( filename );
-			knownfile->m_AvailPartFrequency.Insert(0, 0, knownfile->GetPartCount() );
+		// The file currently getting hashed
+		CFile file;
 	
-			// Starting from scratch
-			current_pos = 0;
-
-			printf("Hasher: Starting to hash file: %s\n", unicode2char(current->m_name));
-		}
-
-
-		// Well read at most CRUMBSIZE bytes per cycle
-		uint32 cur_length = CRUMBSIZE;
-		if ( file.GetPosition() + CRUMBSIZE > filesize ) {
-			// Less than CRUMBSIZE left, reduce read-length		
-			cur_length = filesize - file.GetPosition();
-		}
-
-		byte* data = new byte[cur_length];
-		
-		// Check for read errors
-		if ( file.Read(data, cur_length) != cur_length ) {
-			printf("Hasher: Error while reading file, skipping: %s\n", unicode2char(current->m_name));
+		// Attempt to open the file
+		if ( !file.Open( filename, CFile::read ) ) {
+			printf("Hasher: Warning, failed to open file, skipping: %s\n", unicode2char(filename));
 			
-			file.Close();
-			
-			// Delete and continue 
+			// Whoops, something's wrong. Delete the item and continue
 			RemoveFromQueue( current );
-			current = NULL;
-			delete [] data;
 			continue;
 		}
 
-		// Update hash
-		hasher.Update(data, cur_length);
 
-		delete[] data;
-
-		// Move to next crumb
-		current_pos++;		
-
-
-		// Finished a part?
-		if ( current_pos >= PARTSIZE/CRUMBSIZE ) {
-			byte hash[16];
-
-			// Finalize hash and reset
-			hasher.Final(hash);
-		
-			knownfile->hashlist.Add( (uchar*)hash );
-
-			current_pos = 0;
+		// We only support files <= 4gigs
+		if ( (uint64)file.GetLength() >= (uint64)(4294967295U) ) {
+			printf("Hasher: Warning, file is bigger than 4GB, skipping: %s\n", unicode2char(filename));
+			
+			// Delete and continue 
+			RemoveFromQueue( current );
+			continue;
 		}
-	
-	
-		// Done hashing?
-		if ( file.GetPosition() >= filesize ) {
-			// If there was a part < PARTSIZE, then create the hash for that part now
-			if ( current_pos ) {
-				byte hash[16];
 
-				// Finalize hash and reset
-				hasher.Final(hash);
+
+		// Create a CKnownFile to contain the result
+		knownfile = new CKnownFile();
 		
-				knownfile->hashlist.Add( (uchar*)hash );
-			}
-
+		// Set initial values
+		knownfile->m_strFilePath = current->m_path;
+		knownfile->m_strFileName = current->m_name;
+		
+		knownfile->SetFileSize( file.GetLength() );
+		knownfile->date = wxFileModificationTime( filename );
+		knownfile->m_AvailPartFrequency.Insert(0, 0, knownfile->GetPartCount() );
 	
+		
+		// We can only allow ourselves to create AICH hashsets for knownfiles.
+		// This is because we do not know yet if a partfile was uncorrupted.
+		bool needsAICH = !current->m_owner;
+		bool error = false;
+		
+		
+		// Prepare the AICH hashset if needed
+		if ( needsAICH ) {
+			knownfile->GetAICHHashset()->FreeHashSet();
+		
+			printf("Hasher: Starting to create MD4 and AICH hash for file: %s\n", unicode2char(current->m_name));
+		} else {
+			printf("Hasher: Starting to create MD4 hash for file: %s\n", unicode2char(current->m_name));
+		}
+		
+		
+		// This loops creates the part-hashes, loop-de-loop.
+		while ( !error && ( file.GetPosition() < file.GetLength() ) && IsRunning() ) {
+			error = !CreateNextPartHash( &file, knownfile, needsAICH );
+		}
+
+
+		// Checking if something went wrong
+		if ( error ) {
+			printf("Hasher: Error while reading file, skipping: %s\n", unicode2char(current->m_name));
+		
+			// Remove the temp-result
+			delete knownfile;
+			
+			// Delete and continue 
+			RemoveFromQueue( current );
+			continue;
+		}
+		
+
+
+		if ( IsRunning() ) {
 			// If the file is >= PARTSIZE, then the filehash is that one hash,
 			// otherwise, the filehash is the hash of the parthashes
 			if ( knownfile->hashlist.GetCount() == 1 ) {
 				knownfile->m_abyFileHash = knownfile->hashlist[0];
 				knownfile->hashlist.Clear();
-				
 			} else {
-				hasher.Restart();
+				unsigned int len = knownfile->hashlist.GetCount() * 16;
+				byte* data = new byte[ len ];
 				
-				for (size_t i = 0; i < knownfile->hashlist.GetCount(); i++)
-					hasher.Update( knownfile->hashlist[i], 16 );
-
-				byte hash[16];
-				hasher.Final( hash );
+				for (size_t i = 0; i < knownfile->hashlist.GetCount(); i++) {
+					memcpy( data + 16*i, knownfile->hashlist[i], 16 );
+				}
 	
+				byte hash[16];
+	
+				knownfile->CreateHashFromString( data, len, hash, NULL );
+
 				knownfile->m_abyFileHash.SetHash( (uchar*)hash );
 			}
+			
+			
+			// Did we create a AICH hashset?
+			if ( needsAICH ) {
+				CAICHHashSet* m_pAICHHashSet = knownfile->GetAICHHashset();
 
-			// Nothing more to do with this file ...
-			file.Close();
+				m_pAICHHashSet->ReCalculateHash(false);
+				if ( m_pAICHHashSet->VerifyHashTree(true) ) {
+					m_pAICHHashSet->SetStatus(AICH_HASHSETCOMPLETE);
+		
+					if ( !m_pAICHHashSet->SaveHashSet() ) {
+						printf("Hasher: Warning, failed to save AICH hashset for file: %s\n", unicode2char(current->m_name));
+					}
+				}
+			}
 			
 			
 			// Pass on the completion
@@ -460,9 +434,7 @@ wxThread::ExitCode CAddFileThread::Entry()
 
 			printf("Hasher: Finished hashing file: %s\n", unicode2char(current->m_name));
 			
-			knownfile = NULL;
 			RemoveFromQueue( current );
-			current = NULL;
 		
 			wxPostEvent(&theApp, evt);
 		}
@@ -476,12 +448,65 @@ wxThread::ExitCode CAddFileThread::Entry()
 		current->m_busy = false;
 	}
 	
+	
 	// Notify that the thread has died
 	printf("Hasher: A thread has died.\n");
 
-	ThreadCountDec();
 
 	return 0;
 }
 
+
+bool CAddFileThread::CreateNextPartHash( CFile* file, CKnownFile* owner, bool createAICH )
+{
+	// Well read a PARTSIZE bytes per cycle
+	uint32 cur_length = PARTSIZE;
+	if ( file->GetPosition() + PARTSIZE > file->GetLength() ) {
+		// Less than PARTSIZE left, reduce read-length		
+		cur_length = file->GetLength() - file->GetPosition();
+	}
+
+	// Sainity check, this shoulnd't happen due to the loop calling this function, but ...
+	if ( cur_length == 0 ) {
+		printf("Hasher: Warning, EOF in CreateNextPartHash!\n");
+	
+		return false;
+	}
+	
+
+	byte* data = new byte[cur_length];
+		
+	// Check for read errors
+	if ( file->Read(data, cur_length) != cur_length ) {
+		delete[] data;
+		
+		return false;
+	}
+
+
+	// The MD4 hash
+	byte hash[16];
+	// The AICH hash
+	CAICHHashTree* pBlockAICHHashTree = NULL;
+
+	// Obtain the AICH hash if wanted 
+	if ( createAICH ) {
+		// Get the start-position of the current part
+		uint32 position = file->GetPosition() - cur_length;
+
+		pBlockAICHHashTree = owner->GetAICHHashset()->m_pHashTree.FindHash( position, cur_length);		
+	}
+
+
+	// Create the md4 hash and perhaps a AICH hash
+	owner->CreateHashFromString( data, cur_length, hash, pBlockAICHHashTree );
+	
+	// Store the md4 hash
+	owner->hashlist.Add( (uchar*)hash );
+
+
+	delete[] data;
+
+	return true;
+}
 
