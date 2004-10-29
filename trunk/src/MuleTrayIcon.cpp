5f3a054ebb6e2ad498e53af59ba069d599b54d5e
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-10-29 22:14:53 +0000

    Fixes to the new tray icon (70% done), speed improvements, and coherent code on constructor/init destructor/exit of amule.cpp

--- trunk/src/MuleTrayIcon.cpp
+++ trunk/src/MuleTrayIcon.cpp
@@ -22,6 +22,10 @@
 
 #ifdef USE_WX_TRAY 
 
+#include "pixmaps/mule_TrayIcon_big.ico.xpm"
+#include "pixmaps/mule_Tr_yellow_big.ico.xpm"
+#include "pixmaps/mule_Tr_grey_big.ico.xpm"
+
 #include <wx/menu.h>
 #include <wx/string.h>
 #include <wx/intl.h>		
@@ -32,6 +36,19 @@
 #include "Preferences.h"		// Needed for glod_prefs
 #include "DownloadQueue.h" 	// Needed for GetKbps
 #include "UploadQueue.h" 		// Needed for GetKbps
+#include "sockets.h"			// Needed for CServerConnect
+
+// Pop-up menu clickable entries
+
+enum {
+	TRAY_MENU_INFO = 10317,
+	TRAY_MENU_CLIENTINFO,
+	TRAY_MENU_DISCONNECT,
+	TRAY_MENU_CONNECT,
+	TRAY_MENU_HIDE,
+	TRAY_MENU_SHOW,
+	TRAY_MENU_EXIT
+};
 
 
 /****************************************************/
@@ -51,6 +68,12 @@ END_EVENT_TABLE()
 
 CMuleTrayIcon::CMuleTrayIcon()
 {
+	Old_Icon = -1;
+	Old_SpeedSize = -1;
+	// Create the background icons (speed improvement)
+	Disconnected_Icon = wxIcon(mule_TrayIcon_big_ico_xpm);
+	LowId_Icon 			= wxIcon(mule_Tr_yellow_big_ico_xpm);
+	HighId_Icon			= wxIcon(mule_Tr_grey_big_ico_xpm);
 }
 
 
@@ -68,63 +91,87 @@ CMuleTrayIcon::~CMuleTrayIcon()
 /****************************************************/
 
 
-void CMuleTrayIcon::SetTrayIcon(wxIcon& Icon, uint32 percent) 
+void CMuleTrayIcon::SetTrayIcon(int Icon, uint32 percent) 
 {
-	CurrentIcon = Icon;
-	// Do whatever to the icon before drawing it (percent)
 
-	wxColour temp;
-	
-	IconWithSpeed.SelectObject(CurrentIcon);
+	switch (Icon) {
+		case TRAY_ICON_HIGHID:
+			// Most likely case, test first
+			CurrentIcon = HighId_Icon;
+			break;
+		case TRAY_ICON_LOWID:
+			CurrentIcon = LowId_Icon;
+			break;
+		case TRAY_ICON_DISCONNECTED:
+			CurrentIcon = Disconnected_Icon;
+			break;
+		default:
+			// W00T?
+			wxASSERT(0);			
+	}
 
-	// Get the transparency colour.
-	IconWithSpeed.GetPixel(0,0, &temp);
-
-	IconWithSpeed.SelectObject(wxNullBitmap);
+	// Lookup this values for speed improvement: don't draw if not needed
 	
-	// Set a new mask with transparency removed
-	wxMask* new_mask = new wxMask(CurrentIcon, temp);
-	
-	CurrentIcon.SetMask(new_mask);
-	
-	IconWithSpeed.SelectObject(CurrentIcon);
-	
-	// Get the solid background.
-	IconWithSpeed.GetPixel(0,0, &temp);
-	
-	// Set the colour for the traffic bar.
-	IconWithSpeed.SetBrush(*wxTRANSPARENT_BRUSH);
-	IconWithSpeed.SetPen(*wxCYAN_PEN);
-	
-	// Speed bar is: centered, taking 80% of the icon heigh, and 
-	// right-justified taking a 10% of the icon width.
-	// X
-
-	int Bar_xSize = (Icon.GetWidth() / 4); 
-	int Bar_xPos = Icon.GetWidth() - Bar_xSize -1; 
-		
-	// Y
-	int Bar_yPos = 0;
-	int Bar_ySize = Icon.GetHeight()-2; 
-	 
-	IconWithSpeed.DrawRectangle(Bar_xPos, Bar_yPos, Bar_xSize, Bar_ySize);
-	
-	IconWithSpeed.SetBrush(*wxBLUE_BRUSH);
-	IconWithSpeed.SetPen(*wxTRANSPARENT_PEN);
-	
+	int Bar_ySize = CurrentIcon.GetHeight()-2; 
 	int NewSize = ((Bar_ySize -2) * percent) / 100;
 	
-	IconWithSpeed.DrawRectangle(Bar_xPos + 1, (Bar_yPos + 1) + ((Bar_ySize -2) - NewSize), Bar_xSize -2 , NewSize);
-
-	// Unselect the icon.
-	IconWithSpeed.SelectObject(wxNullBitmap);	
+	if ((Old_Icon != Icon) || (Old_SpeedSize != NewSize)) {
+		
+		Old_Icon = Icon;
+		Old_SpeedSize = NewSize;
+		
+		// Do whatever to the icon before drawing it (percent)
 	
-
-	new_mask = new wxMask(CurrentIcon, temp);
+		wxColour temp;
+		
+		IconWithSpeed.SelectObject(CurrentIcon);
 	
-	CurrentIcon.SetMask(new_mask);
+		// Get the transparency colour.
+		IconWithSpeed.GetPixel(0,0, &temp);
 	
-	UpdateTray();
+		IconWithSpeed.SelectObject(wxNullBitmap);
+		
+		// Set a new mask with transparency removed
+		wxMask* new_mask = new wxMask(CurrentIcon, temp);
+		
+		CurrentIcon.SetMask(new_mask);
+		
+		IconWithSpeed.SelectObject(CurrentIcon);
+		
+		// Get the solid background.
+		IconWithSpeed.GetPixel(0,0, &temp);
+		
+		// Set the colour for the traffic bar.
+		IconWithSpeed.SetBrush(*wxTRANSPARENT_BRUSH);
+		IconWithSpeed.SetPen(*wxCYAN_PEN);
+		
+		// Speed bar is: centered, taking 80% of the icon heigh, and 
+		// right-justified taking a 10% of the icon width.
+		// X
+	
+		int Bar_xSize = (CurrentIcon.GetWidth() / 4); 
+		int Bar_xPos = CurrentIcon.GetWidth() - Bar_xSize -1; 
+			
+		// Y
+		int Bar_yPos = 0;
+		 
+		IconWithSpeed.DrawRectangle(Bar_xPos, Bar_yPos, Bar_xSize, Bar_ySize);
+		
+		IconWithSpeed.SetBrush(*wxBLUE_BRUSH);
+		IconWithSpeed.SetPen(*wxTRANSPARENT_PEN);
+		
+		IconWithSpeed.DrawRectangle(Bar_xPos + 1, (Bar_yPos + 1) + ((Bar_ySize -2) - NewSize), Bar_xSize -2 , NewSize);
+	
+		// Unselect the icon.
+		IconWithSpeed.SelectObject(wxNullBitmap);	
+		
+	
+		new_mask = new wxMask(CurrentIcon, temp);
+		
+		CurrentIcon.SetMask(new_mask);
+			
+		UpdateTray();
+	}
 }
 		
 void CMuleTrayIcon::SetTrayToolTip(const wxString& Tip)
@@ -142,11 +189,6 @@ void CMuleTrayIcon::UpdateTray() {
 	// Icon update and Tip update
 	SetIcon(CurrentIcon, CurrentTip);
 }
-
-enum {
-	TRAY_MENU_INFO = 10317,
-	TRAY_MENU_CLIENTINFO
-};
 
 wxMenu* CMuleTrayIcon::CreatePopupMenu() 
 {
@@ -179,6 +221,52 @@ wxMenu* CMuleTrayIcon::CreatePopupMenu()
 	traymenu->Append(TRAY_MENU_INFO, label);
 	traymenu->AppendSeparator();
 	
+	// Mule info
+	wxMenu* aMuleInfoMenu = new wxMenu();
+	aMuleInfoMenu->SetTitle(_("aMule Tray Menu Info"));
+
+	// Client Info
+	wxMenu* ClientInfoMenu = new wxMenu();
+	ClientInfoMenu->SetTitle(_("Client Information"));
+	
+	// Separator
+	traymenu->AppendSeparator();
+	
+	// Upload Speed sub-menu
+	wxMenu* UploadSpeedMenu = new wxMenu();
+	UploadSpeedMenu->SetTitle(_("Upload Limit"));
+	
+	// Download Speed sub-menu
+	wxMenu* DownloadSpeedMenu = new wxMenu();
+	DownloadSpeedMenu->SetTitle(_("Download Limit"));
+	
+	// Separator
+	traymenu->AppendSeparator();
+	
+	if (theApp.serverconnect->IsConnected()) {
+		//Disconnection Speed item
+		traymenu->Append(TRAY_MENU_DISCONNECT, _("Disconnect from server"));
+	} else {
+		//Connect item
+		traymenu->Append(TRAY_MENU_CONNECT, _("Connect to any server"));
+	}
+	
+	// Separator
+	traymenu->AppendSeparator();
+	
+	if (theApp.amuledlg->IsShown()) {
+		//hide item
+		traymenu->Append(TRAY_MENU_HIDE, _("Hide aMule"));
+	} else {
+		//show item
+		traymenu->Append(TRAY_MENU_SHOW, _("Hide aMule"));
+	}
+	
+	// Separator
+	traymenu->AppendSeparator();
+
+	// Exit item
+	traymenu->Append(TRAY_MENU_EXIT, _("Exit"));
 	
 	return traymenu;
 }		
