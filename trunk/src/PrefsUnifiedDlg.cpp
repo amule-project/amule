b2998cad545e97a707631bfdac75f7532efb5de5
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-11-20 00:32:35 +0000

    Fix crash when shutdown and preferences open

--- trunk/src/PrefsUnifiedDlg.cpp
+++ trunk/src/PrefsUnifiedDlg.cpp
@@ -265,7 +265,9 @@ PrefsUnifiedDlg::PrefsUnifiedDlg(wxWindow* parent)
 PrefsUnifiedDlg::~PrefsUnifiedDlg()
 {
 	// So the gui knows if the dlg exists.
-	theApp.amuledlg->prefs_dialog = NULL;
+	if (theApp.amuledlg) {
+		theApp.amuledlg->prefs_dialog = NULL;
+	}
 
 	// Un-Connect the Cfgs
 	thePrefs::CFGMap::iterator it = thePrefs::s_CfgList.begin();
