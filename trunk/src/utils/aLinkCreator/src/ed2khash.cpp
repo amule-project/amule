233942eb57e3dd0c3bc7906c644db899de925084
phoenix <phoenix@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-12-01 02:40:51 +0000

    Gonosztopi, I checked, it was changed in wx-2.5.1:
    from wx-2.5.1/include/wx/ffile.h:
    wxFFile(const wxChar *filename, const wxChar *mode = _T("r"));

--- trunk/src/utils/aLinkCreator/src/ed2khash.cpp
+++ trunk/src/utils/aLinkCreator/src/ed2khash.cpp
@@ -83,7 +83,7 @@ bool Ed2kHash::SetED2KHashFromFile(const wxFileName& filename, MD4Hook hook)
 {
   // Open file and let wxFFile destructor close the file
   // Closing it explicitly may crash on Win32 ...
-#if wxCHECK_VERSION(2,5,2)
+#if wxCHECK_VERSION(2,5,1)
   wxFFile file(filename.GetFullPath(), wxT("rbS"));
 #else
   wxFFile file(filename.GetFullPath(), "rbS");
