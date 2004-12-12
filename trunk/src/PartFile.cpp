62e683314cf4553b58cfbcb7f844008595a95a4c
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2004-12-12 07:16:30 +0000

    Woops, debug around

--- trunk/src/PartFile.cpp
+++ trunk/src/PartFile.cpp
@@ -2958,7 +2958,7 @@ void CPartFile::AddClientSources(CSafeMemFile* sources,uint8 sourceexchangeversi
 	uint16 nCount = sources->ReadUInt16();
 	for (int i = 0;i != nCount;++i) {
 		uint32 dwID = sources->ReadUInt32();
-		printf("Added source exchange v%u: %u(%s)\n",sourceexchangeversion,dwID,unicode2char(Uint32toStringIP(dwID)));
+		//printf("Added source exchange v%u: %u(%s)\n",sourceexchangeversion,dwID,unicode2char(Uint32toStringIP(dwID)));
 		uint16 nPort = sources->ReadUInt16();
 		uint32 dwServerIP = sources->ReadUInt32();
 		uint16 nServerPort = sources->ReadUInt16();
