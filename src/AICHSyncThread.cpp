4eac24909db405d8e811db6f239187190632d506
kry <kry@amule.org>
2005-01-10 06:59:45 +0000

    64 bits fi

--- src/AICHSyncThread.cpp
+++ src/AICHSyncThread.cpp
@@ -222,7 +222,7 @@ void* CAICHSyncThread::Entry()
 	//	}
 
 	if ( !queue.empty() ) {
-		printf("AICH Thread: Starting to hash files. %i files found.\n", queue.size() );
+		printf("AICH Thread: Starting to hash files. %li files found.\n", queue.size() );
 		while ( !queue.empty() ) {
 			// Check for termination
 			if ( TestDestroy() ) {
@@ -232,7 +232,7 @@ void* CAICHSyncThread::Entry()
 			CKnownFile* pCurFile = queue.front();
 			queue.pop_front();
 
-			printf("AICH Thread: Hashing file: %s, total files left: %u\n", unicode2char( pCurFile->GetFileName() ), queue.size() );
+			printf("AICH Thread: Hashing file: %s, total files left: %li\n", unicode2char( pCurFile->GetFileName() ), queue.size() );
 
 			// Just to be sure that the file hasnt been deleted lately
 			if ( !(theApp.knownfiles->IsKnownFile(pCurFile) && theApp.sharedfiles->GetFileByID(pCurFile->GetFileHash())) )
