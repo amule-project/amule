c3599d94e1c125dd14d52664545902ce414e1bb9
ken <ken@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-02-08 09:02:49 +0000

    Fixed a major endianness bug with widespread effects.  On Mac, servers don't fail continuously until eventually removed from the list; server pings work.  Global search seems to work better. DNS lookup of servers and ED2K link sources should now work.

--- trunk/src/amuleIPV4Address.h
+++ trunk/src/amuleIPV4Address.h
@@ -64,9 +64,9 @@ public:
 		}
 		// We have to take care that wxIPV4address's internals changed on 2.5.2
 		#if wxCHECK_VERSION(2,5,2)
-			return GAddress_INET_SetHostAddress(m_address,ntohl(ip))==GSOCK_NOERROR;
+			return GAddress_INET_SetHostAddress(m_address,wxUINT32_SWAP_ALWAYS(ip))==GSOCK_NOERROR;
 		#else
-			return GAddress_INET_SetHostAddress(m_address,ip)==GSOCK_NOERROR;
+			return GAddress_INET_SetHostAddress(m_address,ENDIAN_SWAP_32(ip))==GSOCK_NOERROR;
 		#endif
 	}
 	
