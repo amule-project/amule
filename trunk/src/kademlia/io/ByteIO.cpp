a9347d68739b0fdfa37613b2a5d796477c5a255f
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-02-11 15:50:01 +0000

    Still not ported, just to start building directory structure

---
+++ trunk/src/kademlia/io/ByteIO.cpp
@@ -0,0 +1,76 @@
+/*
+Copyright (C)2003 Barry Dunne (http://www.emule-project.net)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+// Note To Mods //
+/*
+Please do not change anything here and release it..
+There is going to be a new forum created just for the Kademlia side of the client..
+If you feel there is an error or a way to improve something, please
+post it in the forum first and let us look at it.. If it is a real improvement,
+it will be added to the offical client.. Changing something without knowing
+what all it does can cause great harm to the network if released in mass form..
+Any mod that changes anything within the Kademlia side will not be allowed to advertise
+there client on the eMule forum..
+*/
+
+#include "stdafx.h"
+#include "ByteIO.h"
+#include "IOException.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+
+////////////////////////////////////////
+using namespace Kademlia;
+////////////////////////////////////////
+
+void CByteIO::readArray(LPVOID lpResult, uint32 byteCount)
+{
+	if (m_available < byteCount)
+		throw new CIOException(ERR_BUFFER_TOO_SMALL);
+
+	memcpy(lpResult, m_buffer, byteCount);
+	m_buffer += byteCount;
+	m_used += byteCount;
+	m_available -= byteCount;
+}
+
+void CByteIO::writeArray(LPCVOID lpVal, uint32 byteCount)
+{
+	if (m_bReadOnly)
+		throw new CIOException(ERR_READ_ONLY);
+
+	if (m_available < byteCount)
+		throw new CIOException(ERR_BUFFER_TOO_SMALL);
+
+	memcpy(m_buffer, lpVal, byteCount);
+	m_buffer += byteCount;
+	m_used += byteCount;
+	m_available -= byteCount;
+}
+
+void CByteIO::reset(void)
+{
+	m_available += m_used;
+	m_buffer -= m_used;
+	m_used = 0;
+}
---
+++ trunk/src/kademlia/io/IOException.cpp
@@ -0,0 +1,58 @@
+/*
+Copyright (C)2003 Barry Dunne (http://www.emule-project.net)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either
+version 2 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+// Note To Mods //
+/*
+Please do not change anything here and release it..
+There is going to be a new forum created just for the Kademlia side of the client..
+If you feel there is an error or a way to improve something, please
+post it in the forum first and let us look at it.. If it is a real improvement,
+it will be added to the offical client.. Changing something without knowing
+what all it does can cause great harm to the network if released in mass form..
+Any mod that changes anything within the Kademlia side will not be allowed to advertise
+there client on the eMule forum..
+*/
+
+#include "stdafx.h"
+#include "IOException.h"
+#include <string.h>
+#include "../../Resource.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+
+////////////////////////////////////////
+using namespace Kademlia;
+////////////////////////////////////////
+
+BOOL CIOException::GetErrorMessage(LPTSTR lpszError, UINT nMaxError, PUINT pnHelpContext)
+{
+	CString msg = _T("ONLY AVAILABLE WITH EMULE INTEGRATION");
+//	CString msg = GetResString(IOEXCEPTION_ERR_BASE + m_cause);
+	_tcsncpy(lpszError, msg.GetBuffer(0), nMaxError);
+	lpszError[nMaxError-1] = 0;
+
+	if (pnHelpContext != NULL)
+		*pnHelpContext = 0;
+
+	return true;
+}
