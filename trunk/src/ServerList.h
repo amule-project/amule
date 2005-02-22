11b1f89d18907e7e6696addda320991981c2f152
xaignar <xaignar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-02-22 21:53:28 +0000

    Removing unused functions from CServerList.

--- trunk/src/ServerList.h
+++ trunk/src/ServerList.h
@@ -1,21 +1,23 @@
+//
 // This file is part of the aMule Project
 //
 // Copyright (c) 2003-2004 aMule Project ( http://www.amule-project.net )
 // Copyright (C) 2002 Merkur ( merkur-@users.sourceforge.net / http://www.emule-project.net )
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//You should have received a copy of the GNU General Public License
-//along with this program; if not, write to the Free Software
-//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
 
 
 #ifndef SERVERLIST_H
@@ -53,13 +55,10 @@ public:
 	bool		SaveServermetToFile(); //<<--9/22/02
 	void		ServerStats();
 	void		ResetServerPos()	{serverpos = 0;}
-	void		ResetSearchServerPos()	{searchserverpos = 0;}
 	CServer*	GetNextServer();
-	CServer*	GetNextSearchServer();
 	CServer*	GetNextStatServer();
 	CServer*	GetServerByIndex(uint32 pos)	{return list.GetAt(list.FindIndex(pos));}
 	uint32		GetServerCount()	{return list.GetCount();}
-	CServer*	GetNextServer(CServer* lastserver); // slow
 	CServer*	GetServerByAddress(const wxString& address, uint16 port);
 	CServer*	GetServerByIP(uint32 nIP);
 	CServer*	GetServerByIP(uint32 nIP, uint16 nPort);	
@@ -79,7 +78,6 @@ protected:
 private:
 	uint8			current_url_index;
 	uint32		serverpos;
-	uint32		searchserverpos;
 	uint32		statserverpos;
 	CTypedPtrList<CPtrList, CServer*>	list;
 	uint32		delservercount;
