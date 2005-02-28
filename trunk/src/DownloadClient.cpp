b8eb493c3969a0d582c5bcbf3b6781ea21c30bde
phoenix <phoenix@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-02-28 23:08:38 +0000

    Fixes a user reported crash.

--- trunk/src/DownloadClient.cpp
+++ trunk/src/DownloadClient.cpp
@@ -279,10 +279,11 @@ void CUpDownClient::ProcessFileInfo(const CSafeMemFile* data, const CPartFile* f
 void CUpDownClient::ProcessFileStatus(bool bUdpPacket, const CSafeMemFile* data, const CPartFile* file)
 {
 	// 0.42e
+	wxString strReqFileNull(_("ERROR: Wrong file ID (ProcessFileStatus; m_reqfile==NULL)"));
 	
 	if ( !m_reqfile || file != m_reqfile ){
-		if (m_reqfile==NULL) {
-			throw wxString(_("ERROR: Wrong file ID (ProcessFileStatus; m_reqfile==NULL)"));
+		if (!m_reqfile) {
+			throw strReqFileNull;
 		}
 		throw wxString(_("ERROR: Wrong file ID (ProcessFileStatus; m_reqfile!=file)"));
 	}
@@ -303,11 +304,16 @@ void CUpDownClient::ProcessFileStatus(bool bUdpPacket, const CSafeMemFile* data,
 	}
 	else
 	{
+		// Somehow this happened.
+		if (!m_reqfile) {
+			throw strReqFileNull;
+		}
 		if (m_reqfile->GetED2KPartCount() != nED2KPartCount)
 		{
 			wxString strError;
-			strError.Printf(_("ProcessFileStatus - wrong part number recv=%u  expected=%u  "), nED2KPartCount, m_reqfile->GetED2KPartCount());
-			strError +=  EncodeBase16(m_reqfile->GetFileHash(), 16);
+			strError << _("ProcessFileStatus - wrong part number recv=") << nED2KPartCount <<
+				_("  expected=") << m_reqfile->GetED2KPartCount() << wxT(" ") <<
+				EncodeBase16(m_reqfile->GetFileHash(), 16);
 			m_nPartCount = 0;
 			throw strError;
 		}
