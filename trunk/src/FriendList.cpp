0387ce72dea548b8a451bcb11f2eed78b33effb3
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-03-05 12:08:50 +0000

    Ahem... Woops?

---
+++ trunk/src/FriendList.cpp
@@ -0,0 +1,226 @@
+// This file is part of the aMule Project
+//
+// Copyright (c) 2003-2004 aMule Project ( http://www.amule-project.net )
+// Copyright (C) 2002 Merkur ( merkur-@users.sourceforge.net / http://www.emule-project.net )
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either
+//version 2 of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
+#if defined(__GNUG__) && !defined(NO_GCC_PRAGMA)
+#pragma implementation "FriendList.h"
+#endif
+
+#include <wx/defs.h>		// Needed before any other wx/*.h
+#include <wx/intl.h>		// Needed for _
+
+#include "FriendList.h" // Interface
+
+#include "amule.h"			// Needed for theApp: let it first or fail under win32
+#include "ClientList.h"		// Needed for CClientList
+#include "OPCodes.h"		// Needed for MP_DETAIL
+#include "updownclient.h"	// Needed for CUpDownClient
+#include "Friend.h"		// Needed for CFriend
+#include "ArchSpecific.h"
+#include "OtherFunctions.h"
+#include "SafeFile.h"
+#include "Logger.h"
+
+CFriendList::CFriendList()
+{ 
+	LoadList();
+}
+
+CFriendList::~CFriendList()
+{
+	SaveList();
+}
+
+
+void CFriendList::AddFriend(CFriend* toadd)
+{
+	m_FriendList.push_back(toadd);
+	SaveList();
+}
+
+
+void CFriendList::AddFriend(const CMD4Hash& userhash, uint32 lastSeen, uint32 lastUsedIP, uint32 lastUsedPort, uint32 lastChatted, const wxString& name)
+{
+	CFriend* NewFriend = new CFriend( userhash, lastSeen, lastUsedIP, lastUsedPort, lastChatted, name);
+
+	AddFriend( NewFriend );
+}
+
+
+void CFriendList::AddFriend(CUpDownClient* toadd)
+{
+	if ( toadd->IsFriend() ) {
+		return;
+	}
+	
+	CFriend* NewFriend = new CFriend( toadd );
+	toadd->m_Friend = NewFriend;
+	
+	AddFriend( NewFriend );
+}
+
+
+void CFriendList::RemoveFriend(const CMD4Hash& userhash, uint32 lastUsedIP, uint32 lastUsedPort)
+{
+	CFriend* toremove = FindFriend(userhash, lastUsedIP, lastUsedPort);
+	if (toremove) {
+		if ( toremove->GetLinkedClient() ){
+			toremove->GetLinkedClient()->SetFriendSlot(false);
+			toremove->GetLinkedClient()->m_Friend = NULL;
+			toremove->UnLinkClient();
+		}
+
+		m_FriendList.remove(toremove);
+	
+		SaveList();
+	
+		delete toremove;
+	}
+}
+
+void CFriendList::LoadList()
+{
+  	wxString metfile = theApp.ConfigDir + wxT("emfriends.met"); 
+	
+	if ( !wxFileExists(metfile) ) {
+		return;
+	}
+	
+	CSafeFile file;
+	try {
+		if ( file.Open(metfile) ) {
+			if ( file.ReadUInt8() /*header*/ == MET_HEADER ) {
+				uint32 nRecordsNumber = file.ReadUInt32();
+				for (uint32 i = 0; i < nRecordsNumber; i++) {
+					CFriend* Record = new CFriend();
+					Record->LoadFromFile(&file);
+					m_FriendList.push_back(Record);
+				}				
+			}
+			file.Close();
+		} else if ( wxFileExists(metfile) ) {
+			AddLogLineM(false, _("Failed to open friendlist file 'emfriends.met' for reading!\n"));
+		}
+	} catch (...) {
+		AddLogLineM(false, _("Failed to read corrupted friendlist file 'emfriends.met'!\n"));
+	}
+	
+}
+
+
+void CFriendList::SaveList()
+{
+	CSafeFile file;
+	if( file.Create(theApp.ConfigDir + wxT("emfriends.met"), true) ) {
+		uint8 header = MET_HEADER;
+		file.WriteUInt8(header);
+		
+		file.WriteUInt32(m_FriendList.size());
+		
+		for(FriendList::iterator it = m_FriendList.begin(); it != m_FriendList.end(); ++it) {
+			(*it)->WriteToFile(&file);
+		}
+		
+		file.Close();	
+	} else {
+		AddLogLineM(false, _("Failed to open friendlist file 'emfriends.met' for writing!\n"));
+	}
+}
+
+
+CFriend* CFriendList::FindFriend(const CMD4Hash& userhash, uint32 dwIP, uint16 nPort) 
+{
+	
+	for(FriendList::iterator it = m_FriendList.begin(); it != m_FriendList.end(); ++it) {
+		
+		CFriend* cur_friend = *it;
+		// to avoid that unwanted clients become a friend, we have to distinguish between friends with
+		// a userhash and of friends which are identified by IP+port only.
+		if ( !userhash.IsEmpty() && cur_friend->HasHash() ) {
+			// check for a friend which has the same userhash as the specified one
+			if (cur_friend->GetUserHash() == userhash) {
+				return cur_friend;
+			}
+		} else if (cur_friend->GetIP() == dwIP && cur_friend->GetPort() == nPort) {
+				return cur_friend;
+		}
+	}
+
+	return NULL;
+}
+
+
+bool CFriendList::IsAlreadyFriend( uint32 dwLastUsedIP, uint32 nLastUsedPort )
+{
+	return FindFriend( NULL, dwLastUsedIP, nLastUsedPort );
+}
+
+
+void CFriendList::RemoveAllFriendSlots()
+{
+	for(FriendList::iterator it = m_FriendList.begin(); it != m_FriendList.end(); ++it) {		
+		CFriend* cur_friend = *it;
+		if ( cur_friend->GetLinkedClient() ) {
+				cur_friend->GetLinkedClient()->SetFriendSlot(false);
+		}
+	}
+}
+
+void	CFriendList::RequestSharedFileList(const CMD4Hash& userhash, uint32 dwIP, uint16 nPort) {
+	CFriend* cur_friend = FindFriend(userhash, dwIP, nPort);
+	if (cur_friend) {
+		CUpDownClient* client = cur_friend->GetLinkedClient();
+		if (!client) {
+			client = new CUpDownClient(cur_friend->GetPort(), cur_friend->GetIP(), 0, 0, 0);
+			client->SetUserName(cur_friend->GetName());
+			theApp.clientlist->AddClient(client);
+		}
+		client->RequestSharedFileList();
+	}
+}
+
+void CFriendList::ToogleFriendSlot(const CMD4Hash& userhash, uint32 dwIP, uint16 nPort) {
+	CFriend* cur_friend = FindFriend(userhash, dwIP, nPort);
+	if (cur_friend && cur_friend->GetLinkedClient()) {
+		bool IsAlready = cur_friend->GetLinkedClient()->GetFriendSlot();
+		RemoveAllFriendSlots();
+		if( !IsAlready ) {
+			cur_friend->GetLinkedClient()->SetFriendSlot(true);
+		}
+	}
+}
+
+void CFriendList::StartChatSession(const CMD4Hash& userhash, uint32 dwIP, uint16 nPort) {
+	CFriend* friend_client = FindFriend(userhash, dwIP, nPort);
+	if (friend_client) {
+		CUpDownClient* client = friend_client->GetLinkedClient();
+		if (!client) {
+			client = new CUpDownClient(friend_client->GetPort(), friend_client->GetIP(), 0, 0, 0);
+			client->SetUserName(friend_client->GetName());
+			theApp.clientlist->AddClient(client);
+			friend_client->LinkClient(client);			
+		}
+	}
+}
+	
+void CFriendList::UpdateFriendName(const CMD4Hash& userhash, const wxString& name, uint32 dwIP, uint16 nPort) {
+	CFriend* friend_client = FindFriend(userhash, dwIP, nPort);
+	friend_client->SetName(name);
+	SaveList();
+}
---
+++ trunk/src/FriendList.h
@@ -0,0 +1,65 @@
+// This file is part of the aMule Project
+//
+// Copyright (c) 2003-2004 aMule Project ( http://www.amule-project.net )
+// Copyright (C) 2002 Merkur ( merkur-@users.sourceforge.net / http://www.emule-project.net )
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either
+//version 2 of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+#ifndef FRIENDLIST_H
+#define FRIENDLIST_H
+
+#if defined(__GNUG__) && !defined(NO_GCC_PRAGMA)
+#pragma interface "FriendList.h"
+#endif
+
+#include <list>
+
+#include "Types.h"	// Needed for uint32
+
+class wxString;
+class CFriend;
+class CUpDownClient;
+class CMD4Hash;
+
+typedef std::list<CFriend*> FriendList;
+
+class CFriendList 
+{
+public:
+	CFriendList();
+	~CFriendList();
+	
+	bool		IsAlreadyFriend( uint32 dwLastUsedIP, uint32 nLastUsedPort ); 
+	void		SaveList();
+	void		LoadList();
+	CFriend*	FindFriend( const CMD4Hash& userhash, uint32 dwIP, uint16 nPort);	
+	void 		AddFriend(CFriend* toadd);
+	void		AddFriend( CUpDownClient* toadd );
+	void		AddFriend( const CMD4Hash& userhash, uint32 lastSeen, uint32 lastUsedIP, uint32 lastUsedPort, uint32 lastChatted, const wxString& name);
+	void		RemoveFriend( const CMD4Hash& userhash, uint32 lastUsedIP, uint32 lastUsedPort);
+	void		RequestSharedFileList(const CMD4Hash& userhash, uint32 dwIP, uint16 nPort);
+	void		UpdateFriendName(const CMD4Hash& userhash, const wxString& name, uint32 dwIP, uint16 nPort);
+
+	void		ToogleFriendSlot(const CMD4Hash& userhash, uint32 dwIP, uint16 nPort);
+	void		StartChatSession(const CMD4Hash& userhash, uint32 dwIP, uint16 nPort);
+	void		RemoveAllFriendSlots();
+
+//private:
+
+	#warning THIS MUST BE MADE PRIVATE AFTER EC IS CODED
+	FriendList m_FriendList;
+};
+
+#endif
