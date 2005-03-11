427692d9798c6a630c4557267f101cc945944d0b
xaignar <xaignar@amule.org>
2005-03-11 19:09:49 +0000

    Adding missing GPL noticies.

--- src/Format.h
+++ src/Format.h
@@ -1,3 +1,23 @@
+//
+// This file is part of the aMule Project.
+//
+// Copyright (c) 2005 aMule Team ( http://www.amule-project.net )
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+
 #ifndef FORMAT_H
 #define FORMAT_H
 
