7496c6e301e3c60f2d319872c1512de5e388bdc0
xaignar <xaignar@amule.org>
2005-04-25 23:10:08 +0000

    Workaround for fonts getting changed when the log-view is cleared on wxMac. Please test.

--- src/MuleTextCtrl.cpp
+++ src/MuleTextCtrl.cpp
@@ -135,3 +135,19 @@ void CMuleTextCtrl::OnClear( wxCommandEvent& WXUNUSED(evt) )
 	Clear();
 }
 
+
+#ifdef __WXMAC__
+#warning Remove this when wxMAC has been fixed.
+// https://sourceforge.net/tracker/?func=detail&atid=109863&aid=1189859&group_id=9863
+void CMuleTextCtrl::Clear()
+{
+	if (IsMultiLine()) {
+		wxFont font = GetFont();
+		wxTextCtrl::Clear();
+		SetFont(font);
+	} else {
+		wxTextCtrl::Clear();
+	}
+}
+#endif
+
