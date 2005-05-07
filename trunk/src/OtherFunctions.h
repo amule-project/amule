4cbab4c5f182ff1f774beb65ddac1516c6ad0e22
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-05-07 20:11:01 +0000

    Needed for my Kad stuff.

--- trunk/src/OtherFunctions.h
+++ trunk/src/OtherFunctions.h
@@ -49,6 +49,15 @@
 #include "ArchSpecific.h"
 #include "OtherStructs.h" // for Gap_Struct
 
+#define	SEC2MS(sec)		((sec)*1000)
+#define	MIN2MS(min)		SEC2MS((min)*60)
+#define	HR2MS(hr)		MIN2MS((hr)*60)
+#define	DAY2MS(day)		HR2MS((day)*24)
+#define	SEC(sec)		(sec)
+#define	MIN2S(min)		((min)*60)
+#define	HR2S(hr)		MIN2S((hr)*60)
+#define	DAY2S(day)		HR2S((day)*24)
+
 namespace otherfunctions {
 	
 /**
