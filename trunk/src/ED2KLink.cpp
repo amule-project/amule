541233725eb15dd6960b2aee13fcafbdad96e21f
xaignar <xaignar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-07-05 16:30:07 +0000

    Change the commented out asserts into an if, so that an exception will only be thrown on bad hashes, not always.
    On a related note, please do not leave commented out
    code behind, it just increases the mess.

--- trunk/src/ED2KLink.cpp
+++ trunk/src/ED2KLink.cpp
@@ -286,9 +286,10 @@ CED2KFileLink::CED2KFileLink( const wxString& name, const wxString& size, const 
 			m_bAICHHashValid = otherfunctions::DecodeBase32(
 					strHash, CAICHHash::GetHashSize(), m_AICHHash.GetRawHash()) == 
 				CAICHHash::GetHashSize();
-			//wxASSERT(m_bAICHHashValid);
-			//wxASSERT(m_AICHHash.GetString().CmpNoCase(strHash) == 0);
-			throw wxString(wxT("Invalid hash"));
+			
+			if (!m_bAICHHashValid || m_AICHHash.GetString() != strHash) {
+				throw wxString(wxT("Invalid hash"));
+			}
 		}
 	}
 }
