48a94298c3c4f49f3cb405dd97e5f2e8dff65113
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-07-10 08:01:16 +0000

    Actually, not even that one

--- trunk/src/amule-remote-gui.h
+++ trunk/src/amule-remote-gui.h
@@ -562,7 +562,7 @@ class CSearchListRem : public CRemoteContainer<CSearchFile, CMD4Hash, CEC_Search
 		// Actions
 		//
 
-		bool StartNewSearch(long nSearchID, bool global_search, wxString &searchString, 
+		bool StartNewSearch(uint32* nSearchID, bool global_search, wxString &searchString, 
 			wxString& typeText, wxString &extension, uint32 min, uint32 max, uint32 availability);
 			
 		void StopGlobalSearch();
