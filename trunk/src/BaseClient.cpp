31c9e78e76009f33570d1d5ecef3ca57faab3bb9
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-07-13 04:20:31 +0000

    Kad firewalled sources are now contacted properly.

--- trunk/src/BaseClient.cpp
+++ trunk/src/BaseClient.cpp
@@ -1431,22 +1431,22 @@ bool CUpDownClient::TryToConnect(bool bIgnoreMaxCon)
 				
                	 if( GetDownloadState() == DS_WAITCALLBACK ) {
 					if( GetBuddyIP() && GetBuddyPort()) {
+						printf("This lowid user has a buddy (%s), asking him\n",(const char*)unicode2char(Uint32_16toStringIP_Port(GetBuddyIP(),GetBuddyPort())));
 						CSafeMemFile bio(34);
-						Kademlia::CUInt128 buddy(GetBuddyID());
-						bio.WriteUInt128(&buddy);
-						Kademlia::CUInt128 file(m_reqfile->GetFileHash());
-						bio.WriteUInt128(&file);
+						bio.WriteUInt128(Kademlia::CUInt128(GetBuddyID()));
+						bio.WriteUInt128(Kademlia::CUInt128(m_reqfile->GetFileHash()));
 						bio.WriteUInt16(thePrefs::GetPort());
 						CPacket* packet = new CPacket(&bio, OP_KADEMLIAHEADER, KADEMLIA_CALLBACK_REQ);
 						theApp.clientudp->SendPacket(packet, GetBuddyIP(), GetBuddyPort());
+						AddDebugLogLineM(false,logLocalClient, wxString::Format(wxT("KADEMLIA_CALLBACK_REQ (%i)"),packet->GetPacketSize()));						
 						theApp.statistics->AddUpDataOverheadKad(packet->GetRealPacketSize());
 						SetDownloadState(DS_WAITCALLBACKKAD);
 					} else {
+						printf("Searching buddy for lowid connection\n");
 						//Create search to find buddy.
 						Kademlia::CSearch *findSource = new Kademlia::CSearch;
 						findSource->setSearchTypes(Kademlia::CSearch::FINDSOURCE);
-						Kademlia::CUInt128 ID(GetBuddyID());
-						findSource->setTargetID(ID);
+						findSource->setTargetID(GetBuddyID());
 						findSource->addFileID(Kademlia::CUInt128(m_reqfile->GetFileHash()));
 						if(Kademlia::CSearchManager::startSearch(findSource)) {
 							//Started lookup..
@@ -1513,6 +1513,7 @@ void CUpDownClient::ConnectionEstablished()
 	switch(GetDownloadState()) {
 		case DS_CONNECTING:
 		case DS_WAITCALLBACK:
+		case DS_WAITCALLBACKKAD:
 			m_bReaskPending = false;
 			SetDownloadState(DS_CONNECTED);
 			SendFileRequest();
