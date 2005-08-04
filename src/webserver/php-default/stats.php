1f93b043b78338c60ae90b4b04a3e202611dd8d3
lfroen <lfroen@amule.org>
2005-08-04 16:46:41 +0000

    Adding page for stats and logout icon

---
+++ src/webserver/php-default/stats.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>amule control page</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-color: #6699CC;
+}
+.style1 {font-family: Arial, Helvetica, sans-serif}
+-->
+</style>
+</head>
+<table width="800" border="0" cellpadding="0" cellspacing="0">
+  <!--DWLayoutTable-->
+  <tr>
+    <td width="21" height="18"></td>
+    <td width="22"></td>
+    <td width="152"></td>
+    <td width="16"></td>
+    <td width="140"></td>
+    <td width="449"></td>
+  </tr>
+  <tr>
+    <td height="20">&nbsp;</td>
+    <td valign="top"><img src="connect.gif" width="16" height="16"></td>
+    <td valign="top">Connection status </td>
+    <td>&nbsp;</td>
+    <td valign="top"><?php ?></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="15"></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+</html>
