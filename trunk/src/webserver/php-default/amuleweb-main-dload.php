43599e3eb6792108f226a1bda11fa3b0e888810b
lfroen <lfroen@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-08-06 04:56:36 +0000

    One more print

--- trunk/src/webserver/php-default/amuleweb-main-dload.php
+++ trunk/src/webserver/php-default/amuleweb-main-dload.php
@@ -14,16 +14,93 @@ body {
 	background-color: #003399;
 }
 -->
-</style></head>
+</style>
+<script language="JavaScript" type="text/JavaScript">
+<!--
+function MM_preloadImages() { //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_nbGroup(event, grpName) { //v6.0
+  var i,img,nbArr,args=MM_nbGroup.arguments;
+  if (event == "init" && args.length > 2) {
+    if ((img = MM_findObj(args[2])) != null && !img.MM_init) {
+      img.MM_init = true; img.MM_up = args[3]; img.MM_dn = img.src;
+      if ((nbArr = document[grpName]) == null) nbArr = document[grpName] = new Array();
+      nbArr[nbArr.length] = img;
+      for (i=4; i < args.length-1; i+=2) if ((img = MM_findObj(args[i])) != null) {
+        if (!img.MM_up) img.MM_up = img.src;
+        img.src = img.MM_dn = args[i+1];
+        nbArr[nbArr.length] = img;
+    } }
+  } else if (event == "over") {
+    document.MM_nbOver = nbArr = new Array();
+    for (i=1; i < args.length-1; i+=3) if ((img = MM_findObj(args[i])) != null) {
+      if (!img.MM_up) img.MM_up = img.src;
+      img.src = (img.MM_dn && args[i+2]) ? args[i+2] : ((args[i+1])? args[i+1] : img.MM_up);
+      nbArr[nbArr.length] = img;
+    }
+  } else if (event == "out" ) {
+    for (i=0; i < document.MM_nbOver.length; i++) {
+      img = document.MM_nbOver[i]; img.src = (img.MM_dn) ? img.MM_dn : img.MM_up; }
+  } else if (event == "down") {
+    nbArr = document[grpName];
+    if (nbArr)
+      for (i=0; i < nbArr.length; i++) { img=nbArr[i]; img.src = img.MM_up; img.MM_dn = 0; }
+    document[grpName] = nbArr = new Array();
+    for (i=2; i < args.length-1; i+=2) if ((img = MM_findObj(args[i])) != null) {
+      if (!img.MM_up) img.MM_up = img.src;
+      img.src = img.MM_dn = (args[i+1])? args[i+1] : img.MM_up;
+      nbArr[nbArr.length] = img;
+  } }
+}
+//-->
+</script>
+</head>
+<script language="JavaScript" type="text/JavaScript">
+function formDownloadSubmit()
+{
+	var frm=document.forms.mainform
+	frm.command.value="download"
+	frm.submit()
+}
+
+</script>
 
 <body>
 <form name="mainform" action="amuleweb-main-dload.php" method="post">
 <table width="100%"  border="1" bgcolor="#0099CC">
   <tr>
-    <td height="20"><input type="submit" name="Submit" value="Submit"></td>
+    <td width="96" height="20"><input type="hidden" name="command"></td>
+    <td width="468"><table border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td><a href="javascript:;" target="mainFrame" onClick="MM_nbGroup('down','group1','pause','',1)" onMouseOver="MM_nbGroup('over','pause','','',1)" onMouseOut="MM_nbGroup('out')"><img name="pause" src="pause.jpeg" border="0" alt="" onLoad=""></a></td>
+        <td><a href="javascript:;" target="mainFrame" onClick="MM_nbGroup('down','group1','resume','',1)" onMouseOver="MM_nbGroup('over','resume','','',1)" onMouseOut="MM_nbGroup('out')"><img name="resume" src="resume.jpeg" border="0" alt="" onLoad=""></a></td>
+        <td><a href="javascript:;" target="mainFrame" onClick="MM_nbGroup('down','group1','up','',1)" onMouseOver="MM_nbGroup('over','up','','',1)" onMouseOut="MM_nbGroup('out')"><img name="up" src="up.jpeg" border="0" alt="" onLoad=""></a></td>
+        <td><a href="javascript:;" target="mainFrame" onClick="MM_nbGroup('down','group1','down','',1)" onMouseOver="MM_nbGroup('over','down','','',1)" onMouseOut="MM_nbGroup('out')"><img src="down.jpeg" alt="" name="down" width="50" height="20" border="0" onload=""></a></td>
+        <td><a href="javascript:;" target="mainFrame" onClick="MM_nbGroup('down','group1','delete','',1)" onMouseOver="MM_nbGroup('over','delete','','',1)" onMouseOut="MM_nbGroup('out')"><img src="delete.jpeg" alt="" name="delete" width="50" height="20" border="0" onload=""></a></td>
+      </tr>
+    </table></td>
+    <td width="273">
+    	<select name="status">
+    		<option selected>All</option>
+      		<option>Waiting</option>
+      		<option>Paused</option>
+      		<option>Downloading</option>
+    	</select></td>
   </tr>
   <tr>
-    <td><table width="100%"  border="0">
+    <td colspan="3"><table width="100%"  border="0">
       <tr>
         <th width="22" scope="col">&nbsp;</th>
         <th width="213" scope="col"><div align="left"><a href="amuleweb-main-dload.php?sort=name" target="mainFrame">Filename</a></div></th>
@@ -101,6 +178,19 @@ body {
 			return $result;
 		}
 
+		//
+		// perform command before processing content
+		//
+		var_dump($HTTP_GET_VARS);
+		if ( $HTTP_GET_VARS["command"] != "") {
+			//amule_do_download_cmd($HTTP_GET_VARS["command"]);
+			foreach ( $HTTP_GET_VARS as $name => $val) {
+				// this is file checkboxes
+				if ( (strlen($name) == 32) and ($val == "on") ) {
+					var_dump($name);
+				}
+			}
+		}
 		$downloads = amule_load_vars("downloads");
 
 		$sort_order = $HTTP_GET_VARS["sort"];
@@ -126,7 +216,7 @@ body {
 
 			echo "<td>", '<input type="checkbox" name="', $file->hash, '" >', "</td>";
 
-			echo "<td>", $file->name, "</td>";
+			echo "<td nowrap>", $file->name, "</td>";
 
 			echo "<td>", $file->progress, "</td>";
 			
