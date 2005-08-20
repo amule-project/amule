d7c03dfce96ddebb589086e166bb23f27a764e16
lfroen <lfroen@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-08-20 16:34:20 +0000

    PHP version of page footer

---
+++ trunk/src/webserver/php-default/footer.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>amule-frame-bottom</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+body {
+	background-color: #3399FF;
+}
+-->
+</style></head>
+
+<body>
+<table width="100%" border="0" cellpadding="0" cellspacing="0">
+  <!--DWLayoutTable-->
+  <tr>
+    <td width="70" height="41">&nbsp;</td>
+    <td width="100%" valign="top"><form name="form1" method="post" action="">
+        <input type="submit" name="Submit" value="Download link">
+        <input name="ed2klink" type="text" id="ed2klink" size="100">
+        <img src="arrow-r.png" width="42" height="23"> 
+        <select name="selectcat" id="selectcat">
+        </select>
+        </form></td>
+    <td width="29">&nbsp;</td>
+  </tr>
+  <tr>
+    <td height="1"></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+  </tr>
+</table>
+</body>
+</html>
