f5cdd19f54dd4658030c7e3cc7d0bb3e44f98c09
kry <kry@amule.org>
2005-08-24 18:08:20 +0000

    fixes http://forum.amule.org/thread.php?threadid=7173&sid=

--- src/MuleDebug.cpp
+++ src/MuleDebug.cpp
@@ -30,6 +30,8 @@
 #include <exception>			// Needed for std::exception
 #include <cxxabi.h>				// Needed for __cxxabiv1::
 #include <csignal>				// Needed for raise()
+#include <stdio.h>
+#include <wchar.h>				// Needed for fwprintf
 
 #include <wx/string.h>
 
