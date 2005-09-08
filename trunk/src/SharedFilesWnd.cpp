7d66a5387ec598297bf05e61aeec02fbc059907c
phoenix <phoenix@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-09-08 02:21:15 +0000

    Fixes the Shared Files window bug reported by Mr Faber. The problem is that aMule is now a multithreaded application, and sometimes another thread pulls the carpet under our feet.

--- trunk/src/SharedFilesWnd.cpp
+++ trunk/src/SharedFilesWnd.cpp
@@ -69,27 +69,26 @@ CSharedFilesWnd::~CSharedFilesWnd()
 
 void CSharedFilesWnd::SelectionUpdated()
 {
-	m_bar_requests->SetRange( theApp.knownfiles->requested );
-	m_bar_accepted->SetRange( theApp.knownfiles->accepted );
-	m_bar_transfer->SetRange( theApp.knownfiles->transfered / 1024 );
+	uint64 lTransfered = theApp.knownfiles->transfered;
+	uint32 lAccepted = theApp.knownfiles->accepted;
+	uint32 lRequested = theApp.knownfiles->requested;
+	m_bar_requests->SetRange( lRequested );
+	m_bar_accepted->SetRange( lAccepted );
+	m_bar_transfer->SetRange( lTransfered / 1024 );
 	
 	if ( !sharedfilesctrl->GetSelectedItemCount() ) {
 		// Requests
 		m_bar_requests->SetValue( 0 );
-	
 		CastChild(IDC_SREQUESTED, wxStaticText)->SetLabel( wxT("-") );
 		CastChild(IDC_SREQUESTED2, wxStaticText)->SetLabel( wxT("-") );
 
 		// Accepted requets
 		m_bar_accepted->SetValue( 0 );
-	
 		CastChild(IDC_SACCEPTED, wxStaticText)->SetLabel( wxT("-") );
 		CastChild(IDC_SACCEPTED2, wxStaticText)->SetLabel( wxT("-") );
 
-
 		// Transferred
 		m_bar_transfer->SetValue( 0 );
-	
 		CastChild(IDC_STRANSFERED, wxStaticText)->SetLabel( wxT("-") );
 		CastChild(IDC_STRANSFERED2, wxStaticText)->SetLabel( wxT("-") );
 	} else {
@@ -97,51 +96,44 @@ void CSharedFilesWnd::SelectionUpdated()
 		uint32 session_requests = 0;
 		uint32 session_accepted = 0;
 		uint64 session_transfered = 0;
-		
 		uint32 all_requests = 0;
 		uint32 all_accepted = 0;
 		uint64 all_transfered = 0;
 		
 		long index = sharedfilesctrl->GetNextItem( -1, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED );
-
 		while ( index > -1 ) {
 			if ( sharedfilesctrl->GetItemState( index, wxLIST_STATE_SELECTED ) ) {
 				CKnownFile* file = (CKnownFile*)sharedfilesctrl->GetItemData( index );
 			
-				session_requests	+= file->statistic.GetRequests();
-				session_accepted	+= file->statistic.GetAccepts();
-				session_transfered	+= file->statistic.GetTransfered();
+				session_requests   += file->statistic.GetRequests();
+				session_accepted   += file->statistic.GetAccepts();
+				session_transfered += file->statistic.GetTransfered();
 		
-				all_requests	+= file->statistic.GetAllTimeRequests();
-				all_accepted	+= file->statistic.GetAllTimeAccepts();
-				all_transfered	+= file->statistic.GetAllTimeTransfered();
+				all_requests   += file->statistic.GetAllTimeRequests();
+				all_accepted   += file->statistic.GetAllTimeAccepts();
+				all_transfered += file->statistic.GetAllTimeTransfered();
 			}
-			
 			index = sharedfilesctrl->GetNextItem( index, wxLIST_NEXT_ALL, wxLIST_STATE_SELECTED );
 		};
 		
-		
 		// Requests
+		session_requests = session_requests > lRequested ? lRequested : session_requests;
 		m_bar_requests->SetValue( session_requests );
-	
 		CastChild(IDC_SREQUESTED, wxStaticText)->SetLabel( wxString::Format(wxT("%u"), session_requests ) );
 		CastChild(IDC_SREQUESTED2, wxStaticText)->SetLabel( wxString::Format(wxT("%u"), all_requests ) );
-
 	
 		// Accepted requets
+		session_accepted = session_accepted > lAccepted ? lAccepted : session_accepted;
 		m_bar_accepted->SetValue( session_accepted );
-	
 		CastChild(IDC_SACCEPTED, wxStaticText)->SetLabel( wxString::Format(wxT("%u"), session_accepted ) );
 		CastChild(IDC_SACCEPTED2, wxStaticText)->SetLabel( wxString::Format(wxT("%u"), all_accepted ) );
 
-
 		// Transferred
+		session_transfered = session_transfered > lTransfered ? lTransfered : session_transfered;
 		m_bar_transfer->SetValue( session_transfered / 1024 );
-	
 		CastChild(IDC_STRANSFERED, wxStaticText)->SetLabel( CastItoXBytes( session_transfered ) );
 		CastChild(IDC_STRANSFERED2, wxStaticText)->SetLabel( CastItoXBytes( all_transfered ) );
 	}
-	
 	Layout();
 }
 
