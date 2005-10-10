0d9ead3344e025a1eac032be1b459836de9daccd
lfroen <lfroen@amule.org>
2005-10-10 04:44:53 +0000

    Add statistics tree generation to php code.

--- src/webserver/php-default/amuleweb-main-stats.php
+++ src/webserver/php-default/amuleweb-main-stats.php
@@ -5,6 +5,14 @@
 <title>amule preferences page</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf8">
 <style type="text/css">
+.trigger{
+	cursor: pointer;
+	cursor: hand;
+}
+.branch{
+	display: none;
+	margin-left: 16px;
+}
 <!--
 body {
 	margin-left: 0px;
@@ -17,42 +25,74 @@ body {
 </style>
 </head>
 <script language="JavaScript" type="text/JavaScript">
+var openImg = new Image();
+openImg.src = "tree-open.gif";
+var closedImg = new Image();
+closedImg.src = "tree-closed.gif";
+
+function showBranch(branch){
+	var objBranch = document.getElementById(branch).style;
+	if(objBranch.display=="block")
+		objBranch.display="none";
+	else
+		objBranch.display="block";
+}
+
+function swapFolder(img){
+	objImg = document.getElementById(img);
+	if(objImg.src.indexOf('tree-closed.gif')>-1)
+		objImg.src = openImg.src;
+	else
+		objImg.src = closedImg.src;
+}
 
 </script>
 <?php
 	amule_load_vars("stats_graph");
-	
-	$stattree = amule_load_vars("stats_tree");
-	var_dump($stattree);
 ?>
-<body onload="init_data();" >
+<body>
 <form name="mainform" action="amuleweb-main-prefs.php" method="post">
 <table width="800" border="0" cellpadding="0" cellspacing="0">
   <!--DWLayoutTable-->
   <tr>
     <td width="52" height="10"><!--DWLayoutEmptyCell-->&nbsp;</td>
-    <td width="383">&nbsp;</td>
-    <td width="365">&nbsp;</td>
+    <td colspan="2">&nbsp;</td>
+    <td colspan="2">&nbsp;</td>
   </tr>
   <tr valign="top">
     <td height="58">&nbsp;</td>
-    <td><img src="amule_stats_download.png" width="286" height="200" border="0" alt="" title="" /></td>
-    <td><img src="amule_stats_upload.png" width="286" height="200" border="0" alt="" title="" /></td>
+    <td colspan="2"><img src="amule_stats_download.png" width="286" height="200" border="0" alt="" title="" /></td>
+    <td colspan="2"><img src="amule_stats_upload.png" width="286" height="200" border="0" alt="" title="" /></td>
+  </tr>
+  <tr valign="top">
+    <td height="20">&nbsp;</td>
+    <td width="285"><div align="center">Downloads</div></td>
+    <td width="98">&nbsp;</td>
+    <td width="286"><div align="center">Uploads</div></td>
+    <td width="79">&nbsp;</td>
   </tr>
   <tr valign="top">
     <td height="35">&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
+    <td colspan="2">&nbsp;</td>
+    <td colspan="2">&nbsp;</td>
   </tr>
   <tr valign="top">
     <td height="62">&nbsp;</td>
-    <td><img src="amule_stats_conncount.png" width="286" height="200" border="0" alt="" title="" /></td>
-    <td>&nbsp;</td>
+    <td colspan="2"><img src="amule_stats_conncount.png" width="286" height="200" border="0" alt="" title="" /></td>
+    <td colspan="2">
+		<iframe name="stats" src="stats_tree.php" scrolling="yes" frameborder="1"></iframe>
+    </td>
+  </tr>
+  <tr valign="top">
+    <td height="20">&nbsp;</td>
+    <td><div align="center">Number of connections </div></td>
+    <td><!--DWLayoutEmptyCell-->&nbsp;</td>
+    <td colspan="2"><!--DWLayoutEmptyCell-->&nbsp;</td>
   </tr>
   <tr valign="top">
     <td height="108">&nbsp;</td>
-    <td><!--DWLayoutEmptyCell-->&nbsp;</td>
-    <td>&nbsp;</td>
+    <td colspan="2"><!--DWLayoutEmptyCell-->&nbsp;</td>
+    <td colspan="2">&nbsp;</td>
   </tr>
 </table>
 </form>
---
+++ src/webserver/php-default/stats_tree.php
@@ -0,0 +1,98 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>amule preferences page</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf8">
+<style type="text/css">
+.trigger{
+	cursor: pointer;
+	cursor: hand;
+}
+.branch{
+	display: none;
+	margin-left: 16px;
+}
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+	background-color: #003399;
+}
+-->
+</style>
+</head>
+<script language="JavaScript" type="text/JavaScript">
+var openImg = new Image();
+openImg.src = "tree-open.gif";
+var closedImg = new Image();
+closedImg.src = "tree-closed.gif";
+
+function showBranch(branch){
+	var objBranch = document.getElementById(branch).style;
+	if(objBranch.display=="block")
+		objBranch.display="none";
+	else
+		objBranch.display="block";
+}
+
+function swapFolder(img){
+	objImg = document.getElementById(img);
+	if(objImg.src.indexOf('tree-closed.gif')>-1)
+		objImg.src = openImg.src;
+	else
+		objImg.src = closedImg.src;
+}
+
+</script>
+<body>
+<?php
+
+function print_ident($i)
+{
+	while($i != 0) {
+		echo "\t";
+		$i--;
+	}
+}
+
+function print_item($it, $ident)
+{
+	print_ident($ident);
+	echo "<img src=\"tree-leaf.gif\">", $it, "<br>\n";
+}
+
+function print_folder($key, &$arr, $ident)
+{
+	print_ident($ident);
+	echo "<span class=\"trigger\" onClick=\"showBranch('br_",
+		$key, "');swapFolder('fl_", $key, "')\">\n";
+	print_ident($ident+1);
+	echo "<img src=\"tree-closed.gif\" border=\"0\" id=\"fl_", $key, "\">\n";
+	print_ident($ident+1);
+	echo $key, "<br>\n";
+	print_ident($ident);
+	echo "</span>\n";
+	print_ident($ident);
+	echo "<span class=\"branch\" id=\"br_", $key, "\">\n";
+
+	foreach ($arr as $k => $v) {
+		if ( count(&$v) ) {
+			print_folder($k, $v, $ident+1);
+		} else {
+			print_item($k, $ident+1);
+		}
+	}
+
+	print_ident($ident);
+	echo "</span>\n";
+}
+
+	$stattree = amule_load_vars("stats_tree");
+	//var_dump($stattree);
+	print_folder("Stats", $stattree["Statistics"], 1);
+?>
+</body>
+</html>
