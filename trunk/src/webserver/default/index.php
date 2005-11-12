9faeec27d099355797d4c27d9f05c291358d0ead
lfroen <lfroen@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-11-12 19:35:26 +0000

    Fix links

--- trunk/src/webserver/default/index.php
+++ trunk/src/webserver/default/index.php
@@ -142,7 +142,7 @@ border-color: black;
   			Shared Files
   		</a>
   	<td align="center" class="tabs" width="110">
-		<a href="statistics.php">
+		<a href="stat_tree.php">
 			<img src="cp_stats.gif"><br>
   			Statistics</a>
   		<font color="#000000">|</font>
