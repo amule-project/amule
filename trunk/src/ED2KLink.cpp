9ba72c5f4db2d81cdceaa4f7a1c336248b64f5c6
stefanero <stefanero@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2005-11-16 19:39:50 +0000

    little fix

--- trunk/src/ED2KLink.cpp
+++ trunk/src/ED2KLink.cpp
@@ -23,6 +23,7 @@
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA, 02111-1307, USA
 //
 
+#include <wx/string.h>
 #include <wx/regex.h>			// Needed for wxRegEx
 #include <wx/tokenzr.h>			// Needed for wxStringTokenizer
 
