1c96883eee6c2ffc922667fc245c510a879c68c6
xaignar <xaignar@amule.org>
2005-11-25 15:24:19 +0000

    Upgraded our included listctrl with the bugfixes from v2.6.1 and v2.6.2 of wxWidgets.

--- src/listctrl.h
+++ src/listctrl.h
@@ -1,7 +1,7 @@
 #include <wx/version.h>
 
 #if wxCHECK_VERSION(2, 6, 0)
-	#include "extern/listctrl.260.h"
+	#include "extern/listctrl.262.h"
 #else
 	#error Unsupported version of wxWidgets used!
 #endif
