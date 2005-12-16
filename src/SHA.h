120a28587bd60b36c273a68339f0747c690ba3ae
leio <leio@amule.org>
2005-12-16 12:09:29 +0000

    I might aswell change the license, can't I? Update GPL notes on top of files to the new FSF address. A couple COPYING and license.txt's (the licenses themselves) to do.

--- src/SHA.h
+++ src/SHA.h
@@ -20,7 +20,7 @@
 // 
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA, 02111-1307, USA
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
 //
 // Kry - Modified version of the original SHA.cpp to work on linux and
 // use wxWidgets. Original license follows.
