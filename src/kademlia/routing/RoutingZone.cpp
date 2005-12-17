42e1675e0c5c1ee270445c5a6f8360419119014d
kry <kry@amule.org>
2005-12-17 08:44:59 +0000

    Chances of John and me doing the same mistake on separate implementation?
    Well it happened.
    
    (Fixes mem corruption, and contact asserts)

--- src/kademlia/routing/RoutingZone.cpp
+++ src/kademlia/routing/RoutingZone.cpp
@@ -406,6 +406,9 @@ void CRoutingZone::merge(void)
 		m_subZones[0]->m_superZone = NULL;
 		m_subZones[1]->m_superZone = NULL;
 
+		m_subZones[0]->m_bin->m_dontDeleteContacts = true;
+		m_subZones[1]->m_bin->m_dontDeleteContacts = true;	
+		
 		delete m_subZones[0];
 		delete m_subZones[1];
 
