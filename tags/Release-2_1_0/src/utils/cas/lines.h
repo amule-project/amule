e1102986c747b0e9c71b08224f8346ab029825de
ken <ken@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2006-01-01 22:44:49 +0000

    Deleting premature tag.

--- tags/Release-2_1_0/src/utils/cas/lines.h
+++
@@ -1,7 +0,0 @@
-#ifndef CAS_LINES_H
-#define CAS_LINES_H
-
-void CreateLine(char *lines[], int line, const char *format, ...);
-void AppendToLine(char *lines[], int line, const char *text);
-
-#endif
