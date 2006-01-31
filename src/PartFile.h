fb718d2ae1384465c4ef66be0997878814f58dba
xaignar <xaignar@amule.org>
2006-01-31 16:06:46 +0000

    Function-signature did not match underlying virtual function.

--- src/PartFile.h
+++ src/PartFile.h
@@ -92,7 +92,7 @@ public:
 	
 	bool	CreateFromFile(wxString WXUNUSED(directory), wxString WXUNUSED(filename), void* WXUNUSED(pvProgressParam)) {return false;}// not supported in this class
 	void 	SetPartFileStatus(uint8 newstatus);
-	virtual bool LoadFromFile(const CFile* WXUNUSED(file)) const { return false; }
+	virtual bool LoadFromFile(const CFileDataIO* WXUNUSED(file)) { return false; }
 	bool	WriteToFile(CFileDataIO* WXUNUSED(file))	{ return false; }
 	bool	IsPartFile() const		{ return !(status == PS_COMPLETE); }
 	uint32	Process(uint32 reducedownload, uint8 m_icounter);
