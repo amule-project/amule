7399f5d27a90b1fe00a92e4549a952479676fc9c
phoenix <phoenix@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2006-02-12 15:18:31 +0000

    Commit upon stefanero's request. Notice, I can't test it on win32. <stefanero> _pho3n1x, there is a problem for mingw with your last commit <stefanero> <pinback> seems like I found a fix for the imaglist.h problem with mingw <stefanero> --> soniapatilla (n=soniapat@200.119.94.167) has joined #amule <stefanero> <pinback> in amule-cvs/src/listctrl.h insert this before the line: #include "extern/listctrl.cvs.h" <stefanero> <pinback> #if defined(__WIN32__) <stefanero> <pinback> #include <wx/imaglist.h> <stefanero> <pinback> #endif <stefanero> <pinback> this makes sure the msw imaglist.h is included before the generic version <stefanero> <pinback> which is important because generic/imaglist.h conatins ifdefs for coexistance but msw/imaglist.h doesnt

--- trunk/src/listctrl.h
+++ trunk/src/listctrl.h
@@ -1,7 +1,11 @@
 #include <wx/version.h>
 
 #if wxCHECK_VERSION(2, 6, 0)
-	#include "extern/listctrl.cvs.h"
+	#if defined(__WIN32__)
+		#include <wx/imaglist.h>
+	#endif
+#include "extern/listctrl.cvs.h"
 #else
 	#error Unsupported version of wxWidgets used!
 #endif
+
