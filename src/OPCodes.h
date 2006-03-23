afacaefa3f3132d5e70a1ca48e051230b3e14f5a
kry <kry@amule.org>
2006-03-23 14:31:46 +0000

    2.1.x: Version bump to 2.1.2

--- src/OPCodes.h
+++ src/OPCodes.h
@@ -75,12 +75,12 @@ enum {
 #ifdef __CVS__
 	#define	MOD_VERSION_LONG		wxT("aMule CVS")
 #else
-	#define	MOD_VERSION_LONG		wxT("aMule 2.1.1")
+	#define	MOD_VERSION_LONG		wxT("aMule 2.1.2")
 #endif
 
 #define	VERSION_MJR		2
 #define	VERSION_MIN		1
-#define	VERSION_UPDATE		1
+#define	VERSION_UPDATE		2
 
 #define	EDONKEYVERSION				0x3c
 #define	PREFFILE_VERSION			0x14 //<<-- last change: reduced .dat, by using .ini
