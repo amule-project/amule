7c525b727786f997e3e739d91d8908310b4a9abf
kry <kry@amule.org>
2006-04-26 04:46:24 +0000

    Initial commit of the translation status scripts

---
+++ po/l10n.xsl
@@ -0,0 +1,50 @@
+<?xml version='1.0' ?>
+<xsl:stylesheet version='2.0' xmlns:xsl='http://www.w3.org/1999/XSL/Transform'>
+	<xsl:template match='/project'>
+		<html>
+			<head>
+				<title><xsl:value-of select='@name'/> translation statistics</title>
+				<!-- <link rel="Stylesheet" href="/gaim.css" type="text/css" media="screen" /> -->
+				<style>
+					.bargraph {
+						width: 200px;
+						height: 20px;
+						background: black;
+						border-collapse: collapse;
+						border-spacing: 0px;
+						margin: 0px;
+						border: 0px;
+						padding: 0px;
+					}
+
+				</style>
+			</head>
+			<body>
+				<!-- <div id="content"> -->
+				<h1><xsl:value-of select='@name' /> translation statistics</h1>
+				<table>
+					<tr><th>Language  </th><th colspan='2'>Translated</th><th colspan='2'>Fuzzy</th><th colspan='2'>Untranslated</th></tr>
+					<xsl:for-each select="lang">
+						<xsl:sort select='@translated' data-type="number" order="descending"/>
+						<tr>
+							<td><a><xsl:attribute name='href'>files/<xsl:value-of select='@code'/>.po</xsl:attribute><xsl:value-of select='@name'/> (<xsl:value-of select='@code'/>)</a></td>
+							<td><xsl:value-of select='@translated'/></td><td><xsl:value-of select="format-number(@translated div ../@strings * 100,'#.##  ')"/> %</td>
+							<td><xsl:value-of select='@fuzzy'/></td><td><xsl:value-of select="format-number(@fuzzy div ../@strings * 100,'#.##  ')"/> %</td>
+							<td><xsl:value-of select='../@strings - (@translated + @fuzzy)'/></td><td><xsl:value-of select="format-number((../@strings - (@translated + @fuzzy)) div ../@strings * 100,'#.##  ')"/> %</td>
+						<td>
+							<table class='bargraph'><tr>
+									<td bgcolor='green'><xsl:attribute name='width'><xsl:value-of select='round(@translated div ../@strings * 200)'/>px;</xsl:attribute></td>
+									<td bgcolor='blue'><xsl:attribute name='width'><xsl:value-of select='round(@fuzzy div ../@strings * 200)'/>px;</xsl:attribute></td>
+									<!-- <td bgcolor='red'><xsl:attribute name='width'><xsl:value-of select='200 - round((@translated + @fuzzy) div ../@strings * 200)'/>px;</xsl:attribute></td> -->
+									<td bgcolor='red'></td>
+							</tr></table>
+						</td>
+						</tr>
+					</xsl:for-each>
+				</table>
+				<a><xsl:attribute name='href'><xsl:value-of select='@pofile'/></xsl:attribute><xsl:value-of select='@pofile'/></a> generated on <xsl:value-of select='@generated'/>
+				<!-- </div> -->
+			</body>
+		</html>
+	</xsl:template>
+</xsl:stylesheet>
---
+++ src/utils/scripts/stats.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl
+
+# Copyright 2003-2005 Nathan Walp <faceprint@faceprint.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 50 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+#
+
+# Taken from the Gaim project, modified by Angel Vidal (Kry) for the aMule project.
+
+use POSIX qw(strftime);
+
+
+my $PACKAGE="amule";
+
+
+use Locale::Language;
+
+$lang{gl} = "Galician (Galiza)";
+$lang{en_AU} = "English (Australian)";
+$lang{en_CA} = "English (Canadian)";
+$lang{en_GB} = "English (British)";
+$lang{en_US} = "English (USA)";
+$lang{es_MX} = "Spanish (Mexico)";
+$lang{et_EE} = "Estonian (Estonia)";
+$lang{it_CH} = "Italian (Switzerland)";
+$lang{ko_KR} = "Korean (Korea)";
+$lang{my_MM} = "Burmese (Myanmar)";
+$lang{pt_BR} = "Portuguese (Brazilian)";
+$lang{pt_PT} = "Portuguese (Portugal)";
+$lang{'sr@Latn'} = "Serbian (Latin)";
+$lang{zh_CN} = "Chinese (Simplified)";
+$lang{zh_TW} = "Chinese (Traditional)";
+
+opendir(DIR, ".") || die "can't open directory: $!";
+@pos = grep { /\.po$/ && -f } readdir(DIR);
+foreach (@pos) { s/\.po$//; };
+closedir DIR;
+
+@pos = sort @pos;
+
+$now = `date`;
+
+system("intltool-update --pot > /dev/null");
+
+$_ = `msgfmt --statistics $PACKAGE.pot -o /dev/null 2>&1`;
+
+die "unable to get total: $!" unless (/(\d+) untranslated messages/);
+
+$total = $1;
+$generated = strftime "%Y-%m-%d %H:%M:%S", gmtime;
+
+print "<?xml version='1.0'?>\n";
+print "<?xml-stylesheet type='text/xsl' href='l10n.xsl'?>\n";
+print "<project version='1.0' xmlns:l10n='http://faceprint.com/code/l10n' name='$PACKAGE' pofile='$PACKAGE.pot' strings='$total' generated='$generated'>\n";
+
+foreach $index (0 .. $#pos) {
+	$trans = $fuzz = $untrans = 0;
+	$po = $pos[$index];
+	print STDERR "$po..." if($ARGV[0] eq '-v');
+	system("msgmerge $po.po $PACKAGE.pot -o $po.new 2>/dev/null");
+	$_ = `msgfmt --statistics $po.new -o /dev/null 2>&1`;
+	chomp;
+	if(/(\d+) translated message/) { $trans = $1; }
+	if(/(\d+) fuzzy translation/) { $fuzz = $1; }
+	if(/(\d+) untranslated message/) { $untrans = $1; }
+	unlink("$po.new");
+
+	$name = "";
+	$name = $lang{$po};
+	$name = code2language($po) unless $name ne "";
+	$name = "???" unless $name ne "";
+
+	print "<lang code='$po' name='$name' translated='$trans' fuzzy='$fuzz' />\n";
+	print STDERR "done ($untrans untranslated strings).\n" if($ARGV[0] eq '-v');
+}
+
+print "</project>\n";
+
