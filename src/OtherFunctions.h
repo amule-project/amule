8d527eea9b9185745cc55ad01499898aef9d2354
Dévai Tamás <gonosztopi@amule.org>
2006-06-15 11:08:58 +0000

    Since EC now doesn't use anything related to threads or locks, it is possible to compile EC without thread support. This patch enables compiling amulecmd w/o thread support, so the generated binary doesn't depend on mingwm10.dll on win32 platforms.

--- src/OtherFunctions.h
+++ src/OtherFunctions.h
@@ -28,7 +28,6 @@
 
 #include <wx/string.h>		// Needed for wxString
 #include <wx/intl.h>		// Needed for wxLANGUAGE_ constants
-#include <wx/thread.h>
 
 #include "Types.h"		// Needed for uint16, uint32 and uint64
 
@@ -360,6 +359,10 @@ int StrLang2wx(const wxString& language);
 wxString wxLang2Str(const int lang);
 
 
+#if wxUSE_THREADS
+
+#include <wx/thread.h>
+
 /**
  * Automatically unlocks a mutex on construction and locks it on destruction.
  *
@@ -403,6 +406,7 @@ private:
     bool     m_isOk;
     wxMutex& m_mutex;
 };
+#endif /* wxUSE_THREADS */
 
 
 #endif // OTHERFUNCTIONS_H
