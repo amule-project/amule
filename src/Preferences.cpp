a2e013e4539c91f897c5fd2d0c585c9fc9489193
Dévai Tamás <gonosztopi@amule.org>
2006-07-10 10:14:23 +0000

    Proper restoration of startup locale after checking available locales.
    This fixes http://forum.amule.org/thread.php?threadid=10346&sid= (.'s show up as ,'s)

--- src/Preferences.cpp
+++ src/Preferences.cpp
@@ -684,6 +684,12 @@ public:
 			}
 		}
 
+		// Restore original locale
+		{
+			wxLocale tmpLocale;
+			InitLocale(tmpLocale, theApp.m_locale.GetLanguage());
+		}
+
 		return Cfg_Tmpl<int>::TransferToWindow();
 	}
 
