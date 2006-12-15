68c5238ed3a2f0e289de95cd93f1bc68d53b7c79
phoenix <phoenix@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2006-12-15 01:39:03 +0000

    Ooooops!

---
+++ trunk/src/webserver/src/UPnPCompatibility.cpp
@@ -0,0 +1,55 @@
+//
+// This file is part of the aMule Project.
+//
+// Copyright (c) 2006 Marcelo Roberto Jimenez ( phoenix@amule.org )
+// Copyright (c) 2006 aMule Team ( admin@amule.org / http://www.amule.org )
+//
+// Any parts of this program derived from the xMule, lMule or eMule project,
+// or contributed by third-party developers are copyrighted by their
+// respective authors.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
+//
+
+
+#include "UPnPCompatibility.h"
+
+
+bool CLogger::IsEnabled(DebugType /*type*/)
+{
+	return true;
+}
+
+
+void CLogger::AddLogLine(
+	bool critical,
+	const std::ostringstream &msg)
+{
+	AddLogLine(critical, static_cast<DebugType>(0), msg);
+}
+
+
+void CLogger::AddLogLine(
+	bool critical,
+	DebugType /*type*/,
+	const std::ostringstream &msg)
+{
+	std::string s;
+	if (critical) {
+		s = "Critical message: ";
+	}
+	printf("%s%s\n", s.c_str(), msg.str().c_str());
+}
+
