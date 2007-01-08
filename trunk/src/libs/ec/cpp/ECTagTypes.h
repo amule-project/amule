daeb852e0dc6c7265ab1870a2919087cd14ca9e8
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2007-01-08 02:21:24 +0000

    Generate, and you will win

--- trunk/src/libs/ec/cpp/ECTagTypes.h
+++ trunk/src/libs/ec/cpp/ECTagTypes.h
@@ -1,39 +1,31 @@
-//
-// This file is part of the aMule Project.
-//
-// Copyright (c) 2004-2006 aMule Team ( admin@amule.org / http://www.amule.org )
-//
-// Any parts of this program derived from the xMule, lMule or eMule project,
-// or contributed by third-party developers are copyrighted by their
-// respective authors.
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
 // 
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
-//
+//  This file is part of the aMule Project.
+// 
+//  Copyright (c) 2004-2006 aMule Team ( admin@amule.org / http://www.amule.org )
+// 
+//  Any parts of this program derived from the xMule, lMule or eMule project,
+//  or contributed by third-party developers are copyrighted by their
+//  respective authors.
+// 
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
 
-/*!
- * \file ECTagTypes.h
- *
- * \brief EC tag types
- */
+// Purpose:
+// EC tag types for use on the ec library.
 
-#ifndef	ECTAGTYPES_H
-#define	ECTAGTYPES_H
-
-/*
- * EC Tag types
- */
+#ifndef __ECTAGTYPES_H__
+#define __ECTAGTYPES_H__
 
 enum ECTagTypes {
 	EC_TAGTYPE_UNKNOWN = 0,
@@ -48,5 +40,4 @@ enum ECTagTypes {
 	EC_TAGTYPE_HASH16 = 9
 };
 
-#endif	/* ECTAGTYPES_H */
-// File_checked_for_headers
+#endif // __ECTAGTYPES_H__
