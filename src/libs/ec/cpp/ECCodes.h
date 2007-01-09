396369c1f1ec3c893b7ed8326b5070f89670e02d
kry <kry@amule.org>
2007-01-09 00:00:49 +0000

    Bad license generation

--- src/libs/ec/cpp/ECCodes.h
+++ src/libs/ec/cpp/ECCodes.h
@@ -1,28 +1,28 @@
-*main::CPPFILE
-// *main::CPPFILE This file is part of the aMule Project.
-// *main::CPPFILE
-// *main::CPPFILE Copyright (c) 2004-2006 aMule Team ( admin@amule.org / http://www.amule.org )
-// *main::CPPFILE
-// *main::CPPFILE Any parts of this program derived from the xMule, lMule or eMule project,
-// *main::CPPFILE or contributed by third-party developers are copyrighted by their
-// *main::CPPFILE respective authors.
-// *main::CPPFILE
-// *main::CPPFILE This program is free software; you can redistribute it and/or modify
-// *main::CPPFILE it under the terms of the GNU General Public License as published by
-// *main::CPPFILE the Free Software Foundation; either version 2 of the License, or
-// *main::CPPFILE (at your option) any later version.
-// *main::CPPFILE
-// *main::CPPFILE This program is distributed in the hope that it will be useful,
-// *main::CPPFILE but WITHOUT ANY WARRANTY; without even the implied warranty of
-// *main::CPPFILE MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// *main::CPPFILE GNU General Public License for more details.
-// *main::CPPFILE 
-// *main::CPPFILE You should have received a copy of the GNU General Public License
-// *main::CPPFILE along with this program; if not, write to the Free Software
-// *main::CPPFILE Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
 // 
-*main::CPPFILEPurpose:// 
-*main::CPPFILE// 
+//  This file is part of the aMule Project.
+// 
+//  Copyright (c) 2004-2006 aMule Team ( admin@amule.org / http://www.amule.org )
+// 
+//  Any parts of this program derived from the xMule, lMule or eMule project,
+//  or contributed by third-party developers are copyrighted by their
+//  respective authors.
+// 
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+// 
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
+
+// Purpose:
+// EC codes and type definition.
 
 #ifndef __ECCODES_H__
 #define __ECCODES_H__
