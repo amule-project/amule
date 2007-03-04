466649a809878e3649244d38458e98c2139f6f68
lfroen <lfroen@amule.org>
2007-03-04 05:37:58 +0000

    fixed tag write - login working

--- src/libs/ec/c#/ecProto.cs
+++ src/libs/ec/c#/ecProto.cs
@@ -20,6 +20,7 @@ namespace amule.net
     };
 
     class ecProto {
+
         public class ecTag {
             protected int m_size;
             protected EcTagTypes m_type;
@@ -37,22 +38,32 @@ namespace amule.net
                 m_subtags = new LinkedList<ecTag>();
             }
 
-            public virtual void Write(BinaryWriter wr)
+            protected void WriteSubtags(BinaryWriter wr)
             {
-                UInt16 name16 = (UInt16)m_name;
-                byte type8 = (byte)m_type;
-                UInt32 size32 = (UInt32)Size();
-                wr.Write(name16);
-                wr.Write(type8);
-                wr.Write(size32);
-
-                UInt16 count16 = (UInt16)m_subtags.Count;
-                if ( count16 != 0) {
-                    wr.Write(count16);
-                    foreach (ecTag t in m_subtags) {
+                Int16 count16 = (Int16)m_subtags.Count;
+                if (count16 != 0) {
+                    wr.Write(System.Net.IPAddress.HostToNetworkOrder(count16));
+                    foreach (ecTag t in m_subtags)
+                    {
                         t.Write(wr);
                     }
                 }
+            }
+
+            public virtual void Write(BinaryWriter wr)
+            {
+                Int16 name16 = (Int16)(m_name);
+                name16 <<= 1;
+                byte type8 = (byte)m_type;
+                Int32 size32 = (Int32)Size();
+                if (m_subtags.Count != 0) {
+                    name16 |= 1;
+                }
+                wr.Write(System.Net.IPAddress.HostToNetworkOrder(name16));
+                wr.Write(type8);
+                wr.Write(System.Net.IPAddress.HostToNetworkOrder(size32));
+
+                WriteSubtags(wr);
                 //
                 // here derived class will put actual data
                 //
@@ -65,6 +76,9 @@ namespace amule.net
                     total_size += t.Size();
                     // name + type + size for each tag
                     total_size += (2 + 1 + 4);
+                    if (t.HaveSubtags()) {
+                        total_size += 2;
+                    }
                 }
                 return total_size;
             }
@@ -72,6 +86,11 @@ namespace amule.net
             public void AddSubtag(ecTag t)
             {
                 m_subtags.AddLast(t);
+            }
+
+            bool HaveSubtags()
+            {
+                return (m_subtags.Count != 0);
             }
         }
 
@@ -97,11 +116,12 @@ namespace amule.net
                 
                 switch ( m_size ) {
                     case 8:
-                        Int32 val32 = (Int32)(m_val & 0xffffffff);
+                        Int32 val32 = (Int32)(m_val >> 32);
                         wr.Write(System.Net.IPAddress.HostToNetworkOrder(val32));
-                        val32 = (Int32)(m_val >> 32);
+
+                        val32 = (Int32)(m_val & 0xffffffff);
                         wr.Write(System.Net.IPAddress.HostToNetworkOrder(val32));
-                        goto case 4;
+                        break;
                     case 4:
                         val32 = (Int32)(m_val & 0xffffffff);
                         wr.Write(System.Net.IPAddress.HostToNetworkOrder(val32));
@@ -120,13 +140,25 @@ namespace amule.net
 
         public class ecTagMD5 : ecTag {
             byte[] m_val;
-            public ecTagMD5(ECTagNames n, string s)
+            public ecTagMD5(ECTagNames n, string s, bool string_is_hash)
                 : base(n, EcTagTypes.EC_TAGTYPE_HASH16)
             {
-                MD5CryptoServiceProvider p = new MD5CryptoServiceProvider();
-                byte[] bs = System.Text.Encoding.UTF8.GetBytes(s);
-                m_val = p.ComputeHash(bs);
-
+                if ( string_is_hash ) {
+                    // in this case hash is passed as hex string
+                    if ( s.Length != 16*2 ) {
+                        throw new Exception("md5 hash of proto version have incorrect length");
+                    }
+                    //byte[] hash_str = s.ToCharArray();
+                    for (int i = 0; i < 16; i++) {
+                        string v = s.Substring(i * 2, 2);
+                        
+                    }
+                    m_val = new byte[16];
+                } else {
+                    MD5CryptoServiceProvider p = new MD5CryptoServiceProvider();
+                    byte[] bs = System.Text.Encoding.UTF8.GetBytes(s);
+                    m_val = p.ComputeHash(bs);
+                }
                 m_size = 16;
             }
             public override void Write(BinaryWriter wr)
@@ -142,12 +174,14 @@ namespace amule.net
                 : base(n, EcTagTypes.EC_TAGTYPE_STRING)
             {
                 m_val = System.Text.Encoding.UTF8.GetBytes(s);
-                m_size = m_val.GetLength(0);
+                m_size = m_val.GetLength(0) + 1;
             }
             public override void Write(BinaryWriter wr)
             {
                 base.Write(wr);
                 wr.Write(m_val);
+                byte zero_byte = 0;
+                wr.Write(zero_byte);
             }
         }
 
@@ -173,10 +207,17 @@ namespace amule.net
                 m_opcode = cmd;
             }
 
+            public int PacketSize()
+            {
+                int packet_size = Size();
+                // 1 (command) + 2 (tag count)
+                return packet_size + 1 + 2;
+            }
+
             public override void Write(BinaryWriter wr)
             {
                 UInt32 flags = 0x20;
-                int packet_size = Size();
+                int packet_size = PacketSize();
                 if ( packet_size > MaxUncompressedPacket ) {
                     flags |= (UInt32)ECFlags.EC_FLAG_ZLIB;
                 }
@@ -186,9 +227,9 @@ namespace amule.net
                 }
 
                 wr.Write(System.Net.IPAddress.HostToNetworkOrder((Int32)(flags)));
-                wr.Write(System.Net.IPAddress.HostToNetworkOrder((Int32)(packet_size - 8)));
+                wr.Write(System.Net.IPAddress.HostToNetworkOrder((Int32)(packet_size)));
                 wr.Write((byte)m_opcode);
-                base.Write(wr);
+                WriteSubtags(wr);
             }
 
 
@@ -197,12 +238,15 @@ namespace amule.net
             public ecLoginPacket(string client_name, string version, string pass)
                 : base(ECOpCodes.EC_OP_AUTH_REQ)
             {
-                AddSubtag(new ecTagString(ECTagNames.EC_TAG_CLIENT, client_name));
+                AddSubtag(new ecTagString(ECTagNames.EC_TAG_CLIENT_NAME, client_name));
                 AddSubtag(new ecTagString(ECTagNames.EC_TAG_CLIENT_VERSION, version));
                 AddSubtag(new ecTagInt(ECTagNames.EC_TAG_PROTOCOL_VERSION,
                     (UInt64)ProtocolVersion.EC_CURRENT_PROTOCOL_VERSION));
 
-                AddSubtag(new ecTagMD5(ECTagNames.EC_TAG_PASSWD_HASH, pass));
+                AddSubtag(new ecTagMD5(ECTagNames.EC_TAG_PASSWD_HASH, pass, false));
+
+                // discussion is ongoing
+                //AddSubtag(new ecTagMD5(ECTagNames.EC_TAG_VERSION_ID, EC_VERSION_ID, true));
             }
         }
 
