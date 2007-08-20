8dfa58b2ef48136442558c16e321fc9e51a87476
kry <kry@amule.org>
2007-08-20 14:36:07 +0000

    I dont' even want to know why. Fixed windows compilation.

--- src/EncryptedDatagramSocket.h
+++ src/EncryptedDatagramSocket.h
@@ -23,8 +23,8 @@
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
 //
 
-#include "Types.h"
 #include "Proxy.h"
+#include "Types.h"
 
 class CEncryptedDatagramSocket : public CDatagramSocketProxy
 {
