c40372a3725ea9f2cb1a977bc22e33f4551c17e7
xaignar <xaignar@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2007-12-26 23:10:19 +0000

    wxApp::OnAssert has been deprecated, using OnAssertFailure instead.

--- trunk/src/amule.h
+++ trunk/src/amule.h
@@ -229,7 +229,8 @@ protected:
 	/**
 	 * Handles asserts in a thread-safe manner.
 	 */
-	virtual void OnAssert(const wxChar *file, int line, const wxChar *cond, const wxChar *msg);
+	virtual void OnAssertFailure(const wxChar* file, int line,
+		const wxChar* func, const wxChar* cond, const wxChar* msg);
 #endif
 
 	/**
