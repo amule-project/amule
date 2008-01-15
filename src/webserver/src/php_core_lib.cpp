e6dc93f307167cb830a665ac864ab38ff30125d1
phoenix <phoenix@amule.org>
2008-01-15 18:58:20 +0000

    Fix for printf in php_core_lib.cpp.

--- src/webserver/src/php_core_lib.cpp
+++ src/webserver/src/php_core_lib.cpp
@@ -770,7 +770,7 @@ void php_native_search_start_cmd(PHP_VALUE_NODE *)
 		case 1: search_type = EC_SEARCH_GLOBAL; break;
 		case 2: search_type = EC_SEARCH_KAD; break;
 		default: 
-			php_report_error(PHP_ERROR, "Invalid search type %d", si->var->value.int_val);
+			php_report_error(PHP_ERROR, "Invalid search type %lld", si->var->value.int_val);
 			return;
 	}
 #endif
