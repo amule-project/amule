4abff8a60305b537d47f2ab2000e6a7aaf4fb660
gonosztopi <gonosztopi@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2008-04-02 09:50:03 +0000

    Sparse partfiles on Windows, by sturedman.

---
+++ trunk/src/PlatformSpecific.h
@@ -0,0 +1,47 @@
+//
+// This file is part of the aMule Project.
+//
+// Copyright (c) 2008 aMule Team ( admin@amule.org / http://www.amule.org )
+//
+// Any parts of this program derived from the xMule, lMule or eMule project,
+// or contributed by third-party developers are copyrighted by their
+// respective authors.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
+//
+
+#ifndef PLATFORMSPECIFIC_H
+#define PLATFORMSPECIFIC_H
+
+#include <common/Path.h>
+#include <stdint.h>
+
+
+namespace PlatformSpecific {
+
+/**
+ * Create sparse file.
+ *
+ * This function will create the named file sparse if possible.
+ *
+ * @param name The file to be created (sparse if possible).
+ * @param size The desired size of the file.
+ * @return true, if creating the file succeeded, false otherwise.
+ */
+bool CreateSparseFile(const CPath& name, uint64_t size);
+
+}; 
+
+#endif /* PLATFORMSPECIFIC_H */
