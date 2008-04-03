21ab086a118ea924ff507ce5b99f0f1dff69421d
Dévai Tamás <gonosztopi@amule.org>
2008-04-03 19:24:17 +0000

    Review of muuli.wdr.
    Got rid of a number of unused IDs and variables, and also removed some
    superfluous sizers. The result: GUI (mostly prefs) now use less memory,
    loads faster and looks better (this is because I also unified the look
    of the prefs panels).

--- src/PrefsUnifiedDlg.cpp
+++ src/PrefsUnifiedDlg.cpp
@@ -91,7 +91,6 @@ BEGIN_EVENT_TABLE(PrefsUnifiedDlg,wxDialog)
 
 	// Browse buttons
 //	EVT_BUTTON(IDC_SELSKIN,		PrefsUnifiedDlg::OnButtonDir)
-	EVT_BUTTON(IDC_BTN_BROWSE_WAV,		PrefsUnifiedDlg::OnButtonBrowseWav)
 	EVT_BUTTON(IDC_BROWSEV,			PrefsUnifiedDlg::OnButtonBrowseApplication)
 	EVT_BUTTON(IDC_SELTEMPDIR,		PrefsUnifiedDlg::OnButtonDir)
 	EVT_BUTTON(IDC_SELINCDIR,		PrefsUnifiedDlg::OnButtonDir)
@@ -178,8 +177,6 @@ PrefsPage pages[] =
 	{ wxTRANSLATE("Directories"),		PreferencesDirectoriesTab,	17, NULL },
 	{ wxTRANSLATE("Statistics"),		PreferencesStatisticsTab,	10, NULL },
 	{ wxTRANSLATE("Security"),		PreferencesSecurityTab,		22, NULL },
-	//Notications are disabled since they havent been implemented
-	//{ wxTRANSLATE("Notifications"),	PreferencesNotifyTab,		18, NULL },
 	{ wxTRANSLATE("Gui Tweaks"),		PreferencesGuiTweaksTab,	19, NULL },
 	{ wxTRANSLATE("Core Tweaks"),		PreferencesaMuleTweaksTab,	12, NULL },
 	{ wxTRANSLATE("Events"),		PreferencesEventsTab,		5,  NULL }
@@ -867,20 +864,6 @@ void PrefsUnifiedDlg::OnButtonDir(wxCommandEvent& event)
 		wxDefaultPosition, this);
 	if (!str.IsEmpty()) {
 		widget->SetValue(str);
-	}
-}
-
-
-void PrefsUnifiedDlg::OnButtonBrowseWav(wxCommandEvent& WXUNUSED(evt))
-{
-	wxString str = wxFileSelector( 
-		_("Browse wav"), wxEmptyString, wxEmptyString,
-		wxT("*.wav"), _("File wav (*.wav)|*.wav||"), 0, this );
-	
-	if ( !str.IsEmpty() ) {
-		wxTextCtrl* widget = CastChild( IDC_EDIT_TBN_WAVFILE, wxTextCtrl );
-
-		widget->SetValue( str );
 	}
 }
 
