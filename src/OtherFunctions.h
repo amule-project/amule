1a6181dad5ae3475c5b4cd0ef89b6691d99a64d1
Dévai Tamás <gonosztopi@amule.org>
2008-05-28 13:52:48 +0000

    Removed unused function.

--- src/OtherFunctions.h
+++ src/OtherFunctions.h
@@ -349,8 +349,6 @@ void DumpMem_DW(const uint32 *ptr, int count);
 #define IP_FROM_GUI_ID(x) (x >> 16)
 
 
-void MilliSleep(uint32 msecs);
-
 
 inline const long int make_full_ed2k_version(int a, int b, int c) {
 	return ((a << 17) | (b << 10) | (c << 7));
