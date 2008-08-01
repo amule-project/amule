04e90d09f5ae24b4abef6b313d8eb1ec2565d8e6
gonosztopi <gonosztopi@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2008-08-01 16:27:55 +0000

    Version bump

--- branches/RELEASE-2_2_X/src/include/common/ClientVersion.h
+++ branches/RELEASE-2_2_X/src/include/common/ClientVersion.h
@@ -47,7 +47,7 @@
 #define __SVN__
 
 #ifndef VERSION
-	#define VERSION "2.2.1"
+	#define VERSION "2.2.2"
 #endif
 
 #ifdef __SVN__
@@ -58,7 +58,7 @@
 
 #define	VERSION_MJR		2
 #define	VERSION_MIN		2
-#define	VERSION_UPDATE		1
+#define	VERSION_UPDATE		2
 
 #ifndef PACKAGE
 #define PACKAGE "amule"
