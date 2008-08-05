cbb797af310e498cf448c9ca8ac24fcad3af2533
johkra <johkra@amule.org>
2008-08-05 11:12:49 +0000

    Backport 8791 - log display changes

--- src/muuli_wdr.cpp
+++ src/muuli_wdr.cpp
@@ -81,9 +81,9 @@ wxSizer *muleDlg( wxWindow *parent, bool call_fit, bool set_sizer )
 
     wxBoxSizer *item6 = new wxBoxSizer( wxHORIZONTAL );
 
-    wxBitmapButton *item7 = new wxBitmapButton( parent, IDC_SHOWSTATUSTEXT, amuleDlgImages( 31 ), wxDefaultPosition, wxSize(25,25) );
-    item7->SetToolTip( _("Pop-up status text") );
-    item6->Add( item7, 0, wxALIGN_CENTER, 5 );
+    wxStaticBitmap *item7 = new wxStaticBitmap( parent, ID_STATICBITMAP, amuleDlgImages( 31 ), wxDefaultPosition, wxDefaultSize );
+    item7->SetToolTip( _("Events are displayed here. For a complete list of events, refer to the log in the Servers-tab.") );
+    item6->Add( item7, 0, wxALIGN_CENTER|wxRIGHT|wxTOP|wxBOTTOM, 5 );
 
     wxStaticText *item8 = new wxStaticText( parent, -1, _("Loading ..."), wxDefaultPosition, wxSize(140,-1), wxST_NO_AUTORESIZE );
     item8->SetToolTip( _("Events are displayed here. For a complete list of events, refer to the log in the Servers-tab.") );
