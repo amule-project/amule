ea30819d835f5277177f1b604ed5cf533d76d4af
lfroen <lfroen@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2008-12-03 19:19:30 +0000

    EC code, some partfile handling

--- trunk/cocoa-mule/amuleData.mm
+++ trunk/cocoa-mule/amuleData.mm
@@ -3,8 +3,42 @@
 
 @implementation amuleFile
 
-- (bool)sameID: (MD5Data *) hash {
-	return ((hash->lo == m_hash.lo) && (hash->hi == m_hash.hi));
+
+- (unsigned)hash {
+	return m_hash.lo ^ m_hash.hi;
+}
+
+- (NSString *)key {
+	NSString *s = [NSString stringWithFormat:@"%qx%qx", m_hash.hi, m_hash.lo];
+	return s;
+}
+
+- (BOOL)isEqual: (id) object {
+	if ([object isKindOfClass:[amuleFile class]]) {
+		amuleFile *myobj = (amuleFile *)object;
+		return ((myobj->m_hash.lo == m_hash.lo) && (myobj->m_hash.hi == m_hash.hi));
+	} else {
+		return false;
+	}
+}
+
+@end
+
+@implementation DownloadingFile
+
++ (id)createFromEC: (ECTagMD5 *) tag {
+	DownloadingFile *obj = [[DownloadingFile alloc] init];
+	
+	obj->m_hash = [tag getMD5Data];
+	
+	ECTag *nametag = [tag tagByName: EC_TAG_PARTFILE_NAME];
+	ECTagString *stag = (ECTagString *)nametag;
+	obj->m_name = stag.stringValue;;
+	
+	return obj;
+}
+
+- (void)updateFromEC:(ECTagMD5 *) tag {
 }
 
 @end
@@ -18,6 +52,8 @@
 	
 	obj->m_connection = connection;
 	
+	obj->m_downloads = [NSMutableDictionary dictionaryWithCapacity:256];
+	 
 	return obj;
 }
 
@@ -40,10 +76,18 @@
 - (void)handleDownloadQueueUpdate:(ECPacket *) packet {
 	for (ECTag *t in packet.subtags) {
 		if ( [t isKindOfClass:[ECTagMD5 class]] ) {
-			//
-			// FIXME: some kind of custom hash should be used to locate
-			// object for given MD5.
-			for (DownloadingFile *o in m_downloads) {
+			ECTagMD5 *tag = (ECTagMD5 *)t;
+			ECTag *nametag = [tag tagByName: EC_TAG_PARTFILE_NAME];
+			if ( nametag != nil ) {
+				ECTagString *stag = (ECTagString *)nametag;
+				NSString *str = stag.stringValue;
+				printf("FILE: %s\n", [str UTF8String]);
+				DownloadingFile *file = [DownloadingFile createFromEC:tag];
+				[m_downloads setObject:file forKey:[file key]];
+			} else {
+				DownloadingFile *file = [m_downloads objectForKey:[tag stringKey]];
+				[file updateFromEC:tag];
+				printf("FILE: no-name-received\n");
 			}
 		} else {
 			NSLog(@"[EC] bad tag type '%s'\n", [t class]);
