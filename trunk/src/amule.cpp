7f3eb30da2c26a38acc471e8e8c984beb641f562
sturedman <sturedman@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2009-08-09 20:50:25 +0000

    Fixed crash in termination of UploadBandwidthThrottler with wx 2.9

--- trunk/src/amule.cpp
+++ trunk/src/amule.cpp
@@ -1718,8 +1718,16 @@ void CamuleApp::ShutDown()
 	}
 	
 	CThreadScheduler::Terminate();
-	
-	theApp->uploadBandwidthThrottler->EndThread();
+
+	// Now we have tried to delete all our clients and sockets,
+	// but wx 2.9 has only scheduled them for deletion. 
+	// Force deletion now, or it will occur when we wait for a locket mutex,
+	// and the uploadBandwidthThrottler will crash on invalid objects.
+	AddDebugLogLineN(logGeneral, wxT("Cleanup pending objects."));
+	ProcessPendingEvents();
+
+	AddDebugLogLineN(logGeneral, wxT("Terminate upload thread."));
+	uploadBandwidthThrottler->EndThread();
 
 	// Log
 	AddDebugLogLineM(false, logGeneral, wxT("CamuleApp::ShutDown() has ended."));
