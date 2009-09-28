19e732d6b10fad53aa890a2c409cad692d28b152
gonosztopi <gonosztopi@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2009-09-28 07:11:52 +0000

    See comment. Based on eMule0.49.

--- trunk/src/kademlia/kademlia/SearchManager.cpp
+++ trunk/src/kademlia/kademlia/SearchManager.cpp
@@ -234,13 +234,17 @@ bool CSearchManager::IsFWCheckUDPSearch(const CUInt128& target)
 
 void CSearchManager::GetWords(const wxString& str, WordList *words, bool allowDuplicates)
 {
-	size_t len = 0;
 	wxString current_word;
 	wxStringTokenizer tkz(str, GetInvalidKeywordChars());
 	while (tkz.HasMoreTokens()) {
 		current_word = tkz.GetNextToken();
-		
-		if ((len = current_word.Length()) > 2) {
+		// TODO: We'd need a safe way to determine if a sequence which contains only 3 chars is a real word.
+		// Currently we do this by evaluating the UTF-8 byte count. This will work well for Western locales,
+		// AS LONG AS the min. byte count is 3(!). If the byte count is once changed to 2, this will not
+		// work properly any longer because there are a lot of Western characters which need 2 bytes in UTF-8.
+		// Maybe we need to evaluate the Unicode character values itself whether the characters are located
+		// in code ranges where single characters are known to represent words.
+		if (strlen((const char *)(current_word.utf8_str())) >= 3) {
 			current_word.MakeLower();
 			if (!allowDuplicates) {
 				words->remove(current_word);
