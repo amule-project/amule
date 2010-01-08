a727abf731d95af4ef040f817b1ad446f3b72f5f
sturedman <sturedman@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2010-01-08 14:47:25 +0000

    Trigger download of IP filter from amulecmd

--- trunk/src/ExternalConn.cpp
+++ trunk/src/ExternalConn.cpp
@@ -1336,6 +1336,9 @@ CECPacket *CECServerSocket::ProcessRequest2(const CECPacket *request,
 
 		case EC_OP_IPFILTER_UPDATE: {
 			wxString url = request->GetTagByIndexSafe(0)->GetStringData();
+			if (url == wxEmptyString) {
+				url = thePrefs::IPFilterURL();
+			}
 			theApp->ipfilter->Update(url);
 			response = new CECPacket(EC_OP_NOOP);
 			break;
