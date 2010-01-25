981c385c3e0e87ed45f334fce80237bbd7480767
sturedman <sturedman@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2010-01-25 21:10:16 +0000

    PlasmaMule 0.2 (Vollstrecker)

--- trunk/src/utils/plasmamule/plasma-applet-plasmamule.cpp
+++ trunk/src/utils/plasmamule/plasma-applet-plasmamule.cpp
@@ -1,14 +1,40 @@
+//
+// This file is part of the aMule Project.
+//
+// Copyright (c) 2010 Werner Mahr (Vollstrecker) <amule@vollstreckernet.de>
+//
+// Any parts of this program contributed by third-party developers are copyrighted
+// by their respective authors.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
+//
+
 #include "plasma-applet-plasmamule.h"
-#include <QPainter>
-#include <QFontMetrics>
-#include <QSizeF>
-#include <QTime>
-#include <QStringList>
+
+#include <kdebug.h>
 
 #include <plasma/svg.h>
 #include <plasma/theme.h>
 #include <plasma/version.h>
-#include <kdebug.h>
+
+#include <QFontMetrics>
+#include <QPainter>
+#include <QSizeF>
+#include <QStringList>
+#include <QTime>
+
 K_EXPORT_PLASMA_APPLET(plasma-applet-plasmamule, PlasmaMuleApplet)
 
 PlasmaMuleApplet::PlasmaMuleApplet(QObject *parent, const QVariantList &args)
@@ -30,7 +56,6 @@ PlasmaMuleApplet::~PlasmaMuleApplet()
  
 void PlasmaMuleApplet::init()
 {
-	m_update_interval = 5000;
 	connectToEngine();
 }
 
@@ -81,10 +106,15 @@ void PlasmaMuleApplet::paintInterface(QPainter *painter,
 		minutes=time/60;
 		time=time%60;
 
-		runtime = QTime (hours, minutes, time).toString(Qt::LocalDate);
+		runtime = QTime (hours, minutes, time).toString(Qt::ISODate);
 		if (days)
 		{
-			runtime.prepend(QString("%1 days ").arg(days));
+			if (days = 1)
+			{
+				runtime.prepend(QString("%1 day ").arg(days));
+			} else {
+				runtime.prepend(QString("%1 days ").arg(days));
+			}
 		}
 		message = QString("%1 has been running for %2 \n\n").arg(m_version).arg(runtime);
 		if (m_ed2k_state == 0 && m_kad_status == 0)
@@ -140,14 +170,11 @@ void PlasmaMuleApplet::paintInterface(QPainter *painter,
 		}
 		message.append(QString("Sharing: %1 %2, Clients on queue: %3").arg(m_shared_files_count).arg(files_unit).arg(m_clients_in_up_queue));
 		painter->drawText(contentsRect, Qt::TextDontClip | Qt::AlignTop | Qt::AlignLeft | Qt::TextWordWrap, message);
+
 		if (painter->boundingRect(contentsRect, Qt::TextDontClip | Qt::AlignTop | Qt::AlignLeft | Qt::TextWordWrap,
-			 message).width() > contentsRect.width())
-		{
-			resize(painter->boundingRect(contentsRect, message).width()+(contentsRect.topLeft().x()*2),
-				painter->boundingRect(contentsRect, message).width()+(contentsRect.topLeft().x()*2));
-		} else if (painter->boundingRect(contentsRect, Qt::TextDontClip | Qt::AlignTop | Qt::AlignLeft | Qt::TextWordWrap,
 			 message).height() > contentsRect.height())
 		{
+			kDebug() << "Resizing";
 			resize(painter->boundingRect(contentsRect, message).height()+(contentsRect.topLeft().y()*2),
 				painter->boundingRect(contentsRect, message).height()+(contentsRect.topLeft().y()*2));
 		}
@@ -175,132 +202,126 @@ QString PlasmaMuleApplet::calcSize (qlonglong in_size)
 void PlasmaMuleApplet::connectToEngine()
 {
 	m_aMuleEngine = dataEngine("plasmamule");
-	m_aMuleEngine->connectAllSources(this, m_update_interval);
+	m_aMuleEngine->connectAllSources(this, 0);
 	connect(m_aMuleEngine, SIGNAL(sourceAdded(const QString&)), this, SLOT(onSourceAdded(const QString&)));
 	connect(m_aMuleEngine, SIGNAL(sourceRemoved(const QString&)), this, SLOT(onSourceRemoved(const QString&)));
 }
 
 void PlasmaMuleApplet::onSourceAdded(const QString& source)
 {
-	m_aMuleEngine->connectSource(source, this, m_update_interval);
+	kDebug() << "New Source: " << source << " added";
+	m_aMuleEngine->connectSource(source, this, 0);
 }
 
 void PlasmaMuleApplet::onSourceRemoved(const QString& source)
 {
+	kDebug() << "Source: " << source << " removed";
 	update();
 }
 
 void PlasmaMuleApplet::dataUpdated(const QString& source, const Plasma::DataEngine::Data &data)
 {
 	bool needs_update = FALSE;
-
-	if (source == "update_interval")
+	kDebug() << "Updating data";
+	if (data["os_active"].toBool() != m_os_active && data.contains("os_active"))
 	{
-		if (data["update_interval"] != m_update_interval && data.contains("update_interval"))
-		{
-			m_update_interval = data["update_interval"].toInt();
-			connectToEngine ();
-		}
-	} else {
-		if (data["os_active"].toBool() != m_os_active && data.contains("os_active"))
-		{
-			m_os_active = data["os_active"].toBool();
-			needs_update = TRUE;
-		}
-		if (data["config_found"].toBool() != m_config_found && data.contains("config_found"))
-		{
-			m_config_found = data["config_found"].toBool();
-			needs_update = TRUE;
-		}
-		if (data["ed2k_state"].toInt() != m_ed2k_state && data.contains("ed2k_state"))
-		{
-			m_ed2k_state = data["ed2k_state"].toInt();
-			needs_update = TRUE;
-		}
-		if (data["ed2k_server_name"] != m_ed2k_server_name && data.contains("ed2k_server_name"))
-		{
-			m_ed2k_server_name = data["ed2k_server_name"].toString();
-			needs_update = TRUE;
-		}
-		if (data["ed2k_server_ip"] != m_ed2k_server_ip && data.contains("ed2k_server_ip"))
-		{
-			m_ed2k_server_ip = data["ed2k_server_ip"].toString();
-			needs_update = TRUE;
-		}
-		if (data["ed2k_server_port"].toInt() != m_ed2k_server_port && data.contains("ed2k_server_port"))
-		{
-			m_ed2k_server_port = data["ed2k_server_port"].toInt();
-			needs_update = TRUE;
-		}
-		if (data["ed2k_id_high_low"] != m_ed2k_id_high_low && data.contains("ed2k_id_high_low"))
-		{
-			m_ed2k_id_high_low = data["ed2k_id_high_low"].toString();
-			needs_update = TRUE;
-		}
-		if (data["kad_status"].toInt() != m_kad_status && data.contains("kad_status"))
-		{
-			m_kad_status = data["kad_status"].toInt();
-			needs_update = TRUE;
-		}
-		if (data["down_speed"].toInt() != m_down_speed && data.contains("down_speed"))
-		{
-			m_down_speed = data["down_speed"].toDouble();
-			needs_update = TRUE;
-		}
-		if (data["up_speed"].toDouble() != m_up_speed && data.contains("up_speed"))
-		{
-			m_up_speed = data["up_speed"].toDouble();
-			needs_update = TRUE;
-		}
-		if (data["clients_in_up_queue"].toInt() != m_clients_in_up_queue && data.contains("clients_in_up_queue"))
-		{
-			m_clients_in_up_queue = data["clients_in_up_queue"].toInt();
-			needs_update = TRUE;
-		}
-		if (data["shared_files_count"].toInt() != m_shared_files_count && data.contains("shared_files_count"))
-		{
-			m_shared_files_count = data["shared_files_count"].toInt();
-			needs_update = TRUE;
-		}
-		if (data["nickname"] != m_nickname && data.contains("nickname"))
-		{
-			m_nickname = data["nickname"].toString();
-			needs_update = TRUE;
-		}
-		if (data["total_bytes_downloaded"].toLongLong() != m_total_bytes_downloaded && data.contains("total_bytes_downloaded"))
-		{
-			m_total_bytes_downloaded = data["total_bytes_downloaded"].toLongLong();
-			needs_update = TRUE;
-		}
-		if (data["total_bytes_uploaded"].toLongLong() != m_total_bytes_uploaded && data.contains("total_bytes_uploaded"))
-		{
-			m_total_bytes_uploaded = data["total_bytes_uploaded"].toLongLong();
-			needs_update = TRUE;
-		}
-		if (data["version"] != m_version && data.contains("version"))
-		{
-			m_version = data["version"].toString();
-			needs_update = TRUE;
-		}
-		if (data["session_bytes_downloaded"].toLongLong() != m_session_bytes_downloaded && data.contains("session_bytes_downloaded"))
-		{
-			m_session_bytes_downloaded = data["session_bytes_downloaded"].toLongLong();
-			needs_update = TRUE;
-		}
-		if (data["session_bytes_uploaded"].toLongLong() != m_session_bytes_uploaded && data.contains("session_bytes_uploaded"))
-		{
-			m_session_bytes_uploaded = data["session_bytes_uploaded"].toLongLong();
-			needs_update = TRUE;
-		}
-		if (data["uptime"].toInt() != m_uptime && data.contains("uptime"))
-		{
-			m_uptime = data["uptime"].toInt();
-			needs_update = TRUE;
-		}
+		m_os_active = data["os_active"].toBool();
+		needs_update = TRUE;
+	}
+	if (data["config_found"].toBool() != m_config_found && data.contains("config_found"))
+	{
+		m_config_found = data["config_found"].toBool();
+		needs_update = TRUE;
+	}
+	if (data["ed2k_state"].toInt() != m_ed2k_state && data.contains("ed2k_state"))
+	{
+		m_ed2k_state = data["ed2k_state"].toInt();
+		needs_update = TRUE;
+	}
+	if (data["ed2k_server_name"] != m_ed2k_server_name && data.contains("ed2k_server_name"))
+	{
+		m_ed2k_server_name = data["ed2k_server_name"].toString();
+		needs_update = TRUE;
+	}
+	if (data["ed2k_server_ip"] != m_ed2k_server_ip && data.contains("ed2k_server_ip"))
+	{
+		m_ed2k_server_ip = data["ed2k_server_ip"].toString();
+		needs_update = TRUE;
+	}
+	if (data["ed2k_server_port"].toInt() != m_ed2k_server_port && data.contains("ed2k_server_port"))
+	{
+		m_ed2k_server_port = data["ed2k_server_port"].toInt();
+		needs_update = TRUE;
+	}
+	if (data["ed2k_id_high_low"] != m_ed2k_id_high_low && data.contains("ed2k_id_high_low"))
+	{
+		m_ed2k_id_high_low = data["ed2k_id_high_low"].toString();
+		needs_update = TRUE;
+	}
+	if (data["kad_status"].toInt() != m_kad_status && data.contains("kad_status"))
+	{
+		m_kad_status = data["kad_status"].toInt();
+		needs_update = TRUE;
+	}
+	if (data["down_speed"].toInt() != m_down_speed && data.contains("down_speed"))
+	{
+		m_down_speed = data["down_speed"].toDouble();
+		needs_update = TRUE;
+	}
+	if (data["up_speed"].toDouble() != m_up_speed && data.contains("up_speed"))
+	{
+		m_up_speed = data["up_speed"].toDouble();
+		needs_update = TRUE;
+	}
+	if (data["clients_in_up_queue"].toInt() != m_clients_in_up_queue && data.contains("clients_in_up_queue"))
+	{
+		m_clients_in_up_queue = data["clients_in_up_queue"].toInt();
+		needs_update = TRUE;
+	}
+	if (data["shared_files_count"].toInt() != m_shared_files_count && data.contains("shared_files_count"))
+	{
+		m_shared_files_count = data["shared_files_count"].toInt();
+		needs_update = TRUE;
+	}
+	if (data["nickname"] != m_nickname && data.contains("nickname"))
+	{
+		m_nickname = data["nickname"].toString();
+		needs_update = TRUE;
+	}
+	if (data["total_bytes_downloaded"].toLongLong() != m_total_bytes_downloaded && data.contains("total_bytes_downloaded"))
+	{
+		m_total_bytes_downloaded = data["total_bytes_downloaded"].toLongLong();
+		needs_update = TRUE;
+	}
+	if (data["total_bytes_uploaded"].toLongLong() != m_total_bytes_uploaded && data.contains("total_bytes_uploaded"))
+	{
+		m_total_bytes_uploaded = data["total_bytes_uploaded"].toLongLong();
+		needs_update = TRUE;
+	}
+	if (data["version"] != m_version && data.contains("version"))
+	{
+		m_version = data["version"].toString();
+		needs_update = TRUE;
+	}
+	if (data["session_bytes_downloaded"].toLongLong() != m_session_bytes_downloaded && data.contains("session_bytes_downloaded"))
+	{
+		m_session_bytes_downloaded = data["session_bytes_downloaded"].toLongLong();
+		needs_update = TRUE;
+	}
+	if (data["session_bytes_uploaded"].toLongLong() != m_session_bytes_uploaded && data.contains("session_bytes_uploaded"))
+	{
+		m_session_bytes_uploaded = data["session_bytes_uploaded"].toLongLong();
+		needs_update = TRUE;
+	}
+	if (data["uptime"].toInt() != m_uptime && data.contains("uptime"))
+	{
+		m_uptime = data["uptime"].toInt();
+		needs_update = TRUE;
 	}
 
 	if (needs_update)
 	{
+		kDebug() << "Updating view";
 		update();
 	}
 }
--- trunk/src/utils/plasmamule/plasma-engine-plasmamule.h
+++ trunk/src/utils/plasmamule/plasma-engine-plasmamule.h
@@ -1,8 +1,32 @@
+//
+// This file is part of the aMule Project.
+//
+// Copyright (c) 2010 Werner Mahr (Vollstrecker) <amule@vollstreckernet.de>
+//
+// Any parts of this program contributed by third-party developers are copyrighted
+// by their respective authors.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA
+//
+
 #ifndef PLASMAMULENGINE_H
 #define PLASMAMULENGINE_H
 
 #include <plasma/dataengine.h>
-#include <QString>
+#include <QStringList>
+#include <kdirwatch.h>
 
 class PlasmaMuleEngine : public Plasma::DataEngine
 {
@@ -14,13 +38,20 @@ class PlasmaMuleEngine : public Plasma::DataEngine
 		QStringList sources() const;
 		void init ();
 
+	public Q_SLOTS:
+		void file_changed (const QString &path);
+		void new_file (const QString &path);
+		void timeout ();
+
 	protected:
 		bool sourceRequestEvent(const QString& name);
 		bool updateSourceEvent(const QString& name);
 
 	private:
-		bool m_OSActive;
+		bool m_OSActive, m_timer;
 		QString m_OSFile;
+		KDirWatch m_dirwatcher;
+		QStringList m_incoming_dirs;
 };
 
 #endif // PLASMAMULENGINE_H
