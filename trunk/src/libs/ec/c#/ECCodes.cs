b2f5e0d5e70e90755d272ff10373a2a7d97b25ec
marcell <marcell@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2010-01-29 23:35:03 +0000

    Updated German translation of manpages (msgmerge)

--- trunk/src/libs/ec/c#/ECCodes.cs
+++ trunk/src/libs/ec/c#/ECCodes.cs
@@ -35,7 +35,7 @@ public enum ECFlags {
 	EC_FLAG_HAS_ID	 = 0x00000004,
 	EC_FLAG_ACCEPTS	 = 0x00000010,
 	EC_FLAG_NOTIFY	 = 0x00008000,
-	EC_FLAG_UNKNOWN_MASK = 0xff7f7f08
+
 };
 public enum ECOpCodes {
 	EC_OP_NOOP                          = 0x01,
