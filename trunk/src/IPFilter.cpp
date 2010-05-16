d64828bdbea8a91bdda40503986116dc4c6345fb
kry <kry@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2010-05-16 11:54:23 +0000

    Already trimmed

--- trunk/src/IPFilter.cpp
+++ trunk/src/IPFilter.cpp
@@ -345,17 +345,14 @@ private:
 	 */
 	bool ProcessAntiP2PLine(const wxString& sLine)
 	{
-		// remove spaces from the left and right.
-		const wxString line = sLine.Strip(wxString::leading);
-
 		// Extract description (first) and IP-range (second) form the line
-		int pos = line.Find(wxT(':'), true);
+		int pos = sLine.Find(wxT(':'), true);
 		if (pos == -1) {
 			return false;
 		}
 
-		wxString Description = line.Left(pos).Strip(wxString::trailing);
-		wxString IPRange     = line.Right(line.Len() - pos - 1);
+		wxString Description = sLine.Left(pos).Strip(wxString::trailing);
+		wxString IPRange     = sLine.Right(sLine.Len() - pos - 1);
 
 		// Convert string IP's to host order IP numbers
 		uint32 IPStart = 0;
