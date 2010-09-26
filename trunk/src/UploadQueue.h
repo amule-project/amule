1add3661d965d0f9724a5c1a52202a1579e97fa1
sturedman <sturedman@6ea3b165-b8b8-dda6-0cea-db2f03e7d5ff>
2010-09-26 16:34:18 +0000

    Reworked suspended files handling
    Deleted files were kept permanently in the suspended list before, which had the effect that add, delete, add again resulted in a download that wasn't shared.
    Now deleted files are detected simply by not being shared.
    Also use a set instead of a list.
    
    See http://forum.amule.org/index.php?topic=18182

--- trunk/src/UploadQueue.h
+++ trunk/src/UploadQueue.h
@@ -27,7 +27,7 @@
 #define UPLOADQUEUE_H
 
 #include "MD4Hash.h"		// Needed for CMD4Hash
-
+#include <set>
 
 class CUpDownClient;
 
@@ -50,19 +50,19 @@ public:
 	CUpDownClient* GetWaitingClientByIP_UDP(uint32 dwIP, uint16 nUDPPort, bool bIgnorePortOnUniqueIP, bool* pbMultipleIPs = NULL);
 
 	uint16	GetWaitingPosition(const CUpDownClient *client) const;
-	uint16	SuspendUpload(const CMD4Hash &);
+	uint16	SuspendUpload(const CMD4Hash &, bool terminate);
 	void	ResumeUpload(const CMD4Hash &);
 
 private:
 	void	RemoveFromWaitingQueue(CClientPtrList::iterator pos);
 	uint16	GetMaxSlots() const;
 	void	AddUpNextClient(CUpDownClient* directadd = 0);
+	bool	IsSuspended(const CMD4Hash& hash) { return suspendedUploadsSet.find(hash) != suspendedUploadsSet.end(); }
 
 	CClientPtrList m_waitinglist;
 	CClientPtrList m_uploadinglist;
 	
-	typedef std::list<CMD4Hash> suspendlist;
-	suspendlist suspended_uploads_list;  //list for suspended uploads
+	std::set<CMD4Hash> suspendedUploadsSet;  // set for suspended uploads
 	uint32	m_nLastStartUpload;
 	bool	lastupslotHighID; // VQB lowID alternation
 	bool	m_allowKicking;
