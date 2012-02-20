9f28a408b5ac80359751546d4a69f4e14844aeb9
sturedman <sturedman@amule.org>
2012-02-20 21:57:12 +0000

    Fixed nodes.dat default url again (gav616)

--- src/Preferences.cpp
+++ src/Preferences.cpp
@@ -1246,7 +1246,7 @@ void CPreferences::BuildItemList( const wxString& appdir )
 
 	s_MiscList.push_back( new Cfg_Bool( wxT("/eMule/DropSlowSources"),		s_DropSlowSources, false ) );
 
-	s_MiscList.push_back( new Cfg_Str(  wxT("/eMule/KadNodesUrl"),			s_KadURL, wxT("http://upd.emule-security.net/nodes.dat") ) );
+	s_MiscList.push_back( new Cfg_Str(  wxT("/eMule/KadNodesUrl"),			s_KadURL, wxT("http://upd.emule-security.org/nodes.dat") ) );
 	s_MiscList.push_back( new Cfg_Str(  wxT("/eMule/Ed2kServersUrl"),		s_Ed2kURL, wxT("http://gruk.org/server.met.gz") ) );
 	s_MiscList.push_back( MkCfg_Int( wxT("/eMule/ShowRatesOnTitle"),		s_showRatesOnTitle, 0 ));
 
