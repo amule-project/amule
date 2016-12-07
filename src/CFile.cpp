047a55c93f93760cc3534d3d60e5a9aa8c13cfa1
Vollstrecker <werner@vollstreckernet.de>
2016-12-07 16:24:11 +0100

    mslu is removed in wxMSW 3.1.0

--- src/CFile.cpp
+++ src/CFile.cpp
@@ -92,7 +92,7 @@ char* mktemp( char * path ) { return path ;}
 #	define   O_BINARY    (0)
 #endif  //__UNIX__
 
-#ifdef __WINDOWS__
+#if defined(__WINDOWS__) && !wxCHECK_VERSION(3, 1, 0)
 #include <wx/msw/mslu.h>
 #endif
 
