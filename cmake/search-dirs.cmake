a1be90d0a642959e81c18f1b964e17c6bd58c0ee
Vollstrecker <werner@vollstreckernet.de>
2016-12-07 14:51:24 +0100

    Added some more search-paths

--- cmake/search-dirs.cmake
+++ cmake/search-dirs.cmake
@@ -1,12 +1,17 @@
 # Uncomment one or more of the following lines to tell cmake, where to find it's header and
 # libs. You can specify the path also using -D<VAR> from commandline.
 
+# The location, where the bison executable can be found
+#SET (BISON_EXECUTABLE "${CMAKE_SOURCE_DIR}/../win_flex_bison-latest/win_bison.exe")
+
 # The location, where to find the crypto++ header-files
 #SET (CRYTOPP_HEADER_PATH "${CMAKE_SOURCE_DIR}/../cryptopp")
 
 # The location, where the linkable output of crypto++ compilation can be found.
 #SET (CRYTOPP_LIB_SEARCH_PATH "${CMAKE_SOURCE_DIR}/../cryptopp/Win32/Output/Debug")
 
+# The location, where the flex executable can be found
+#SET (FLEX_EXECUTABLE "${CMAKE_SOURCE_DIR}/../win_flex_bison-latest/win_flex.exe")
 
 # The location, where the headers of libgeoip can be found.
 #SET (GEOIP_INCLUDE_DIR "${CMAKE_SOURCE_DIR}/../geoip-api-c-master/libGeoIP")
@@ -21,4 +26,4 @@
 #SET (wxWidgets_ROOT_DIR "${CMAKE_SOURCE_DIR}/../wxWidgets")
 
 # The location, where to find zlib.
-#SET (ZLIB_ROOT "${CMAKE_SOURCE_DIR}/../zlib")
+#SET (ZLIB_ROOT "${CMAKE_SOURCE_DIR}/../zlib-1.2.8")
