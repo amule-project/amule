7c6f3ab43d550b5af09864a6415372a88d7132a4
Pablo Barciela <scow@riseup.net>
2017-11-17 20:42:55 +0100

    Fix: switch tabs on search result
    Fixes https://github.com/amule-project/amule/issues/114

--- src/MuleNotebook.cpp
+++ src/MuleNotebook.cpp
@@ -45,10 +45,10 @@ BEGIN_EVENT_TABLE(CMuleNotebook, wxNotebook)
 	EVT_MENU(MP_CLOSE_OTHER_TABS,	CMuleNotebook::OnPopupCloseOthers)
 
 	// Madcat - tab closing engine
-	EVT_LEFT_DOWN(CMuleNotebook::OnMouseButtonRelease)
-	EVT_LEFT_UP(CMuleNotebook::OnMouseButtonRelease)
-	EVT_MIDDLE_DOWN(CMuleNotebook::OnMouseButtonRelease)
-	EVT_MIDDLE_UP(CMuleNotebook::OnMouseButtonRelease)
+	EVT_LEFT_DOWN(CMuleNotebook::OnMouseButton)
+	EVT_LEFT_UP(CMuleNotebook::OnMouseButton)
+	EVT_MIDDLE_DOWN(CMuleNotebook::OnMouseButton)
+	EVT_MIDDLE_UP(CMuleNotebook::OnMouseButton)
 	EVT_MOTION(CMuleNotebook::OnMouseMotion)
 #if MULE_NEEDS_DELETEPAGE_WORKAROUND
 	EVT_MULENOTEBOOK_DELETE_PAGE(wxID_ANY, CMuleNotebook::OnDeletePage)
@@ -217,7 +217,7 @@ void CMuleNotebook::OnPopupCloseOthers(wxCommandEvent& WXUNUSED(evt))
 }
 
 
-void CMuleNotebook::OnMouseButtonRelease(wxMouseEvent &event)
+void CMuleNotebook::OnMouseButton(wxMouseEvent &event)
 {
 	if (GetImageList() == NULL) {
 		// This Mulenotebook has no images on tabs, so nothing to do.
@@ -235,16 +235,13 @@ void CMuleNotebook::OnMouseButtonRelease(wxMouseEvent &event)
 
 	if (event.LeftDown() &&  (flags == wxNB_HITTEST_ONICON)) {
 		tab_down_icon = tab;
-		return;
 	}
 	else if (event.MiddleDown() && (flags == wxNB_HITTEST_ONLABEL)) {
 		tab_down_label = tab;
-		return;
 	}
 	else if (event.LeftDown() || event.MiddleDown()) {
 		tab_down_icon = -1;
 		tab_down_label = -1;
-		return;
 	}
 	
 	if (((tab != -1) &&  (((flags == wxNB_HITTEST_ONICON) && event.LeftUp() && (tab == tab_down_icon)) ||
