cb30875bd4e45736ff4ee1137ff97316295ff7be
Pablo Barciela <scow@riseup.net>
2018-11-24 01:23:58 +0100

    Fix build with libupnp 1.6

--- src/UPnPBase.h
+++ src/UPnPBase.h
@@ -489,9 +489,15 @@ public:
 
 	// Callback function
 	static int Callback(
+#if UPNP_VERSION >= 10800
 		Upnp_EventType_e EventType,
 		const void *Event,
 		void *Cookie);
+#else
+		Upnp_EventType EventType,
+		void* Event,
+		void* Cookie);
+#endif
 
 private:
 	void OnEventReceived(
