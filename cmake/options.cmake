962e2605ac9d144203b7f09fcb04812832e783c2
Werner Mahr <werner@vollstreckernet.de>
2019-06-18 00:39:28 +0200

    Add the actual unittests

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -46,6 +46,7 @@ OPTION (BUILD_REMOTEGUI "compile aMule remote GUI")
 OPTION (BUILD_WEBSERVER "compile aMule WebServer")
 OPTION (BUILD_WXCAS "compile aMule GUI Statistics")
 OPTION (BUILD_XAS "install xas XChat2 plugin")
+OPTION (BUILD_TESTING "Run Tests after compile" OFF)
 
 IF (PREFIX)
 	SET (CMAKE_INSTALL_PREFIX "${PREFIX}")
