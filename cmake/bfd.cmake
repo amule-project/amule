13d5ab291699f892fa6c0bfe8eb913aecdf4c3c7
Werner Mahr <werner@vollstreckernet.de>
2019-07-18 16:38:22 +0200

    Added check, if compiling with bfd works, too.

--- cmake/bfd.cmake
+++ cmake/bfd.cmake
@@ -4,7 +4,7 @@ INCLUDE (CheckCSourceCompiles)
 CHECK_INCLUDE_FILE (bfd.h HAVE_BFD)
 
 IF (HAVE_BFD)
-	FIND_LIBRARY (LIBBFD bfd)
+	FIND_LIBRARY (LIBBFD_TMP bfd)
 
 	IF (NOT LIBBFD)
 		MESSAGE (STATUS "No useable bfd-lib found, disabling support")
@@ -13,21 +13,30 @@ ELSE (HAVE_BFD)
 	MESSAGE (STATUS "bfd.h not found, disabling support")
 ENDIF (HAVE_BFD)
 
-FOREACH (CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD}" "${LIBBFD};iberty" "${LIBBFD};dl" "${LIBBFD};iberty;dl" "${LIBBFD};${LIBINTL}" "${LIBBFD};iberty;${LIBINTL}" "${LIBBFD};iberty;dl;${LIBINBTL}")
+FOREACH (
+	CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD_TMP}" "${LIBBFD_TMP};iberty" "${LIBBFD_TMP};dl" "${LIBBFD};iberty;dl" "${LIBBFD_TMP};${LIBINTL}" "${LIBBFD_TMP};iberty;${LIBINTL_TMP}" "${LIBBFD_TMP};iberty;dl;${LIBINBTL}"
+)
 	UNSET (BFD_COMPILE_TEST CACHE)
 	CHECK_C_SOURCE_COMPILES ("#include <ansidecl.h>
 		#include <bfd.h>
 
 		int main()
 		{
-			char *dummy = bfd_errmsg(bfd_get_error());
+			const char *dummy = bfd_errmsg(bfd_get_error());
 		}"
 		BFD_COMPILE_TEST
 	)
 
 	IF (BFD_COMPILE_TEST)
 		SET (LIBBFD ${CMAKE_REQUIRED_LIBRARIES})
+		UNSET (${CMAKE_REQUIRED_LIBRARIES})
 		BREAK()
 	ENDIF (BFD_COMPILE_TEST)	
-ENDFOREACH (CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD}" "${LIBBFD};iberty" "${LIBBFD};dl" "${LIBBFD};iberty;dl" "${LIBBFD};${LIBINTL}" "${LIBBFD};iberty;${LIBINTL}" "${LIBBFD};iberty;dl;${LIBINBTL}")
+ENDFOREACH (
+	CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD_TMP}" "${LIBBFD_TMP};iberty" "${LIBBFD_TMP};dl" "${LIBBFD};iberty;dl" "${LIBBFD_TMP};${LIBINTL}" "${LIBBFD_TMP};iberty;${LIBINTL_TMP}" "${LIBBFD_TMP};iberty;dl;${LIBINBTL}"
+)
 
+IF (NOT LIBBFD)
+	SET (HAVE_BFD FALSE)
+	MESSAGE (STATUS "bfd.h found but can't link against it, disabling support")
+ENDIF (NOT LIBBFD)
