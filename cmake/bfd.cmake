35efdc318b2ab9c007ed743c85fc32454f3ad002
Werner Mahr <werner@vollstreckernet.de>
2019-07-19 17:40:28 +0200

    If the header wasn't found, we don't have to check for useable libbfd.

--- cmake/bfd.cmake
+++ cmake/bfd.cmake
@@ -9,34 +9,34 @@ IF (HAVE_BFD)
 	IF (NOT LIBBFD)
 		MESSAGE (STATUS "No useable bfd-lib found, disabling support")
 	ENDIF (NOT LIBBFD)		
+
+	FOREACH (
+		CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD_TMP}" "${LIBBFD_TMP};iberty" "${LIBBFD_TMP};dl" "${LIBBFD};iberty;dl" "${LIBBFD_TMP};${LIBINTL}" "${LIBBFD_TMP};iberty;${LIBINTL_TMP}" "${LIBBFD_TMP};iberty;dl;${LIBINBTL}"
+	)
+		UNSET (BFD_COMPILE_TEST CACHE)
+		CHECK_C_SOURCE_COMPILES ("#include <ansidecl.h>
+			#include <bfd.h>
+
+			int main()
+			{
+				const char *dummy = bfd_errmsg(bfd_get_error());
+			}"
+			BFD_COMPILE_TEST
+		)
+
+		IF (BFD_COMPILE_TEST)
+			SET (LIBBFD ${CMAKE_REQUIRED_LIBRARIES})
+			UNSET (${CMAKE_REQUIRED_LIBRARIES})
+			BREAK()
+		ENDIF (BFD_COMPILE_TEST)	
+	ENDFOREACH (
+		CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD_TMP}" "${LIBBFD_TMP};iberty" "${LIBBFD_TMP};dl" "${LIBBFD};iberty;dl" "${LIBBFD_TMP};${LIBINTL}" "${LIBBFD_TMP};iberty;${LIBINTL_TMP}" "${LIBBFD_TMP};iberty;dl;${LIBINBTL}"
+	)
+
+	IF (NOT LIBBFD)
+		SET (HAVE_BFD FALSE)
+		MESSAGE (STATUS "bfd.h found but can't link against it, disabling support")
+	ENDIF (NOT LIBBFD)
 ELSE (HAVE_BFD)
 	MESSAGE (STATUS "bfd.h not found, disabling support")
 ENDIF (HAVE_BFD)
-
-FOREACH (
-	CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD_TMP}" "${LIBBFD_TMP};iberty" "${LIBBFD_TMP};dl" "${LIBBFD};iberty;dl" "${LIBBFD_TMP};${LIBINTL}" "${LIBBFD_TMP};iberty;${LIBINTL_TMP}" "${LIBBFD_TMP};iberty;dl;${LIBINBTL}"
-)
-	UNSET (BFD_COMPILE_TEST CACHE)
-	CHECK_C_SOURCE_COMPILES ("#include <ansidecl.h>
-		#include <bfd.h>
-
-		int main()
-		{
-			const char *dummy = bfd_errmsg(bfd_get_error());
-		}"
-		BFD_COMPILE_TEST
-	)
-
-	IF (BFD_COMPILE_TEST)
-		SET (LIBBFD ${CMAKE_REQUIRED_LIBRARIES})
-		UNSET (${CMAKE_REQUIRED_LIBRARIES})
-		BREAK()
-	ENDIF (BFD_COMPILE_TEST)	
-ENDFOREACH (
-	CMAKE_REQUIRED_LIBRARIES "" "${LIBBFD_TMP}" "${LIBBFD_TMP};iberty" "${LIBBFD_TMP};dl" "${LIBBFD};iberty;dl" "${LIBBFD_TMP};${LIBINTL}" "${LIBBFD_TMP};iberty;${LIBINTL_TMP}" "${LIBBFD_TMP};iberty;dl;${LIBINBTL}"
-)
-
-IF (NOT LIBBFD)
-	SET (HAVE_BFD FALSE)
-	MESSAGE (STATUS "bfd.h found but can't link against it, disabling support")
-ENDIF (NOT LIBBFD)
