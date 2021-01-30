9f5d4331a3905b2722c6158972ab3e92d4f33691
Vollstrecker <werner@vollstreckernet.de>
2021-01-30 16:36:22 +0100

    Changed geoip detection and definitions to use imported lib and interfaces

--- cmake/ip2country.cmake
+++ cmake/ip2country.cmake
@@ -1,22 +1,23 @@
-if (NOT GEOIP_LIBRARY)
+if (GEOIP_INCLUDE_DIR)
+	set (CMAKE_REQUIRED_INCLUDES ${GEOIP_INCLUDE_DIR})
+endif()
+
+if (NOT GEOIP_LIB)
 	include (CheckIncludeFile)
 
-	if (GEOIP_INCLUDE_DIR)
-		set (CMAKE_REQUIRED_INCLUDES ${GEOIP_INCLUDE_DIR})
-	endif()
 
 	check_include_file (GeoIP.h GEOIP_H)
 
 	if (GEOIP_H)
-		find_library (GEOIP_LIBRARY GeoIP)
+		find_library (GEOIP_LIB GeoIP)
 
-		if (NOT GEOIP_LIBRARY AND GEOIP_INCLUDE_DIR)
-			find_library (GEOIP_LIBRARY GeoIP
+		if (NOT GEOIP_LIB AND GEOIP_INCLUDE_DIR)
+			find_library (GEOIP_LIB GeoIP
 				PATHS ${GEOIP_INCLUDE_DIR}
 			)
 		endif()
 
-		if (NOT GEOIP_LIBRARY)
+		if (NOT GEOIP_LIB)
 			set (ENABLE_IP2COUNTRY FALSE)
 			message (STATUS "GeoIP lib not found, disabling support")
 		else()
@@ -26,10 +27,16 @@ if (NOT GEOIP_LIBRARY)
 		set (ENABLE_IP2COUNTRY FALSE)
 		message (STATUS "GeoIP headers not found, disabling support")
 	endif()
-
-	unset (CMAKE_REQUIRED_INCLUDES)
-
-	if (GEOIP_INCLUDE_DIR)
-		set (CMAKE_REQUIRED_INCLUDES)
-	endif()
 endif()
+
+if (ENABLE_IP2COUNTRY)
+	add_library (GeoIP::Shared UNKNOWN IMPORTED)
+
+	set_target_properties (GeoIP::Shared PROPERTIES
+		INTERFACE_COMPILE_DEFINITIONS "ENABLE_IP2COUNTRY"
+		INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_REQUIRED_INCLUDES}"
+		IMPORTED_LOCATION "${GEOIP_LIB}"
+	)
+endif()
+
+unset (CMAKE_REQUIRED_INCLUDES)
