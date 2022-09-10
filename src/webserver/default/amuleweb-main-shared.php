883ca1903fa78a634ebc0cec056747406d11de88
Sergi Amoros <sergi.amoros@softmachine.es>
2022-09-10 12:11:15 +0200

    change short_name for name

--- src/webserver/default/amuleweb-main-shared.php
+++ src/webserver/default/amuleweb-main-shared.php
@@ -109,6 +109,15 @@ label {
 	font-family: Helvetica;
 	font-size: 12px;
 	font-weight: normal;
+	overflow: hidden;
+	white-space: nowrap;
+	text-overflow: ellipsis;
+	word-break: break-all;
+	word-wrap: break-word;
+}
+.texte-full-name{
+	max-width: 0;
+	width: 45%;
 }
 label {
 font-family:"trebuchet ms",sans-serif;
@@ -178,9 +187,9 @@ function formCommandSubmit(command)
                   <td><input type="hidden" name="command"></td>
                   
             <td><a href="javascript:formCommandSubmit('reload');" onClick="MM_nbGroup('down','group1','reload','',1)" onMouseOver="MM_nbGroup('over','reload','','',1)" onMouseOut="MM_nbGroup('out')"><img src="images/refresh.png" alt="Ricarica Files Condivisi" name="reload" border="0" onload=""></a></td>
-				  <td><a href="javascript:formCommandSubmit('prioup');" onClick="MM_nbGroup('down','group1','up','',1)" onMouseOver="MM_nbGroup('over','up','','',1)" onMouseOut="MM_nbGroup('out')"><img name="up" src="images/up.png" border="0" alt="Alza Priorità" onLoad=""></a></td>
+				  <td><a href="javascript:formCommandSubmit('prioup');" onClick="MM_nbGroup('down','group1','up','',1)" onMouseOver="MM_nbGroup('over','up','','',1)" onMouseOut="MM_nbGroup('out')"><img name="up" src="images/up.png" border="0" alt="Alza Prioritï¿½" onLoad=""></a></td>
                   
-            <td><a href="javascript:formCommandSubmit('priodown');" onClick="MM_nbGroup('down','group1','down','',1)" onMouseOver="MM_nbGroup('over','down','','',1)" onMouseOut="MM_nbGroup('out')"><img src="images/down.png" alt="Abbassa Priorità" name="down" border="0" onload=""></a></td>
+            <td><a href="javascript:formCommandSubmit('priodown');" onClick="MM_nbGroup('down','group1','down','',1)" onMouseOver="MM_nbGroup('over','down','','',1)" onMouseOut="MM_nbGroup('out')"><img src="images/down.png" alt="Abbassa Prioritï¿½" name="down" border="0" onload=""></a></td>
                   <td><select name="select">
                       <option selected>Select prio</option>
                       <option>Low</option>
@@ -188,7 +197,7 @@ function formCommandSubmit(command)
                       <option>High</option>
                     </select> </td>
                   
-            <td><a href="javascript:formCommandSubmit('setprio');" onClick="MM_nbGroup('down','group1','resume','',1)" onMouseOver="MM_nbGroup('over','resume','','',1)" onMouseOut="MM_nbGroup('out')"><img src="images/ok.png" alt="Imposta Priorità" name="resume" border="0" onload=""></a></td>
+            <td><a href="javascript:formCommandSubmit('setprio');" onClick="MM_nbGroup('down','group1','resume','',1)" onMouseOver="MM_nbGroup('over','resume','','',1)" onMouseOut="MM_nbGroup('out')"><img src="images/ok.png" alt="Imposta Prioritï¿½" name="resume" border="0" onload=""></a></td>
               
                   <td> 
                     <?php
@@ -340,7 +349,7 @@ function formCommandSubmit(command)
 
 			echo "<td class='texte'>", '<input type="checkbox" name="', $file->hash, '" >', "</td>";
 
-			echo "<td class='texte'>", $file->short_name, "</td>";
+			echo "<td class='texte texte-full-name'>", $file->name, "</td>";
 			echo "<td class='texte' align='center'>", CastToXBytes($file->xfer), " (", CastToXBytes($file->xfer_all),")</td>";
 
 			echo "<td class='texte' align='center'>", $file->req, " (", $file->req_all, ")</td>";
