221c658d070ab94fa8f4d7486557076e87a3430f
RealGreenDragon <14246920+RealGreenDragon@users.noreply.github.com>
2026-06-14 15:17:05 +0200

    fix

--- src/webserver/src/php_core_lib.cpp
+++ src/webserver/src/php_core_lib.cpp
@@ -782,8 +782,8 @@ void CWriteStrBuffer::Write(const char *s, int len)
 	while ( len ) {
 		if ( (len + 1) <= m_curr_buf_left ) {
 			memcpy(m_buf_ptr, s, len);
-			*m_buf_ptr = '\0';
 			m_buf_ptr += len;
+			*m_buf_ptr = '\0';
 			m_curr_buf_left -= len;
 			len = 0;
 		} else {
